59950dd6c4594b0791f6c6e98024c0931af5777a
hoothin <rixixi@gmail.com>
2016-11-27 21:52:59 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2016.11.27.0
+// @version        2016.11.27.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -8258,6 +8258,10 @@ background-image:url("'+ prefs.icons.magnifier +'");\
                 try{
                     baiduJson=JSON.parse(d.responseText);
                 }catch(e){
+                    setSearchState("未找到原图");
+                    setTimeout(function(){
+                        setSearchState("");
+                    },2000);
                     if(noneResult)noneResult();
                     return;
                 }
@@ -8270,6 +8274,10 @@ background-image:url("'+ prefs.icons.magnifier +'");\
                     setSearchState("百度识图结束，共找到"+srcs.length+"张匹配图片");
                     callBack(srcs, 2);
                 }else{
+                    setSearchState("未找到原图");
+                    setTimeout(function(){
+                        setSearchState("");
+                    },2000);
                     if(noneResult)noneResult();
                     return;
                 }
