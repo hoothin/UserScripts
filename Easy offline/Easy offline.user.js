f0b39cdd89eeddf0a54c5f5a75c0d6d6be1be632
hoothin <rixixi@gmail.com>
2016-12-20 19:23:51 +0800

    fix

--- Easy offline/Easy offline.user.js
+++ Easy offline/Easy offline.user.js
@@ -8,7 +8,7 @@
 // @namespace    http://tampermonkey.net/
 // @require      https://cdn.jsdelivr.net/jquery/1.7.2/jquery.min.js
 // @require      https://cdn.jsdelivr.net/hi-base64/0.2.0/base64.min.js
-// @version      1.2.2
+// @version      1.2.3
 // @author       Hoothin
 // @mail         rixixi@gmail.com
 // @include      http*://*/*
@@ -362,15 +362,15 @@
             break;
         }
     }
-    if(isDisk){
+    if(location.href.indexOf("pan.baidu.com/wap/home") != -1){
+        if(curlink)location.href="https://pan.baidu.com/disk/home";
+    }else if(location.href.indexOf("github.com/hoothin/UserScripts/tree/master/Easy%20offline") != -1){
+        setting();
+    }else if(isDisk){
         if(curlink){
             diskFun();
             GM_deleteValue('eoUrl');
         }
-    }else if(location.href.indexOf("pan.baidu.com/wap/home") != -1){
-        if(curlink)location.href="https://pan.baidu.com/disk/home";
-    }else if(location.href.indexOf("github.com/hoothin/UserScripts/tree/master/Easy%20offline") != -1){
-        setting();
     }else{
         var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
         var observer = new MutationObserver(function(records){
