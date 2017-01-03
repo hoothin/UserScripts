20172f407c2b5fa459ea6188721a21990b44d503
hoothin <rixixi@gmail.com>
2017-01-03 22:31:09 +0800

    fix

--- HacgGodTurn/HacgGodTurn.user.js
+++ HacgGodTurn/HacgGodTurn.user.js
@@ -4,9 +4,9 @@
 // @name:zh-TW  琉神轉
 // @name:ja     琉璃神社工具セット
 // @namespace   hoothin
-// @description         琉璃神社白科技工具集，支持诸多绅士站（灵梦御所、纯爱计划、绅士二次元、萌心次元、次元轨迹、ACG调查小队、幻天领域、天使二次元、樱花漫舍、风铃窝、次元の圣光、爱弹幕、幻想次元、司机会所、里番萌、最ACG、绅士仓库、绅士图书馆、梦幻二次元、ACG和谐区/里世界、寂月神社、萌幻之乡、绅士之庭、萌口组、九妖萌、CE家族社、喵窝、次元老司机、绅士ACG社等），神秘代码转换成下载链接，百度云网盘自动填写提取密码，F8、Shift+F8站点切换，Alt+F8列表浏览，左右方向键文章跳转，Ctrl+左右快捷翻页，Ctrl+上下跳入跳出，下载链接嗅探，绕过重定向跳转，各种和谐补丁
+// @description         琉璃神社老司机工具箱，支持诸多绅士站（灵梦御所、纯爱计划、绅士二次元、萌心次元、次元轨迹、ACG调查小队、幻天领域、天使二次元、樱花漫舍、风铃窝、次元の圣光、爱弹幕、幻想次元、司机会所、里番萌、最ACG、绅士仓库、绅士图书馆、梦幻二次元、ACG和谐区/里世界、寂月神社、萌幻之乡、绅士之庭、萌口组、九妖萌、CE家族社、喵窝、次元老司机、绅士ACG社等），神秘代码转换成下载链接，百度云网盘自动填写提取密码，F8、Shift+F8站点切换，Alt+F8列表浏览，左右方向键文章跳转，Ctrl+左右快捷翻页，Ctrl+上下跳入跳出，下载链接嗅探，绕过重定向跳转，各种和谐补丁
 // @description:en      For glazed shrine and other adult gentleman sites (Reimu imperial, pure love plan, gentleman two dimension, dimension and dimension trajectory, adorable heart ACG survey team, the magic day in the field, light agency, two dimensional, adorable Angel Sakura diffuse homes, Shengguang, love nest, Campanula dimension barrage, fantasy element, our opportunities, some adorable) mysterious code into the download link, F8 & shift+F8 switching sites, Baidu disk automation, harmonious patch
-// @description:zh-TW   琉璃神社白科技工具集，支持諸多紳士站（靈夢禦所、純愛計劃、紳士二次元、萌心次元、次元軌跡、ACG調查小隊、幻天領域、天使二次元、櫻花漫舍、風鈴窩、次元の聖光、愛彈幕、幻想次元、司機會所、裏番萌、最ACG、紳士倉庫、紳士圖書館、夢幻二次元、ACG和諧區/裏世界、寂月神社、萌幻之鄕、紳士の庭、萌口組、九妖萌、CE家族社、喵窩、次元老司機、紳士ACG社等），神秘代碼轉換成下載鏈接，百度雲網盤自動填寫提取密碼，F8、Shift+F8站點切換，Alt+F8列表瀏覽，左右方向鍵文章跳轉，Ctrl+左右快捷翻頁，Ctrl+上下跳入跳出，下載鏈接嗅探，繞過重定向跳轉，各種和諧補丁
+// @description:zh-TW   琉璃神社老司機工具箱，支持諸多紳士站（靈夢禦所、純愛計劃、紳士二次元、萌心次元、次元軌跡、ACG調查小隊、幻天領域、天使二次元、櫻花漫舍、風鈴窩、次元の聖光、愛彈幕、幻想次元、司機會所、裏番萌、最ACG、紳士倉庫、紳士圖書館、夢幻二次元、ACG和諧區/裏世界、寂月神社、萌幻之鄕、紳士の庭、萌口組、九妖萌、CE家族社、喵窩、次元老司機、紳士ACG社等），神秘代碼轉換成下載鏈接，百度雲網盤自動填寫提取密碼，F8、Shift+F8站點切換，Alt+F8列表瀏覽，左右方向鍵文章跳轉，Ctrl+左右快捷翻頁，Ctrl+上下跳入跳出，下載鏈接嗅探，繞過重定向跳轉，各種和諧補丁
 // @description:ja      琉璃神社工具セット、秋の名山老運転手専用
 // @author      hoothin
 // @icon        https://www.hacg.fi/favicon.ico
@@ -75,7 +75,7 @@
 // @include     http*://www.l-sj.cc/*
 // @include     http*://htacg.cc/*
 // @include     http*://www.htacg.cc/*
-// @version     3.21.19
+// @version     3.21.20
 // @grant       GM_notification
 // @grant       GM_xmlhttpRequest
 // @grant       GM_setClipboard
@@ -546,10 +546,21 @@
                     changeUrl(true,[["a"],[['http:(.*hacg)','https:$1']]]);
                 }
                 if(/hacg\.(ch|la|at|tw)/.test(location.href)){
-                    document.title = document.title.replace(/\u7409\u7483\u795e\u793e/,"\u6d41\u5229\u4f38\u820c");
+                    document.title = document.title.replace(/\u7409\u7483\u795e\u793e/,"\u674e\u9b3c\u795e\u793e");
                     [].forEach.call(document.querySelectorAll("a"), function(item, index, arr) {
-                        item.innerHTML=item.innerHTML.replace(/\u7409\u7483\u795e\u793e/,"\u6d41\u5229\u4f38\u820c");
+                        item.innerHTML=item.innerHTML.replace(/\u7409\u7483\u795e\u793e/,"\u674e\u9b3c\u795e\u793e");
                     });
+                }
+                var tags=document.querySelectorAll("article>footer>a");
+                for(i=0;i<tags.length;i++){
+                    var tag=tags[i];
+                    if(tag.innerHTML == "\u4f2a\u5a18" || tag.innerHTML == "\u53ef\u7231\u7684\u7537\u5b69\u5b50" || tag.innerHTML == "\u5973\u88c5" || tag.innerHTML == "\u7537\u306e\u5a18"){
+                        var articleTitle=document.querySelector(".entry-title");
+                        if(articleTitle){
+                            articleTitle.innerHTML="<font color='red' title='！\u53cd\u9e21\u590d\u5976！'>\u2642\u8def\u897f\u6cd5\u2642</font> "+articleTitle.innerHTML;
+                        }
+                        break;
+                    }
                 }
                 var embeds=document.querySelectorAll(".wp-embedded-content");
                 for(i=0;i<embeds.length;i++){
