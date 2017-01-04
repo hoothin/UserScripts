6d22efe0dd457449523764a64a585245f756b11f
hoothin <rixixi@gmail.com>
2017-01-04 17:21:10 +0800

    fix

--- JiandanHero/JiandanHero.user.js
+++ JiandanHero/JiandanHero.user.js
@@ -3,7 +3,7 @@
 // @name:en      Jiandan Hero
 // @name:zh-TW   煎蛋俠
 // @namespace    hoothin
-// @version      1.1
+// @version      1.2
 // @description  为煎蛋jandan.net提供左右方向键快捷翻页、鼠标悬停显示大图、屏蔽指定用户发言等功能
 // @description:en  Tools for jandan.net
 // @description:zh-TW  為煎蛋jandan.net提供左右方向鍵快捷翻頁、鼠標懸停顯示大圖、屏蔽指定用戶發言等功能
@@ -16,6 +16,23 @@
 
 (function() {
     'use strict';
+    var timer,getImgWH=function(img,callBack){
+        if(timer)clearInterval(timer);
+        var check=function(){
+            if(img.width>0 || img.height>0){
+                if(timer)clearInterval(timer);
+                callBack();
+                return true;
+            }else{
+                return false;
+            }
+        };
+        var geted=check();
+        if(!geted){
+            timer = setInterval(check,40);
+        }
+    };
+
     document.addEventListener("keydown", function(e) {
         if(/INPUT|TEXTAREA/.test(document.activeElement.tagName))return;
         switch(e.keyCode){
@@ -30,6 +47,7 @@
         }
     });
     var authors=document.querySelectorAll("div.author"),i;
+    var isHttps=location.protocol=="https:";
     for(i=0;i<authors.length;i++){
         let author=authors[i];
         let authorId=author.querySelector("strong").title.replace(/防伪码：/,"");
@@ -68,22 +86,27 @@
             }
         };
     }
-    var imgs=document.querySelectorAll("img");
+    var imgs=document.querySelectorAll("img"),left,top,src;
     var bigImg=document.createElement("img");
     bigImg.style.cssText="pointer-events: none;position:fixed;z-index:999";
     for(i=0;i<imgs.length;i++){
-        let img=imgs[i],left,top;
+        let img=imgs[i];
         img.onmouseover=function(e){
-            bigImg.src=img.src.replace(/\b(custom|square)\b/,"medium").replace(/\.sinaimg\.cn\/mw600/,".sinaimg.cn/large");
+            src=img.src.replace(/\b(custom|square)\b/,"medium").replace(/\.sinaimg\.cn\/(mw600|thumb\d+)/,".sinaimg.cn/large");
+            src=isHttps?src.replace(/http\:\/\//,"https://"):src.replace(/https\:\/\//,"http://");
+            bigImg.src=img.src;
+            bigImg.src=src;
             document.body.appendChild(bigImg);
-            bigImg.onload=function(){
+            relocBigImg(left, top);
+            getImgWH(bigImg,function(){
                 relocBigImg(left, top);
-            };
+            });
         };
         img.onmouseout=function(e){
             document.body.removeChild(bigImg);
             bigImg.removeAttribute("height");
             bigImg.removeAttribute("width");
+            bigImg.src=null;
         };
         img.onmousemove=function(e){
             left=e.clientX;
@@ -94,6 +117,30 @@
             relocBigImg(left, top);
         };
     }
+    $("p").on("mouseover","div.gif-mask",function(e){
+        src=this.previousSibling.getAttribute("org_src").replace(/\b(custom|square)\b/,"medium").replace(/\.sinaimg\.cn\/(mw600|thumb\d+)/,".sinaimg.cn/large");
+        src=isHttps?src.replace(/http\:\/\//,"https://"):src.replace(/https\:\/\//,"http://");
+        bigImg.src=this.previousSibling.src;
+        bigImg.src=src;
+        document.body.appendChild(bigImg);
+        getImgWH(bigImg,function(){
+            relocBigImg(left, top);
+        });
+    });
+    $("p").on("mouseout","div.gif-mask",function(e){
+        document.body.removeChild(bigImg);
+        bigImg.removeAttribute("height");
+        bigImg.removeAttribute("width");
+        bigImg.src=null;
+    });
+    $("p").on("mousemove","div.gif-mask",function(e){
+        left=e.clientX;
+        top=e.clientY;
+        if(!bigImg.src || bigImg.src===""){
+            this.onmouseover(null);
+        }
+        relocBigImg(left, top);
+    });
     function relocBigImg(left, top){
         var type=bigImg.height/bigImg.width>document.documentElement.clientHeight/document.documentElement.clientWidth;
         if(type && bigImg.height>document.documentElement.clientHeight){
