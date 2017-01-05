22505752a0a23231616313c1211a669bba323a49
hoothin <rixixi@gmail.com>
2017-01-05 16:50:28 +0800

    fix

--- Highlight Every Code/Highlight Every Code.user.js
+++ Highlight Every Code/Highlight Every Code.user.js
@@ -5,8 +5,8 @@
 // @namespace    hoothin
 // @version      1.0
 // @description  Add a icon to popup a window that allows syntax highlighting and beautify and word count of source code snippets on current page
-// @description:zh-CN 选择代码片段后点击图标弹出新窗口显示高亮美化与格式化后的代码与字数统计
-// @description:zh-TW 選擇程式碼片段後點選圖示彈出新視窗顯示高亮美化與格式化後的程式碼與字數統計
+// @description:zh-CN 选择代码片段后点击图标弹出新窗口显示语法高亮美化与格式化后的代码与字数统计
+// @description:zh-TW 選擇程式碼片段後點選圖示彈出新視窗顯示語法高亮美化與格式化後的程式碼與字數統計
 // @author       Hoothin
 // @grant        GM_openInTab
 // @compatible   chrome
