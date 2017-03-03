d1e8053f3a536462737541f61aefcb6fabc1936f
hoothin <rixixi@gmail.com>
2017-03-03 19:53:15 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2017.3.2.1
+// @version        2017.3.3.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -554,6 +554,8 @@
                 }
                 if(/\.sinaimg\.cn\/mw\d+\//.test(oldsrc)){
                     newsrc=oldsrc.replace(/\/mw\d+\//,"/large/");
+                }else if(/gravatar.com\/avatar\/.*\?s=/.test(oldsrc)){
+                    newsrc=oldsrc.replace(/(gravatar.com\/avatar\/.*\?s=).*/,"$1100000");
                 }
                 return oldsrc != newsrc ? newsrc : null;
             }
