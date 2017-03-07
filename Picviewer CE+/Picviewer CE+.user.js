6a26cb5187149594c01484e019c0e010cb49ba7f
hoothin <rixixi@gmail.com>
2017-03-07 17:16:34 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2017.3.7.1
+// @version        2017.3.7.2
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -8418,9 +8418,9 @@ background-image:url("'+ prefs.icons.magnifier +'");\
                         let imgs=googleHtml.querySelectorAll("div.rg_meta");
                         if(imgs.length==0){searchNext();return;}
                         srcs=[];
-                        for(let img of imgs){
+                        for(var i=0;i<imgs.length;i++){
                             if(srcs.length>2)break;
-                            let jsonData=JSON.parse(img.innerHTML);
+                            let jsonData=JSON.parse(imgs[i].innerHTML);
                             srcs.push(jsonData.ou);
                         }
                         setSearchState("谷歌识图结束，共找到"+srcs.length+"张匹配图片");
@@ -8439,9 +8439,9 @@ background-image:url("'+ prefs.icons.magnifier +'");\
                 let searchImg=tineyeHtml.querySelectorAll("div.matches>div>div>div>div:first-of-type>p.hidden-xs:first-of-type>a");
                 if(searchImg.length>0){
                     srcs=[];
-                    for(let img of searchImg){
+                    for(var i=0;i<searchImg.length;i++){
                         if(srcs.length>2)break;
-                        srcs.push(img.href);
+                        srcs.push(searchImg[i].href);
                     }
                     setSearchState("Tineye识图结束，共找到"+srcs.length+"张匹配图片");
                     callBackFun(srcs);
