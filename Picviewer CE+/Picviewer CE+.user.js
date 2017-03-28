8a0fe65219264d7a067ff845eb4a5f2d5e970aee
hoothin <rixixi@gmail.com>
2017-03-28 10:20:15 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2017.3.25.1
+// @version        2017.3.28.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -307,8 +307,13 @@
              url:/^https?:\/\/[^.]*\.deviantart\.com/i,
              getImage:function(img, a){
                  var oldsrc=this.src;
-                 var newsrc = a.getAttribute('data-super-img') || a.getAttribute('data-super-full-img')
-                 || oldsrc.replace(/(http:\/\/[^\/]+\/fs\d+\/)200H\/(.*)/i,'$1$2');
+                 var newsrc;
+                 if(this.className=="fakethumb" && this.parentNode && this.parentNode.parentNode && this.parentNode.parentNode.parentNode && this.parentNode.parentNode.parentNode.dataset){
+                     newsrc=this.parentNode.parentNode.parentNode.dataset.superFullImg;
+                 }
+                 if(!newsrc)
+                     newsrc = a.getAttribute('data-super-img') || a.getAttribute('data-super-full-img')
+                         || oldsrc.replace(/(http:\/\/[^\/]+\/fs\d+\/)200H\/(.*)/i,'$1$2');
                  return newsrc==oldsrc? '' : newsrc;
              },
             },
@@ -542,6 +547,12 @@
              url: /(greasyfork|sleazyfork)\.org/,
              getImage: function() {
                  return this.src.replace(/\/thumb\//i,"/original/").replace(/\/thumbnails\//i,"/");
+             }
+            },
+            {name: "dribbble",
+             url: /dribbble\.com/,
+             getImage: function() {
+                 return this.src.replace(/_teaser(.[^\.]+)$/i,"$1");
              }
             }
         ];
@@ -7969,6 +7980,23 @@ background-image:url("'+ prefs.icons.magnifier +'");\
             //      }
             //  }
             // }
+            if (target.nodeName != 'IMG'){
+                var imgChild=target.querySelectorAll('img');
+                if(imgChild.length==1){
+                    target=imgChild[0];
+                }else if(target.parentNode){
+                    if(target.parentNode.nodeName=='IMG'){
+                        target=target.parentNode;
+                    }else{
+                        if(unsafeWindow.getComputedStyle(target).position=="absolute"){
+                            imgChild=target.parentNode.querySelectorAll('img');
+                            if(imgChild.length==1){
+                                target=imgChild[0];
+                            }
+                        }
+                    }
+                }
+            }
 
             if (!target || !result && target.nodeName != 'IMG') return;
 
