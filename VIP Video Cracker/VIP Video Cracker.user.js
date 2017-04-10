c3c7273955fcfe40c57e5ae8cfc737fc0490e256
hoothin <rixixi@gmail.com>
2017-04-10 21:20:23 +0800

    sort https site

--- VIP Video Cracker/VIP Video Cracker.user.js
+++ VIP Video Cracker/VIP Video Cracker.user.js
@@ -2,7 +2,7 @@
 // @name         VIP视频破解
 // @name:en      VIP Video Cracker
 // @namespace    hoothin
-// @version      1.6.3
+// @version      1.6.5
 // @description  解析并破解各大视频站的VIP权限
 // @description:en  Crack VIP permissions of some chinese video sites
 // @author       hoothin
@@ -42,38 +42,38 @@
     'use strict';
     var cracks=[
         {name:"47影视云",url:"https://api.47ks.com/webcloud/?v=%s",title:"首选"},
-        {name:"无名小站1",url:"http://www.wmxz.wang/video.php?url=%s",title:"次选"},
-        {name:"无名小站2",url:"http://www.sfsft.com/admin.php?url=%s",title:"无名小站的源码"},
-        {name:"小海解析1",url:"https://ckplaer.duapp.com/hai.php?url=%s",title:"播放器似乎放在百度开发者平台"},
+        {name:"无名小站",url:"http://www.wmxz.wang/video.php?url=%s",title:"次选"},
+        {name:"小海解析1(s)",url:"https://ckplaer.duapp.com/hai.php?url=%s",title:"播放器似乎放在百度开发者平台"},
         {name:"小海解析2",url:"http://jx.ck921.com/hai.php?url=%s",title:"和上面的用的应该是同样的服务器"},
-        {name:"VIP看看",url:"http://2.jx.72du.com/video.php?url=%s",title:"无名小站的源码"},
-        {name:"歪歪电影",url:"http://www.yydy8.com/common/?url=%s"},
-        {name:"石头解析",url:"https://jiexi.071811.cc/jx.php?url=%s"},
-        {name:"10号影院",url:"http://player.gakui.top/?url=%s"},
+        {name:"石头解析(s)",url:"https://jiexi.071811.cc/jx.php?url=%s"},
+        {name:"爱看TV(s)",url:"https://aikan-tv.com/tong.php?url=%s"},
+        {name:"最小品(s)",url:"https://www.zuixiaopin.com/api/cloudVideo?url=%s"},
+        {name:"妹儿云(s)",url:"https://www.yymeier.com/api.php?url=%s"},
+        {name:"那片(s)",url:"https://jxapi.nepian.com/ckparse/?url=%s"},
+        {name:"眼睛会下雨(s)",url:"https://www.vipjiexi.com/yun.php?url=%s"},
         {name:"Relon",url:"http://yyygwz.com/index.php?url=%s"},
-        {name:"爱看TV",url:"http://aikan-tv.com/tong.php?url=%s"},
         {name:"SO视频",url:"http://parse.colaparse.cc/?url=%s"},
-        {name:"最小品",url:"https://www.zuixiaopin.com/api/cloudVideo?url=%s"},
-        {name:"PPYPP",url:"http://www.ppypp.com/yunparse/?url=%s"},
-        {name:"梦中人",url:"http://www.wpswan.com/mzr/vipparse/index.php?url=%s"},
-        {name:"言朋影院",url:"http://xin.yingyanxinwen.cn/dxl/?url=%s"},
-        {name:"妹儿云",url:"https://www.yymeier.com/api.php?url=%s"},
-        {name:"那片",url:"https://jxapi.nepian.com/ckparse/?url=%s"},
+        {name:"言朋影院",url:"http://vip.yingyanxinwen.cn/vip/index.php?url=%s"},
         {name:"5奇异",url:"http://www.jiexi.cx/5qiyi/?url=%s"},
         {name:"Moondown",url:"http://moon.moondown.net/?url=%s"},
         {name:"选片网",url:"http://jx.xuanpianwang.com/parse?url=%s"},
         {name:"云上",url:"http://www.ou522.cn/t2/1.php?url=%s"},
         {name:"强强卷",url:"http://000o.cc/jx/ty.php?url==%s"},
         {name:"Lewei369",url:"http://s1y2.com/?url=%s"},
-        {name:"紫狐云",url:"http://yun.zihu.tv/play.html?url=%s"},
-        {name:"眼睛会下雨",url:"http://www.vipjiexi.com/yun.php?url=%s"},
+        {name:"紫狐云",url:"http://yun.zihu.tv/api.php?url=%s"},
         {name:"土豪网",url:"http://www.tuhao13.com/yunparse/index.php?url=%s"},
         {name:"舞动秋天",url:"http://qtzr.net/s/?qt=%s"},
-        {name:"CloudParse",url:"http://api.cloudparse.com/?url=%s"},
-        {name:"迷失之梦",url:"http://mt2t.com/yun?url=%s",title:"这个解析站似乎不大稳定"},
-        {name:"疯狂解析",url:"http://vip.ifkdy.com/?url=%s",title:"仅是简单嵌了47影视云、小海解析等几个解析站"},
         {name:"97在线看",url:"http://www.97zxkan.com/jiexi/97zxkanapi.php?url=%s"},
+        {name:"无名小站源",url:"http://www.sfsft.com/admin.php?url=%s",title:"无名小站的源"},
+        {name:"疯狂解析",url:"http://vip.ifkdy.com/?url=%s",title:"仅是简单嵌了47影视云、小海解析等几个解析站"},
+        {name:"VIP看看",url:"http://2.jx.72du.com/video.php?url=%s",title:"嵌了无名小站的服务"},
+        {name:"歪歪电影",url:"http://www.yydy8.com/common/?url=%s",title:"嵌了47影视云的服务"},
+        {name:"梦中人",url:"http://www.wpswan.com/mzr/vipparse/index.php?url=%s",title:"嵌了47影视云的服务"},
+        {name:"迷失之梦",url:"http://mt2t.com/yun?url=%s",title:"这个解析站似乎不大稳定"},
         {name:"71ki解析",url:"http://jx.71ki.com/tong.php?url=%s"},
+        {name:"CloudParse",url:"http://api.cloudparse.com/?url=%s"},
+        {name:"10号影院",url:"http://player.gakui.top/?url=%s"},
+        {name:"PPYPP",url:"http://www.ppypp.com/yunparse/?url=%s"},
         //{name:"百域阁",url:"http://api.svip.baiyug.cn/svip/index.php?url=%s"},
     ],video,videoWidth,videoHeight,i=0;
     var iqiyi=location.hostname.indexOf("iqiyi.com")!=-1;
@@ -83,7 +83,7 @@
     var vipVideoCrackUrl=GM_getValue("vipVideoCrackUrl");
     var iframe=document.createElement("iframe");
     var selectStyle=document.createElement("style");
-    selectStyle.innerHTML=".crackJump{font-size:12px;margin-left:5px;color:white;text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;-webkit-text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;-moz-text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;*filter: Glow(color=#000, strength=1);}.crackJump input{vertical-align:middle;}.vipSelect{background:black;color:white;font-size:12px;border:none;}.crackArea{position:absolute;z-index:999999;left:0px;top:0px;opacity:0.50;filter:alpha(opacity=50);transition:opacity 0.3s ease,width 0.3s ease;width:18px;overflow:hidden;white-space:nowrap;border:1px solid #666;background:black;}.crackArea:hover{opacity:1;filter:alpha(opacity=100);width:230px;}.crackArea>p{display:block;font-size:13px}.crackArea:hover>p{display:none;}.crackArea>label,.crackArea>select{display:none;}.crackArea:hover>label,.crackArea:hover>select{display:initial;}";
+    selectStyle.innerHTML=".crackJump{font-size:12px;margin-left:5px;color:white;text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;-webkit-text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;-moz-text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;*filter: Glow(color=#000, strength=1);}.crackJump input{vertical-align:middle;}.vipSelect{background:black;color:white;font-size:12px;border:none;}.crackArea{position:absolute;z-index:999999;left:0px;top:0px;opacity:0.50;filter:alpha(opacity=50);transition:opacity 0.3s ease,width 0.3s ease;width:18px;overflow:hidden;white-space:nowrap;border:1px solid #666;background:black;}.crackArea:hover{opacity:1;filter:alpha(opacity=100);width:230px;}.crackArea>p{display:block;font-size:13px;text-align:center;}.crackArea:hover>p{display:none;}.crackArea>label,.crackArea>select{display:none;}.crackArea:hover>label,.crackArea:hover>select{display:initial;}";
     document.getElementsByTagName("head")[0].appendChild(selectStyle);
     var placeholder=document.createElement("div");
     placeholder.style.cssText="width:100%;height:100%;text-align:center;font-size:x-large;cursor:pointer;color:#666;";
