4bb76c18539e967e0bf23b51018033b096a1d35a
hoothin <rixixi@gmail.com>
2017-04-18 09:58:47 +0800

    setTimeout only transition

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2017.4.17.1
+// @version        2017.4.18.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -2473,10 +2473,12 @@ padding-left:24px;">'+shareItem.name+'</span>');
                 var canScroll=true;
                 var scrollToChange=function(next){
                     if(canScroll){
-                        canScroll=false;
-                        setTimeout(function(){
-                            canScroll=true;
-                        },500);
+                        if(prefs.gallery.transition){
+                            canScroll=false;
+                            setTimeout(function(){
+                                canScroll=true;
+                            },500);
+                        }
                         next ? self.selectNext() : self.selectPrevious();
                     }
                 }
