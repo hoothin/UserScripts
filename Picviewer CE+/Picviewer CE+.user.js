3d32a91f127e84e78567d09cd70ccf80daecba92
hoothin <rixixi@gmail.com>
2017-10-13 06:54:38 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2017.10.12.2
+// @version        2017.10.13.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -599,7 +599,7 @@
             {name: "Acgget",
              url: /acg18\.us|acgget\./,
              getImage: function() {
-                 return this.src.replace(/(pic\.acgget\.com\/thumb\/)w\d+_h\d+\//i,"$1w9999_h9999/");
+                 return this.getAttribute('data-original') || this.src.replace(/(pic\.acgget\.com\/thumb\/)w\d+_h\d+\//i,"$1w9999_h9999/");
              }
             },
             {name: "Pixiv",
@@ -803,7 +803,7 @@
             // GithubAvatars
             {name: "GithubAvatars",
              d: "github.com",
-             r: /(avatars\d*\.githubusercontent\.com.*)\?.*$/i,
+             r: /(avatars\d+\.githubusercontent\.com.*)\?.*$/i,
              s: "$1",
              example: "https://avatars2.githubusercontent.com/u/3233275/",
             },
