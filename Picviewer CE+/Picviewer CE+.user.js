bdefe6f11ee78eecabc778190e3418ada1849bf8
hoothin <rixixi@gmail.com>
2017-10-25 06:45:30 +0800

    add pixlr

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2017.10.13.1
+// @version        2017.10.25.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -1818,6 +1818,7 @@
                     '<span class="pv-gallery-vertical-align-helper"></span>'+
                     '</span>'+
                     '<span class="pv-gallery-head-command-drop-list pv-gallery-head-command-drop-list-others">'+
+                    '<span class="pv-gallery-head-command-drop-list-item" data-command="psImage" title="使用Pixlr在线编辑该图片">在线编辑</span>'+
                     '<span class="pv-gallery-head-command-drop-list-item" data-command="openInNewWindow" title="新窗口打开图片">新窗口打开</span>'+
                     '<span class="pv-gallery-head-command-drop-list-item" data-command="scrollIntoView" title="滚动到当前图片所在的位置">定位到图片</span>'+
                     '<span class="pv-gallery-head-command-drop-list-item" data-command="enterCollection" title="查看所有收藏的图片">查看收藏</span>'+
@@ -2404,6 +2405,9 @@
                     switch(command){
                         case 'openInNewWindow':{
                             window.open(self.src,'_blank');
+                        }break;
+                        case 'psImage':{
+                            window.open('https://pixlr.com/editor/?image='+self.src,'_blank');
                         }break;
                         case 'scrollIntoView':{
                             if(collection.mMode){
