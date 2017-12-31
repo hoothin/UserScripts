dec42857f6a82d42bcb14f4fadc71b89bb66ab82
hoothin <rixixi@gmail.com>
2018-01-01 00:40:57 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2017.12.4.1
+// @version        2018.1.1.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -1011,7 +1011,7 @@
         // 通配型规则,无视站点.
         var tprules=[
             function(img, a) { // 解决新的dz论坛的原图获取方式.
-                var regs = [/(.+\/attachments?\/.+)\.thumb\.\w{2,5}$/i,/(wp-content\/uploads\/.*)\-\d+x\d+/i,/.*(?:url|src)=(https?:\/\/.*\.(?:jpg|jpeg|png|gif|bmp)).*/i,/.*thumb\.php\?src=([^&]*).*/i];
+                var regs = [/(.+\/attachments?\/.+)\.thumb\.\w{2,5}$/i,/((wp-content|moecdn\.org)\/uploads\/.*)\-\d+x\d+/i,/.*(?:url|src)=(https?:\/\/.*\.(?:jpg|jpeg|png|gif|bmp)).*/i,/.*thumb\.php\?src=([^&]*).*/i];
                 var oldsrc = this.src,newsrc = this.src;
                 if (oldsrc){
                     for(let reg of regs){
@@ -1020,7 +1020,7 @@
                         }
                     }
                 }
-                var original=this.getAttribute('data-original');
+                var original=this.getAttribute?this.getAttribute('data-original'):"";
                 if(original && original!=oldsrc)return original;
                 else if(/\.sinaimg\.(cn|com)\/mw\d+\//.test(oldsrc)){
                     newsrc=oldsrc.replace(/.*(https?:\/\/[^\.]+\.sinaimg\.(cn|com))\/mw\d+\//,"$1/large/");
@@ -8662,12 +8662,25 @@ left: -45px;\
             if (target.nodeName != 'IMG' && typeof target.className === 'string' && target.className.indexOf("pv-float-bar")==-1){
                 var targetStyle=getComputedStyle(target);
                 if(prefs.floatBar.listenBg && targetStyle.backgroundImage && /^url/.test(targetStyle.backgroundImage) && targetStyle.width.replace("px","")>prefs.floatBar.minSizeLimit.w && targetStyle.height.replace("px","")>prefs.floatBar.minSizeLimit.h){
-                    var src=targetStyle.backgroundImage.replace(/url\(["'](.*)["']\)/,"$1");
+                    var src=targetStyle.backgroundImage.replace(/url\(["'](.*)["']\)/,"$1"),nsrc=src,noActual=true,type="scale";
+                    var img={src:src};
+                    tprules._find(function(rule,index,array){
+                        try{
+                            nsrc=rule.call(img,img,null);
+                            if(src){
+                                type="tpRule";
+                                noActual=false;
+                                return true;
+                            };
+                        }catch(err){
+                            throwErrorInfo(err);
+                        };
+                    });
                     result = {
-                        src: src,
-                        type: "scale",
+                        src: nsrc,
+                        type: type,
                         imgSrc: src,
-                        noActual:true,
+                        noActual:noActual,
                         img: target
                     };
                 }else if(target.children.length<=2 && target.querySelectorAll("img").length==1){
