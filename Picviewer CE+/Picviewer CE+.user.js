1bd5487c5e813610df68d193fb2647b2d7b687f5
hoothin <rixixi@gmail.com>
2018-01-19 23:58:01 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2018.1.4.1
+// @version        2018.1.19.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -1022,7 +1022,9 @@
                 }
                 var original=this.getAttribute?this.getAttribute('data-original'):"";
                 if(original && original!=oldsrc)return original;
-                else if(/\.sinaimg\.(cn|com)\/mw\d+\//.test(oldsrc)){
+                else if(/attachment\.php\?attachmentid=\d+/.test(oldsrc)){
+                     newsrc=oldsrc.replace(/(attachment\.php\?attachmentid=\d+).*/,"$1");
+                }else if(/\.sinaimg\.(cn|com)\/mw\d+\//.test(oldsrc)){
                     newsrc=oldsrc.replace(/.*(https?:\/\/[^\.]+\.sinaimg\.(cn|com))\/mw\d+\//,"$1/large/");
                 }else if(/gravatar\.com\/avatar\/.*[\?&]s=/.test(oldsrc)){
                     newsrc=oldsrc.replace(/(gravatar\.com\/avatar\/.*[\?&]s=).*/,"$1500");
@@ -1103,7 +1105,7 @@
             },
             {name: "ali2",
              r: /(.*\.alicdn\.com\/.*?)((\.|_)\d+x\d+.*|\.search)\.jpg(_\.webp)?$/,
-             s: "$1.jpg"
+             s: "$1_800x800.jpg"
             },
             {name: "1号店",
              r: /(.*\.yihaodianimg\.com\/.*)_\d+x\d+\.jpg$/,
