5827541a011e30ab2883445dfaed3cb717758a45
hoothin <rixixi@gmail.com>
2018-04-03 11:41:26 +0800

    fix
    b站前端简直是。。

--- Bilibili Bangumi Cover/Bilibili Bangumi Cover.user.js
+++ Bilibili Bangumi Cover/Bilibili Bangumi Cover.user.js
@@ -1,22 +1,24 @@
 // ==UserScript==
 // @name         Bilibili Bangumi Cover
-// @name:zh-CN   哔哩哔哩(bilibili.com)番剧封面
+// @name:zh-CN   哔哩哔哩番剧封面
 // @namespace    hoothin
-// @version      0.6
+// @version      0.7
 // @description        Show Bilibili Bangumi Cover
 // @description:zh-CN  在哔哩哔哩番剧页面中显示封面
 // @grant        GM_xmlhttpRequest
 // @author       hoothin
 // @include      http*://bangumi.bilibili.com/anime/*
+// @include      http*://www.bilibili.com/bangumi/play/*
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var vSmall=document.querySelector(".recom");
+    var vSmall=document.querySelector(".bangumi-recom");
+    console.log(vSmall);
     if(!vSmall)return;
-    var title=document.createElement("h3");
+    var title=document.createElement("h4");
     title.id="coverTitle";
-    title.classList.add("v1-bangumi-head-h3");
+    title.classList.add("recom-title");
     title.style.display="block";
     title.innerHTML="封面";
     var coverLink=document.createElement("a");
@@ -24,23 +26,24 @@
     cover.style.width="270px";
     coverLink.target="_blank";
     coverLink.appendChild(cover);
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+    /*var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
     var observer = new MutationObserver(function(records){
-        if(!vSmall.querySelector("h3#coverTitle")){
-            vSmall.insertBefore(coverLink,vSmall.firstChild);
-            vSmall.insertBefore(title,vSmall.firstChild);
-            refreshCover();
-        }
     });
     var option = {
         'childList': true
     };
-    observer.observe(vSmall, option);
+    observer.observe(vSmall, option);*/
+    if(!vSmall.querySelector("h3#coverTitle")){
+        vSmall.insertBefore(coverLink,vSmall.firstChild);
+        vSmall.insertBefore(title,vSmall.firstChild);
+        refreshCover();
+    }
     function refreshCover(){
         GM_xmlhttpRequest({
             method: 'GET',
-            url: "http://bangumi.bilibili.com/web_api/episode/"+location.hash.slice(1)+".json",
+            url: "http://bangumi.bilibili.com/web_api/episode/"+location.href.replace(/.*ep/i,"")+".json",
             onload: function(result) {
+                console.log(11);
                 try {
                     var bangumiData = JSON.parse(result.responseText);
                     coverLink.href=cover.src=bangumiData.result.currentEpisode.cover;
