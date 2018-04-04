469a7fdf84659e50c201da384eb0908a64356e97
hoothin <rixixi@gmail.com>
2018-04-04 15:58:20 +0800

    fix pixiv

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2018.4.3.1
+// @version        2018.4.4.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -982,7 +982,8 @@
             {name: "Pixiv",
              url: /pixiv\.net|pximg\.net/,
              getImage: function() {
-                 return this.src.replace(/pximg\.net\/c\/\d+x\d+\/img-master\/img\/(.*)_[^_]+(\.[a-z]+)$/i,"pximg.net/img-original/img/$1$2");
+                 var preStr=this.src.replace(/pximg\.net\/c\/\d+x\d+\/img-master\/img\/(.*)_[^_]+(\.[a-z]+)$/i,"pximg.net/img-original/img/$1");
+                 return [preStr+".jpg",preStr+".png"];
              }
             },
             {name: "Wallhaven",
