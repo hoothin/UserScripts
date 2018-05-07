aba5f3333f468cc9cb7081cdc986de785a6797c1
hoothin <rixixi@gmail.com>
2018-05-07 13:18:31 +0800

    remove ins

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2018.5.4.1
+// @version        2018.5.7.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -1125,12 +1125,6 @@
              url: /dribbble\.com/,
              getImage: function() {
                  return this.src.replace(/_teaser(.[^\.]+)$/i,"$1");
-             }
-            },
-            {name: "instagram",
-             url: /\.instagram\.com/,
-             getImage: function() {
-                 return this.src.replace(/\/s\d+x\d+\//i,"/");
              }
             },
             {name: "百度百科",
