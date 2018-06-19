ecf57dc600a34ae690d1cc0b2330691bedd61bae
hoothin <rixixi@gmail.com>
2018-06-19 17:24:23 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2018.6.15.1
+// @version        2018.6.19.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -4265,6 +4265,7 @@
                 return {pre:pre,next:next};
             },
             canonicalUri:function(src, base_path){
+                if(src=="#")return location.href;
                 var root_page = /^[^?#]*\//.exec(location.href)[0],
                     root_domain = /^\w+\:\/\/\/?[^\/]+/.exec(root_page)[0],
                     absolute_regex = /^\w+\:\/\//;
@@ -4338,7 +4339,7 @@
                             nimg.src = isrc;
                             nimg.onload=function(){
                                 var result = findPic(this);
-                                if (result) {
+                                if (result && !self._dataCache[this.src]) {
                                     self.data.push(result);
                                     self._appendThumbSpans([result]);
                                     self.loadThumb();
@@ -4418,7 +4419,7 @@
                             nimg.src = isrc;
                             nimg.onload=function(){
                                 var result = findPic(this);
-                                if (result) {
+                                if (result && !self._dataCache[this.src]) {
                                     self.data.push(result);
                                     self._appendThumbSpans([result]);
                                     self.loadThumb();
