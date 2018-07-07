67ffcbb79aaf6f9645e711feda07a2072403daba
hoothin <rixixi@gmail.com>
2018-07-07 13:08:47 +0800

    fix for srcset only

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2018.7.4.1
+// @version        2018.7.7.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -8630,7 +8630,7 @@ left: -45px;\
                 srcs,  // 备用的大图地址
                 type,  // 类别
                 noActual = false, //没有原图
-                imgSrc = img.src,  // img 节点的 src
+                imgSrc = img.src||img.srcset,  // img 节点的 src
                 xhr,
                 description;  // 图片的注释
 
