706b2a1f348f1a50d5fcb7d0bcecde67fd4671d9
hoothin <rixixi@gmail.com>
2018-07-10 12:18:50 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2018.7.7.1
+// @version        2018.7.10.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -3945,18 +3945,18 @@
                     iStatisCopy[item.type].count++;
                     var spanMark=document.createElement("span");
                     thumbnails.appendChild(spanMark);
-                    spanMark.outerHTML = '<span class="pv-gallery-sidebar-thumb-container' +
-                        '" data-type="' + item.type +
-                        '" data-src="' + item.src +
-                        '" data-srcs="' + (item.srcs?item.srcs.join(","):"") +
-                        (item.xhr ? '" data-xhr="' + encodeURIComponent(JSON.stringify(item.xhr)) : '') +
-                        '" data-description="' + encodeURIComponent(item.description || '') +
-                        '" data-thumb-src="' + item.imgSrc +
-                        '" title="' + (item.img?item.img.title:"") +
-                        '">' +
-                        '<span class="pv-gallery-vertical-align-helper"></span>' +
-                        '<span class="pv-gallery-sidebar-thumb-loading" title="'+i18n("loading")+'......"></span>' +
-                        '</span>';
+                    try{
+                        spanMark.className="pv-gallery-sidebar-thumb-container";
+                        spanMark.dataset.type=item.type;
+                        spanMark.dataset.src=item.src;
+                        spanMark.dataset.srcs=item.srcs;
+                        if(item.xhr)spanMark.dataset.xhr=encodeURIComponent(JSON.stringify(item.xhr));
+                        spanMark.dataset.description=encodeURIComponent(item.description || '');
+                        spanMark.dataset.thumbSrc=item.imgSrc;
+                        spanMark.title=(item.img?item.img.title:"");
+                        spanMark.innerHTML='<span class="pv-gallery-vertical-align-helper"></span>' +
+                            '<span class="pv-gallery-sidebar-thumb-loading" title="'+i18n("loading")+'......"></span>';
+                    }catch(e){};
                 });
 
                 var self = this;
