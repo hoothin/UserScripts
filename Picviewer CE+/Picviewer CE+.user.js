e818a025bea9b499ea1793b88239ed0c6c6010cc
hoothin <rixixi@gmail.com>
2018-08-27 17:37:42 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2018.8.1.1
+// @version        2018.8.27.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -813,10 +813,10 @@
             {name:"新浪微博",
              siteExample:"http://weibo.com/pub/?source=toptray",
              enabled:true,
-             url:/^https?:\/\/(?:[^.]+\.)*weibo\.com/i,
+             url:/^https?:\/\/(?:[^.]+\.)*weibo\.(com|cn)/i,
              getImage:function(img){
                  var oldsrc=this.src;
-                 var pic=/(\.sinaimg\.(cn|com)\/)(?:bmiddle)/i;//微博内容图片.
+                 var pic=/(\.sinaimg\.(cn|com)\/)(?:bmiddle|orj360)/i;//微博内容图片.
                  var pic2=/(\.sinaimg\.(cn|com)\/)(?:square|thumbnail)/i;// 微博内容图片2.
                  var head=/(\.sinaimg\.(cn|com)\/\d+)\/50\//i;//头像.
                  var photoList=/\.sinaimg\.(cn|com)\/thumb\d+\/\w+/i//相册
@@ -1197,6 +1197,18 @@
              url: /fanfou\.com/,
              getImage: function() {
                  return this.src.replace(/@.+/i,"");
+             }
+            },
+            {name: "meitudata",
+             url: /meipai\.com/,
+             getImage: function() {
+                 return this.src.replace(/!thumb.+/i,"");
+             }
+            },
+            {name: "mafengwo",
+             url: /mafengwo\.cn/,
+             getImage: function() {
+                 return this.src.replace(/\?imageMogr.*/i,"");
              }
             }
         ];
