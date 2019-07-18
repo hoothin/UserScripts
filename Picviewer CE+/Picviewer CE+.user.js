495b3e298c96f9b6ab7dc1b3d039230484f8226c
hoothin <rixixi@gmail.com>
2019-07-18 11:45:14 +0800

    fix for dribbble&deviantart

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures or find the HD original picture automatically
 // @description:zh-CN    NLF 的围观图修改版，增加高清原图查找显示（在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存、查找原图）
 // @description:zh-TW    NLF 的圍觀圖修改版，增加高清原圖查詢顯示（線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存、查詢原圖）
-// @version        2019.7.12.1
+// @version        2019.7.18.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -878,7 +878,7 @@
              getImage:function(img, a){
                  var oldsrc=this.src;
                  var newsrc;
-                 if(this.className=="fakethumb" && this.parentNode && this.parentNode.parentNode && this.parentNode.parentNode.parentNode && this.parentNode.parentNode.parentNode.dataset){
+                 if(this.parentNode && this.parentNode.parentNode && this.parentNode.parentNode.parentNode && this.parentNode.parentNode.parentNode.dataset && this.parentNode.parentNode.parentNode.dataset.superFullImg){
                      newsrc=this.parentNode.parentNode.parentNode.dataset.superFullImg;
                  }
                  if(!newsrc)
@@ -1128,7 +1128,7 @@
             {name: "dribbble",
              url: /dribbble\.com/,
              getImage: function() {
-                 return this.src.replace(/_teaser(.[^\.]+)$/i,"$1");
+                 return this.src.replace(/_teaser(.[^\.]+)$/i,"$1").replace(/_1x\./,".");
              }
             },
             {name: "百度百科",
