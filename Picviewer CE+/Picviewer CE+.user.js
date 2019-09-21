f7a55daaa64dc0d592346dcf8eae5c14519caa0b
hoothin <rixixi@gmail.com>
2019-09-21 13:39:43 +0800

    fix

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures automatically
 // @description:zh-CN    在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
-// @version        2019.9.19.2
+// @version        2019.9.21.1
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -6324,7 +6324,7 @@ left: -45px;\
                 };
 
                 //是否点击图片外部关闭
-                if(prefs.imgWindow.close.clickOutside.trigger){
+                if(prefs.imgWindow.close.clickOutside){
                     var clickOutside=function(e){
                         var target=e.target;
                         if(!container.contains(target)){
@@ -6332,7 +6332,7 @@ left: -45px;\
                         };
                     };
                     this.clickOutside=clickOutside;
-                    document.addEventListener(prefs.imgWindow.close.clickOutside.trigger,clickOutside,true);
+                    document.addEventListener(prefs.imgWindow.close.clickOutside,clickOutside,true);
                 };
 
                 //是否双击图片本身关闭
@@ -6651,6 +6651,7 @@ left: -45px;\
             },
 
             firstOpen:function(){
+                ImgWindowC.selectedTool='hand';
                 this.focus();
                 var imgWindow=this.imgWindow;
                 var scrolled=getScrolled();
@@ -7692,7 +7693,7 @@ left: -45px;\
 
                 //点击点击外部关闭的监听
                 if(prefs.imgWindow.close.clickOutside.enabled){
-                    document.removeEventListener(prefs.imgWindow.close.clickOutside.trigger,this.clickOutside,true);
+                    document.removeEventListener(prefs.imgWindow.close.clickOutside,this.clickOutside,true);
                 };
 
                 var index=ImgWindowC.all.indexOf(this);
