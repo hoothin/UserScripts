93615fe75e21a7d8dafe51b08679d3ca929c1755
hoothin <rixixi@gmail.com>
2021-12-13 20:00:57 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description    Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures automatically
 // @description:zh-CN    在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
-// @version        2021.12.13.2
+// @version        2021.12.13.6
 // @created        2011-6-15
 // @namespace      http://userscripts.org/users/NLF
 // @homepage       http://hoothin.com
@@ -1301,6 +1301,12 @@
              url: /zhisheji\.com/,
              getImage: function() {
                  return this.src.replace(/thumbnail\/.*/,"");
+             }
+            },
+            {name: "Reddit",
+             url: /reddit\.com|redd\.it/,
+             getImage: function() {
+                 return this.src.replace(/\/\/preview\.redd.it\/([^\?]+)?.*/,"https://i.redd.it/$1");
              }
             }
         ];
