2a1e6cb6a798e8548da5603ee3798392bb59ab6b
hoothin <rixixi@gmail.com>
2021-12-15 16:52:12 +0800

    Update loliconSexPic.user.js

--- LoliconSexPic/loliconSexPic.user.js
+++ LoliconSexPic/loliconSexPic.user.js
@@ -8,7 +8,7 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_addStyle
 // ==/UserScript==
-
+//https://api.lolicon.app/setu/v2?r18=1&num=5
 (function() {
     'use strict';
     GM_addStyle("img{width:100%}");
@@ -17,7 +17,7 @@
 
     datas.forEach(function(data){
         var img=document.createElement("img");
-        img.src=data.urls.original;
+        img.src=data.urls?data.urls.original:data.url;
         document.body.appendChild(img);
     });
 })();
