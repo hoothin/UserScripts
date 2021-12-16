068b77c7ddcc634ecdf20b9eefd7fb66588c0c14
hoothin <rixixi@gmail.com>
2021-12-16 21:12:34 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6875,7 +6875,7 @@
                     background-color: rgba(40,40,40,0.9);\
                     padding: 8px;\
                     border: 5px solid #ccc;\
-                    border-radius: 15px;\
+                    border-radius: 12px;\
                     line-height: 0;\
                     text-align: left;\
                     box-sizing: content-box;\
