6ae332ba32c71de48a0cce00049f6e8cf9f6d7ee
hoothin <rixixi@gmail.com>
2021-12-24 15:53:51 +0800

    Update Disable AD of ThisAV.user.js

--- Disable AD of ThisAV/Disable AD of ThisAV.user.js
+++ Disable AD of ThisAV/Disable AD of ThisAV.user.js
@@ -3,7 +3,7 @@
 // @name:en            Disable AD of ThisAV
 // @name:zh-TW         ThisAV去廣告
 // @namespace          hoothin
-// @version            0.5
+// @version            0.61
 // @description        去除ThisAV页面中的弹出广告与导航栏广告，添加视频下载按钮
 // @description:en     Just disable popup AD & tab menu AD of ThisAV.com
 // @description:zh-TW  去除ThisAV頁面中的彈出廣告與導航欄廣告，添加視頻下載按鈕
@@ -29,12 +29,27 @@
         ratingText.style.width="55px";
         ratingText.style.marginLeft="5px";
         var downBtn=document.createElement("a");
+        downBtn.style.display="flex";
         if(unsafeWindow.so){
             downBtn.href=unsafeWindow.so.variables.file;
-        }else{
+        }else if(unsafeWindow.jwplayer){
             downBtn.href=unsafeWindow.jwplayer("myElement").config.file;
+        }else if(unsafeWindow.videojs){
+            var vplayer = unsafeWindow.videojs('my-video', {
+                techOrder: ['shaka', 'html5'],
+                plugins: {
+                    videoJsResolutionSwitcher: {
+                        default: 'high',
+                        dynamicLabel: true,
+                    },
+                },
+            });
+            var mpd=vplayer.options_.sources[0].src;
+            downBtn.href=mpd.replace(/\.mpd$/,"_dashinit.mp4");
+        }else{
+            downBtn.href=document.querySelector("source").src;
         }
         ratingText.parentNode.insertBefore(downBtn,ratingText.nextSibling);
-        downBtn.innerHTML="<input type='button' value='下載視頻' style='float: left;'></input>";
+        downBtn.innerHTML="<input type='button' value='下載視頻' style='float: left;padding: 2px 5px 2px 5px!important; cursor: pointer;'></input>";
     }
 })();
