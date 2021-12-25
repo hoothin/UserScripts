986d574ef8db18e9056f0a12c18d18f9c7d7100e
hoothin <rixixi@gmail.com>
2021-12-25 16:37:32 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description     Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures automatically
 // @description:zh-CN    在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
-// @version         2021.12.25.1
+// @version         2021.12.25.2
 // @created         2011-6-15
 // @namespace       http://userscripts.org/users/NLF
 // @homepage        http://hoothin.com
@@ -1225,6 +1225,9 @@ Trace Moe | https://trace.moe/?url=#t#`;
             {name: "greasyfork",
              url: /(greasyfork|sleazyfork)\.org/,
              getImage: function() {
+                 if(this.parentNode.nodeName=="A" && /amazonaws\.com/.test(this.parentNode.href)){
+                     return this.parentNode.href;
+                 }
                  return this.src.replace(/\/thumb\//i,"/original/").replace(/\/thumbnails\//i,"/").replace(/(\/forum\/uploads\/userpics\/.*\/)n([^\/]+)$/,"$1p$2");
              }
             },
