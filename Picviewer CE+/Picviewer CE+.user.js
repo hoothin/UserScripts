e75609bc91b3d2265f7c921e90267e017b0447bb
hoothin <rixixi@gmail.com>
2021-12-28 19:02:23 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -6,7 +6,7 @@
 // @description     Powerful picture viewing tool online, which can popup/scale/rotate/batch save pictures automatically
 // @description:zh-CN    在线看图工具，支持图片翻转、旋转、缩放、弹出大图、批量保存
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
-// @version         2021.12.28.1
+// @version         2021.12.28.2
 // @created         2011-6-15
 // @namespace       http://userscripts.org/users/NLF
 // @homepage        http://hoothin.com
@@ -5413,7 +5413,7 @@ Trace Moe | https://trace.moe/?url=#t#`;
                 var bgReg=/.*url\(\s*["']?(.+?)["']?\s*\)/i;
                 var bgImgs=Array.from(document.querySelectorAll('*'))
                     .reduce((total, node) => {
-                        if(!node.src && (!node.className || !node.className.indexOf || node.className.indexOf("pv-")==-1)){
+                        if(node.nodeName != "IMG" && !node.src && (!node.className || !node.className.indexOf || node.className.indexOf("pv-")==-1)){
                             let prop = unsafeWindow.getComputedStyle(node).backgroundImage;
                             let match = bgReg.exec(prop)
                             if (match) {
