f3bbe437753a22b12a4945b64b1f50a20dbd4989
hoothin <rixixi@gmail.com>
2022-01-08 17:31:15 +0800

    Update managerLinks.js

--- True URL downloads/managerLinks.js
+++ True URL downloads/managerLinks.js
@@ -6,8 +6,18 @@ https://github.com/hoothin/UserScripts/tree/master/True%20URL%20downloads/manage
 var specialUrl=/^magnet|^ed2k/i,
 	simplefilter= /\.php|\.htm|\.jsp|\.asp|\/[^\.]+$/i,
 	resReg = /.*(^magnet|^ed2k|\.torrent$|\.mp4$|\.rar$|\.7z$|\.zip$|\.rmvb$|\.mkv$|\.avi$|\.iso$|\.mp3$|\.txt$|\.exe$|\.chm$|\.pdf$|\.ppt$|\.doc$|\.pptx$|\.docx$|\.epub$|\.xlsx$|\.xls$|\.flac$|\.wma$|\.wav$|\.aac$|\.ape$|\.mid$|\.ogg$|\.m4a$|\.dts$|\.dsd$|\.apk$|\.flv$).*/i,
-	linksArr = [],
-	frame;
+	linksArr = [],frame,
+	copyAll="全部复制",
+	copySel="复制选中",
+	addTips="%i代表递增 %n代表文件名",
+	sortByName="按文件名排序",
+	sortByUrl="按网址排序",
+	sortByType="按扩展名排序",
+	preHolder="批量前缀",
+	nextHolder="批量后缀",
+	closeBtn="关闭",
+	typeHead="类型：";
+
 var by = function(byName, secName) {
 	var compare = function(o, p, name) {
 		var a, b;
@@ -59,26 +69,38 @@ function getLinks() {
 	});
 }
 
-function showLinkFrame(callBack,allBtn,selBtn) {
+function initLang(lang){
+	if(!lang)return;
+	if(lang.copyAll)copyAll=lang.copyAll;
+	if(lang.copySel)copySel=lang.copySel;
+	if(lang.addTips)addTips=lang.addTips;
+	if(lang.sortByName)sortByName=lang.sortByName;
+	if(lang.sortByUrl)sortByUrl=lang.sortByUrl;
+	if(lang.sortByType)sortByType=lang.sortByType;
+	if(lang.preHolder)preHolder=lang.preHolder;
+	if(lang.nextHolder)nextHolder=lang.nextHolder;
+	if(lang.closeBtn)closeBtn=lang.closeBtn;
+	if(lang.typeHead)typeHead=lang.typeHead;
+}
+
+function showLinkFrame(callBack) {
 	var linkItems = [];
-	var typeHtml = "类型：";
-	if(!allBtn)allBtn="全部复制";
-	if(!selBtn)selBtn="复制选中";
+	var typeHtml = typeHead;
 	if (!frame) {
-		$('<style>#managerLinksContent input{border-width:2px;border-style:outset;border-color:buttonface;border-image:initial;font-size:12px}#managerLinksBody>.sort>input{width:33.3%}#managerLinksBody>.addTxt>input{width: 48%;}#managerLinksBody>.fun>input{width: 33.3%;}#managerLinksLinks>div{width:100%;height:20px;overflow:hidden}</style>').appendTo('head');
-		frame = $('<div id="managerLinksContent" style="display:none;">\
-		<div style="height:100%; width:100%; position:fixed; top:0; z-index:99998; opacity:0.3; filter: alpha(opacity=30); background-color:#000;"></div>\
-		<div id="managerLinksBody" style="width:300px;height:300px;position:fixed;left:50%;top:50%;margin-top:-150px;margin-left:-150px;z-index:99998;background-color:#ffffff;border:1px solid #afb3b6;border-radius:10px;opacity:0.95;filter:alpha(opacity=95);box-shadow:5px 5px 20px 0px #000;">\
-			<div id="managerLinksType" style="width:290px;margin-left:5px;"></div>\
-			<div class="sort"><input id="managerLinksSortByName" value="按文件名排序" type="button"><input id="managerLinksSortByUrl" value="按网址排序" type="button"><input id="managerLinksSortByType" value="按扩展名排序" type="button">\
-			</div>\
-			<div id="managerLinksLinks" style="width:100%;overflow:auto;word-wrap:break-word;"></div>\
-			<div title="%i代表递增 %n代表文件名" class="addTxt"><input id="managerLinksPre" type="text" placeholder="批量前缀"><input id="managerLinksAfter" type="text" placeholder="批量后缀">\
-			</div>\
-			<div class="fun"><input id="managerLinksCopyAll" value="'+allBtn+'" type="button"><input id="managerLinksCopySel" value="'+selBtn+'" type="button"><input id="managerLinksClose" value="关闭" type="button">\
-			</div>\
-		</div>\
-		</div>');
+		$('<style>#managerLinksContent input{border-width:2px;border-style:outset;border-color:buttonface;border-image:initial;border: 1px #DADADA solid; padding: 5px 10px; border-radius: 8px; font-weight: bold; font-size: 9pt; outline: none;}#managerLinksBody>.sort>input{width:33.3%}#managerLinksBody>.addTxt{white-space: nowrap;}#managerLinksBody>.addTxt>input{width: 136px; border: 1px #DADADA solid; padding: 5px; margin: 1px; border-radius: 2px; font-weight: bold; font-size: 9pt;}#managerLinksBody>.fun>input{width: 33.3%;}#managerLinksLinks>div{width:100%;height:20px;overflow:hidden}</style>').appendTo('head');
+		frame = $(`<div id="managerLinksContent" style="display:none;">
+		<div style="height:100%; width:100%; position:fixed; top:0; z-index:99998; opacity:0.3; filter: alpha(opacity=30); background-color:#000;"></div>
+		<div id="managerLinksBody" style="width:300px;height:auto;position:fixed;left:50%;top:50%;margin-top:-150px;margin-left:-150px;z-index:99998;background-color:#ffffff;border:1px solid #afb3b6;border-radius:10px;opacity:0.95;filter:alpha(opacity=95);box-shadow:5px 5px 20px 0px #000;">
+			<div id="managerLinksType" style="width:290px;margin-left:5px;"></div>
+			<div class="sort"><input id="managerLinksSortByName" value="${sortByName}" type="button"><input id="managerLinksSortByUrl" value="${sortByUrl}" type="button"><input id="managerLinksSortByType" value="${sortByType}" type="button">
+			</div>
+			<div id="managerLinksLinks" style="width:100%;overflow:auto;word-wrap:break-word;"></div>
+			<div title="${addTips}" class="addTxt"><input id="managerLinksPre" type="text" placeholder="${preHolder}"><input id="managerLinksAfter" type="text" placeholder="${nextHolder}">
+			</div>
+			<div class="fun"><input id="managerLinksCopyAll" value="${copyAll}" type="button"><input id="managerLinksCopySel" value="${copySel}" type="button"><input id="managerLinksClose" value="${closeBtn}" type="button">
+			</div>
+		</div>
+		</div>`);
 		$(document.body).append(frame);
 		$("#managerLinksSortByName").click(function() {
 			$("#managerLinksLinks").html("");
