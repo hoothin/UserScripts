250a8e92a15936680841f78275d231a22bc5e270
hoothin <rixixi@gmail.com>
2022-01-08 14:52:59 +0800

    Update Highlight Every Code.user.js

--- Highlight Every Code/Highlight Every Code.user.js
+++ Highlight Every Code/Highlight Every Code.user.js
@@ -3,13 +3,17 @@
 // @name:zh-CN   代码片段高亮
 // @name:zh-TW   程式碼片斷高亮
 // @namespace    hoothin
-// @version      2.0.1
+// @version      2.1
 // @description  Add a icon to popup a window that allows syntax highlighting and beautify and word count of source code snippets on current page
 // @description:zh-CN 选择代码片段后点击图标弹出新窗口显示语法高亮美化与格式化后的代码与字数统计
 // @description:zh-TW 選擇程式碼片段後點選圖示彈出新視窗顯示語法高亮美化與格式化後的程式碼與字數統計
 // @author       Hoothin
 // @grant        GM_registerMenuCommand
 // @grant        GM.registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM.setValue
+// @grant        GM_getValue
+// @grant        GM.getValue
 // @grant        unsafeWindow
 // @compatible   chrome
 // @compatible   firefox
@@ -22,7 +26,7 @@
 (function() {
     'use strict';
     var codeIcon=document.createElement("img");
-    var codes, selStr, scrollX, scrollY, customInput=false;
+    var codes, selStr, scrollX, scrollY, customInput=false,altKey=true,ctrlKey=true,shiftKey=true,metaKey=true;
     var _unsafeWindow=(typeof unsafeWindow=='undefined'? window : unsafeWindow);
     codeIcon.style.cssText="position:fixed;z-index:99999;cursor:pointer;transition:opacity 0.5s ease-in-out 0s;opacity:0;border:5px solid rgba(0, 0, 0, 0.2);border-radius:10px;";
     codeIcon.title="Show this code snippet";
@@ -58,7 +62,7 @@
         hideIcon();
     });
     document.addEventListener('mouseup', function(o) {
-        if (o.button === 0 && (o.ctrlKey || o.altKey || o.metaKey || o.shiftKey)) {
+        if (o.button === 0 && ((!ctrlKey && !altKey && !metaKey && !shiftKey) || (ctrlKey && o.ctrlKey) || (altKey && o.altKey) || (metaKey && o.metaKey) || (shiftKey && o.shiftKey))) {
             //var customInputKey=(o.ctrlKey && o.shiftKey);
             setTimeout(function(){
                 selStr=document.getSelection().toString();
@@ -123,8 +127,111 @@
         var y = (e.pageY || e.clientY) - scrollY;
         return { 'x': x, 'y': y };
     }
-    var _GM_registerMenuCommand=GM_registerMenuCommand||(typeof GM!='undefined' && GM.registerMenuCommand);
-    if(!_GM_registerMenuCommand)_GM_registerMenuCommand=(s,f)=>{};
+
+    var _GM_getValue,_GM_setValue,_GM_registerMenuCommand;
+    if(typeof GM_getValue!='undefined'){
+        _GM_getValue=GM_getValue;
+        _GM_setValue=GM_setValue;
+        _GM_registerMenuCommand=GM_registerMenuCommand;
+    }else if(typeof GM!='undefined' && typeof GM.getValue!='undefined'){
+        _GM_getValue=GM.getValue;
+        _GM_setValue=GM.setValue;
+        _GM_registerMenuCommand=GM.registerMenuCommand;
+    }
+    if(typeof _GM_registerMenuCommand=='undefined')_GM_registerMenuCommand=(s,f)=>{};
+    var storage={
+        supportGM: typeof GM_getValue=='function' && typeof GM_getValue('a','b')!='undefined',
+        supportGMPromise: typeof GM!='undefined' && typeof GM.getValue=='function' && typeof GM.getValue('a','b')!='undefined',
+        mxAppStorage:(function(){
+            try{
+                return window.external.mxGetRuntime().storage;
+            }catch(e){
+            };
+        })(),
+        operaUJSStorage:(function(){
+            try{
+                return window.opera.scriptStorage;
+            }catch(e){
+            };
+        })(),
+        setItem:function(key,value){
+            if(this.operaUJSStorage){
+                this.operaUJSStorage.setItem(key,value);
+            }else if(this.mxAppStorage){
+                this.mxAppStorage.setConfig(key,value);
+            }else if(this.supportGM || this.supportGMPromise){
+                _GM_setValue(key,value);
+            }else if(window.localStorage){
+                window.localStorage.setItem(key,value);
+            };
+        },
+        getItem:function(key,cb){
+            var value;
+            if(this.operaUJSStorage){
+                value=this.operaUJSStorage.getItem(key);
+            }else if(this.mxAppStorage){
+                value=this.mxAppStorage.getConfig(key);
+            }else if(this.supportGM){
+                value=GM_getValue(key);
+            }else if(this.supportGMPromise){
+                value=GM.getValue(key).then(v=>{cb(v)});
+                return;
+            }else if(window.localStorage){
+                value=window.localStorage.getItem(key);
+            };
+            cb(value);
+        },
+    };
+    storage.getItem("keyConfig",v=>{
+        if(v){
+            var keys=v.split("");
+            altKey=keys[0]!="0";
+            ctrlKey=keys[1]!="0";
+            shiftKey=keys[2]!="0";
+            metaKey=keys[3]!="0";
+        }
+        if(/greasyfork\.org\/.*\/scripts\/24150[^\/]*$/.test(location.href)){
+            var saveConfig=()=>{
+                var conStr="";
+                conStr+=altKey?"1":"0";
+                conStr+=ctrlKey?"1":"0";
+                conStr+=shiftKey?"1":"0";
+                conStr+=metaKey?"1":"0";
+                storage.setItem("keyConfig",conStr);
+            };
+            var keyEles=document.querySelectorAll("h1>em>code");
+            keyEles[0].style.userSelect="none";
+            keyEles[1].style.userSelect="none";
+            keyEles[2].style.userSelect="none";
+            keyEles[3].style.userSelect="none";
+            keyEles[0].style.opacity=altKey?"":"0.3";
+            keyEles[1].style.opacity=ctrlKey?"":"0.3";
+            keyEles[2].style.opacity=shiftKey?"":"0.3";
+            keyEles[3].style.opacity=metaKey?"":"0.3";
+
+            keyEles[0].onclick=e=>{
+                altKey=!altKey;
+                keyEles[0].style.opacity=altKey?"":"0.3";
+                saveConfig();
+            };
+            keyEles[1].onclick=e=>{
+                ctrlKey=!ctrlKey;
+                keyEles[1].style.opacity=ctrlKey?"":"0.3";
+                saveConfig();
+            };
+            keyEles[2].onclick=e=>{
+                shiftKey=!shiftKey;
+                keyEles[2].style.opacity=shiftKey?"":"0.3";
+                saveConfig();
+            };
+            keyEles[3].onclick=e=>{
+                metaKey=!metaKey;
+                keyEles[3].style.opacity=metaKey?"":"0.3";
+                saveConfig();
+            };
+        }
+    });
+
     _GM_registerMenuCommand("Highlight", ()=>{
         selStr=document.getSelection().toString();
         codes=selStr.replace(/&/g, "&amp;").replace(/\</g,"&lt;").replace(/\>/g,"&gt;");
