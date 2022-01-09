d7c808ea1c51ab31f793f9f43e4f834306916211
hoothin <rixixi@gmail.com>
2022-01-09 12:54:56 +0800

    Update Select All Checkboxes.user.js

--- Select All Checkboxes/Select All Checkboxes.user.js
+++ Select All Checkboxes/Select All Checkboxes.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   多选框伴侣
 // @name:zh-TW   多選框伴侶
 // @namespace    hoothin
-// @version      0.6
+// @version      0.6.1
 // @description  A mate for checkbox, select all checkboxes by press Ctrl+Alt+mouse1, or select checkboxes with mouse over by press Alt, or select checkboxes between 2 marks by press Shift.
 // @description:zh-CN Ctrl+Alt点击全选多选框，Alt加鼠标悬停选择多选框，Shift选择两个多选框之间的所有多选框
 // @description:zh-TW Ctrl+Alt點擊全選多選框，Alt加鼠標懸停選擇多選框，Shift選擇兩個多選框之間的所有多選框
@@ -32,8 +32,12 @@
         langStr = "SelectAll";
     }
     var _GM_registerMenuCommand;
-    if(typeof GM_registerMenuCommand=='undefined')_GM_registerMenuCommand=typeof GM!='undefined'&&GM.registerMenuCommand?GM.registerMenuCommand:(s,f)=>{};
-    else _GM_registerMenuCommand=GM_registerMenuCommand;
+    if(typeof GM_registerMenuCommand!='undefined'){
+        _GM_registerMenuCommand=GM_registerMenuCommand;
+    }else if(typeof GM!='undefined' && typeof GM.registerMenuCommand!='undefined'){
+        _GM_registerMenuCommand=GM.registerMenuCommand;
+    }
+    if(typeof _GM_registerMenuCommand=='undefined')_GM_registerMenuCommand=(s,f)=>{};
     _GM_registerMenuCommand(langStr, selectAll);
     var selector="input:checkbox:enabled,.checkbox";
 
