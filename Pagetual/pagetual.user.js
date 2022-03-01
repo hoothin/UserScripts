42e32256a834e3f29d1a54ed55325b1624698337
hoothin <rixixi@gmail.com>
2022-03-01 17:08:03 +0800

    Update pagetual.user.js

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:it      Pagetual
 // @name:ko      東方永頁機
 // @namespace    hoothin
-// @version      1.4.0
+// @version      1.5.0
 // @description  Most compatible Auto pager script ever! Simply auto loading paginated web pages.
 // @description:zh-CN  自动加载并拼接下一分页内容（适用于论坛、漫画站、小说站、资讯站、博客等），无需规则支持所有网页
 // @description:zh-TW  自動加載並拼接下一分頁內容（適用於論壇、漫畫站、小說站、資訊站、博客等），無需規則支持所有網頁
@@ -691,6 +691,7 @@
                     }
                     for(i=0;i<ele.children.length;i++){
                         let curNode=ele.children[i];
+                        if(curNode.tagName=="CANVAS")continue;
                         let comStyle=curWin.getComputedStyle(curNode);
                         let h=parseInt(curNode.scrollHeight);
                         let w=parseInt(curNode.scrollWidth);
