ccd289298ba019d9aa7f9c9f94ec997b144884c3
hoothin <rixixi@gmail.com>
2022-03-12 15:56:16 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2022.3.11.1
+// @version              2022.3.12.1
 // @created              2011-6-15
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             http://hoothin.com
@@ -8280,6 +8280,7 @@ ImgOps | https://imgops.com/#b#`;
                     },
                     hide:function(){
                         fbs.opacity=0;
+                        fbs.display="none";
                     },
                 };
 
@@ -8291,7 +8292,7 @@ ImgOps | https://imgops.com/#b#`;
                 if(this.setPosition())return;
                 this.shown=true;
                 this.setButton();
-                this.floatBar.style.display='block';
+                if(this.floatBar.style.opacity!=0)this.floatBar.style.display='block';
                 clearTimeout(this.hideTimer);
                 window.removeEventListener('scroll',this._scrollHandler,true);
                 window.addEventListener('scroll',this._scrollHandler,true);
