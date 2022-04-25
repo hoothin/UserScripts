283898e5a4fa5549b43b7c57fedd0abbf70bd40f
hoothin <rixixi@gmail.com>
2022-04-25 09:53:17 +0800

    Update pagetual.user.js

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:it      Pagetual
 // @name:ko      東方永頁機
 // @namespace    hoothin
-// @version      1.6.8
+// @version      1.6.8.1
 // @description  Most compatible Auto Pager script ever. Auto loading next paginated web pages and inserting into current page.
 // @description:zh-CN  自动加载并拼接下一分页内容，无需规则即可支持任何网页
 // @description:zh-TW  自動加載並拼接下一分頁內容，無需規則即可支持任意網頁
@@ -1423,12 +1423,12 @@
         });
         _GM_registerMenuCommand(i18n("update"), ()=>{
             updateRules(()=>{
-                alert(i18n("updateSucc"));
+                showTips(i18n("updateSucc"));
                 location.reload();
             },(rule,err)=>{
-                alert("Update "+rule.url+" rules fail!");
+                showTips("Update "+rule.url+" rules fail!");
             });
-            alert(i18n("beginUpdate"));
+            showTips(i18n("beginUpdate"));
         });
 
         var configCon,insertPos;
@@ -2128,6 +2128,31 @@
          .pagetual_pageBar .pagetual_pageNum:hover{
            color: #ff6464;
          }
+         .pagetual_tipsWords{
+           font-size: 50px;
+           font-weight: bold;
+           font-family: "黑体", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+             "Oxygen", "Ubuntu", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji",
+             "Segoe UI Emoji", "Segoe UI Symbol";
+           color: #ffffff;
+           height: 70px;
+           line-height: 70px;
+           position: fixed;
+           left: 50%;
+           top: 10%;
+           margin-left: -150px;
+           padding: 10px;
+           z-index: 999999999;
+           background-color: #000;
+           border: 1px solid black;
+           border-radius: 10px;
+           opacity: 0;
+           filter: alpha(opacity=65);
+           box-shadow: 5px 5px 20px 0px #000;
+           -moz-transition:opacity 0.3s ease-in-out 0s;\
+           -webkit-transition:opacity 0.3s ease-in-out 0s;\
+           transition:opacity 0.3s ease-in-out 0s;\
+         }
 
          @keyframes touhouAni{
            from {transform: rotate(0deg) scale3d(1.2, 1.2, 1.2);}
@@ -2177,6 +2202,9 @@
 
     var initStyle=`display: contents;right: unset;left: unset;top: unset;bottom: unset;inset: unset;clear: both;cy: initial;d: initial;dominant-baseline: initial;empty-cells: initial;fill: initial;fill-opacity: initial;fill-rule: initial;filter: initial;flex: initial;flex-flow: initial;float: initial;flood-color: initial;flood-opacity: initial;grid: initial;grid-area: initial;height: initial;hyphens: initial;image-orientation: initial;image-rendering: initial;inline-size: initial;inset-block: initial;inset-inline: initial;isolation: initial;letter-spacing: initial;lighting-color: initial;line-break: initial;list-style: initial;margin-block: initial;margin: 0px auto;margin-inline: initial;marker: initial;mask: initial;mask-type: initial;max-block-size: initial;max-height: initial;max-inline-size: initial;max-width: initial;min-block-size: initial;min-height: initial;min-inline-size: initial;min-width: initial;mix-blend-mode: initial;object-fit: initial;object-position: initial;offset: initial;opacity: initial;order: initial;orphans: initial;outline: initial;outline-offset: initial;overflow-anchor: initial;overflow-clip-margin: initial;overflow-wrap: initial;overflow: initial;overscroll-behavior-block: initial;overscroll-behavior-inline: initial;overscroll-behavior: initial;padding-block: initial;padding: initial;padding-inline: initial;page: initial;page-orientation: initial;paint-order: initial;perspective: initial;perspective-origin: initial;pointer-events: initial;position: relative;quotes: initial;r: initial;resize: initial;ruby-position: initial;rx: initial;ry: initial;scroll-behavior: initial;scroll-margin-block: initial;scroll-margin: initial;scroll-margin-inline: initial;scroll-padding-block: initial;scroll-padding: initial;scroll-padding-inline: initial;scroll-snap-align: initial;scroll-snap-stop: initial;scroll-snap-type: initial;scrollbar-gutter: initial;shape-image-threshold: initial;shape-margin: initial;shape-outside: initial;shape-rendering: initial;size: initial;speak: initial;stop-color: initial;stop-opacity: initial;stroke: initial;stroke-dasharray: initial;stroke-dashoffset: initial;stroke-linecap: initial;stroke-linejoin: initial;stroke-miterlimit: initial;stroke-opacity: initial;stroke-width: initial;tab-size: initial;table-layout: initial;text-align: initial;text-align-last: initial;text-anchor: initial;text-combine-upright: initial;text-decoration: initial;text-decoration-skip-ink: initial;text-indent: initial;text-overflow: initial;text-shadow: initial;text-size-adjust: initial;text-transform: initial;text-underline-offset: initial;text-underline-position: initial;touch-action: initial;transform: initial;transform-box: initial;transform-origin: initial;transform-style: initial;transition: initial;user-select: initial;vector-effect: initial;vertical-align: initial;visibility: initial;border-spacing: initial;-webkit-border-image: initial;-webkit-box-align: initial;-webkit-box-decoration-break: initial;-webkit-box-direction: initial;-webkit-box-flex: initial;-webkit-box-ordinal-group: initial;-webkit-box-orient: initial;-webkit-box-pack: initial;-webkit-box-reflect: initial;-webkit-highlight: initial;-webkit-hyphenate-character: initial;-webkit-line-break: initial;-webkit-line-clamp: initial;-webkit-mask-box-image: initial;-webkit-mask: initial;-webkit-mask-composite: initial;-webkit-perspective-origin-x: initial;-webkit-perspective-origin-y: initial;-webkit-print-color-adjust: initial;-webkit-rtl-ordering: initial;-webkit-ruby-position: initial;-webkit-tap-highlight-color: initial;-webkit-text-combine: initial;-webkit-text-decorations-in-effect: initial;-webkit-text-emphasis: initial;-webkit-text-emphasis-position: initial;-webkit-text-fill-color: initial;-webkit-text-security: initial;-webkit-text-stroke: initial;-webkit-transform-origin-x: initial;-webkit-transform-origin-y: initial;-webkit-transform-origin-z: initial;-webkit-user-drag: initial;-webkit-user-modify: initial;white-space: initial;widows: initial;width: initial;will-change: initial;word-break: initial;word-spacing: initial;x: initial;y: initial;`;
     var pageTextStyle=`line-height: 30px;text-decoration: none;user-select: none;visibility: visible;position: initial;width: auto;height: auto;float: none;clear: both;margin: 0px auto;text-align: center;display: inline;font-weight: bold;font-style: normal;font-size: 16px;letter-spacing: initial;vertical-align: super;color: rgb(85, 85, 95);`;
+
+    var tipsWords=document.createElement("div");
+    tipsWords.className="pagetual_tipsWords";
 
     var isPause=false,isLoading=false,curPage=1,forceState=0,bottomGap=1000,autoLoadNum=-1;
 
@@ -2282,7 +2310,7 @@
             },100);
         }, true);
         document.addEventListener('dblclick', e=>{
-            let selStr=document.getSelection().toString();
+            let selStr=document.getSelection().toString().trim();
             if(selStr){
                 return;
             }
@@ -2292,9 +2320,20 @@
                     if(!isPause){
                         checkScrollReach();
                     }
+                    showTips(i18n(isPause?"disable":"enable"));
                 },10);
             }
         });
+    }
+
+    function showTips(content, wordColor, backColor){
+        document.body.appendChild(tipsWords);
+        tipsWords.style.opacity=0.8;
+        tipsWords.innerText=content;
+        tipsWords.style.marginLeft=-tipsWords.offsetWidth/2+"px";
+        tipsWords.style.color=wordColor||0xFFFFFF;
+        tipsWords.style.backgroundColor=backColor||0x000;
+        setTimeout(()=>{tipsWords.style.opacity=0},1000);
     }
 
     function getLoadMore(doc){
