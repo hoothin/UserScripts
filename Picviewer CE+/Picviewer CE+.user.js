ddc1cdcd6288d7978cf2b46d030b4e00b36a0ae2
hoothin <rixixi@gmail.com>
2022-05-12 15:48:05 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2022.5.9.1
+// @version              2022.5.12.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             http://hoothin.com
@@ -4086,9 +4086,10 @@ ImgOps | https://imgops.com/#b#`;
                 if(!next)next=curPage.querySelector('[rel="next"]');
                 return {pre:pre,next:next};
             },
-            canonicalUri:function(src, base_path){
+            canonicalUri:function(src){
                 if(src.charAt(0)=="#")return location.href+src;
                 var root_page = /^[^?#]*\//.exec(location.href)[0],
+                    base_path = /^[^?#]*\/([^?#\/]*)/.exec(location.href)[1],
                     root_domain = /^\w+\:\/\/\/?[^\/]+/.exec(root_page)[0],
                     absolute_regex = /^\w+\:\/\//;
                 src=src.replace(/\.\//,"");
@@ -9287,11 +9288,12 @@ ImgOps | https://imgops.com/#b#`;
             var target = e.target;
 
             if (!target || target.id=="pv-float-bar-container" ||
-                (target.parentNode && target.parentNode.id=="icons") ||
+                (target.parentNode && (target.parentNode.id=="icons" || target.parentNode.className=="search-jumper-btn")) ||
                 (target.className && target.className.indexOf &&
                  (/^pv\-/.test(target.className) ||
                   target.className=="whx-a" ||
                   target.className=="whx-a-node" ||
+                  target.className=="search-jumper-btn" ||
                   target.classList.contains("ks-imagezoom-lens")))) {
                 return;
             }
