e81182665ba29fea46a8610298e69485d80d402e
hoothin <rixixi@gmail.com>
2022-05-26 18:14:47 +0800

    Update pagetual.user.js

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:it      Pagetual
 // @name:ko      東方永頁機
 // @namespace    hoothin
-// @version      1.7.3
+// @version      1.7.5
 // @description  Perpetual pages - Most powerful Auto Pager script. Auto loading next paginated web pages and inserting into current page.
 // @description:zh-CN  自动翻页脚本 - 自动加载并拼接下一分页内容，无需规则驱动支持任意网页
 // @description:zh-TW  自動翻頁脚本 - 自動加載並拼接下一分頁內容，無需規則驅動支持任意網頁
@@ -173,7 +173,9 @@
                 openInNewTab:"使拼接页面的内容在新页面打开",
                 importSucc:"导入成功",
                 editCurrent:"自定义此站规则",
-                editBlacklist:"编辑黑名单网址，一行一条，支持? *通配符"
+                editBlacklist:"编辑黑名单网址，一行一条，支持? *通配符",
+                upBtnImg:"回到页首图标",
+                downBtnImg:"前往页尾图标"
             };
             break;
         case "zh-TW":
@@ -221,7 +223,9 @@
                 openInNewTab:"使拼接頁面的内容在新頁面打開",
                 importSucc:"導入成功",
                 editCurrent:"自定義此站規則",
-                editBlacklist:"編輯黑名單網址，一行一條，支持? *通配符"
+                editBlacklist:"編輯黑名單網址，一行一條，支持? *通配符",
+                upBtnImg:"回到頁首圖標",
+                downBtnImg:"前往頁尾圖標"
             };
             break;
         case "ja":
@@ -268,7 +272,9 @@
                 openInNewTab:"スプライスされたページのコンテンツを新しいページで開きます",
                 importSucc:"インポート完了",
                 editCurrent:"現在のルールの編集",
-                editBlacklist:"ブラックリストのURLを編集し、1行ずつ、サポート? *ワイルドカード"
+                editBlacklist:"ブラックリストのURLを編集し、1行ずつ、サポート? *ワイルドカード",
+                upBtnImg:"トップアイコンに戻る",
+                downBtnImg:"フッターアイコンに移動"
             };
             break;
         default:
@@ -315,7 +321,9 @@
                 openInNewTab:"Open urls of additions in new tab",
                 importSucc:"Import completed",
                 editCurrent:"Edit rule for current",
-                editBlacklist:"Edit the blacklist urls, line by line, Support ? * for wildcard"
+                editBlacklist:"Edit the blacklist urls, line by line, Support ? * for wildcard",
+                upBtnImg:"Icon of back to top",
+                downBtnImg:"Icon of go to footer"
             };
             break;
     }
@@ -1755,6 +1763,28 @@
         customUrlsInput.placeholder="0 | http://wedata.net/databases/AutoPagerize/items_all.json";
         configCon.insertBefore(customUrlsInput, insertPos);
 
+        let upBtnImgTitle=document.createElement("h2");
+        upBtnImgTitle.innerHTML=i18n("upBtnImg");
+        upBtnImgTitle.style.width="50%";
+        upBtnImgTitle.style.float="left";
+        let upBtnImgInput=document.createElement("input");
+        upBtnImgInput.style.width="100%";
+        upBtnImgInput.placeholder="data:image/png;base64,UpBtn";
+        upBtnImgInput.value=rulesData.upBtnImg||'';
+        upBtnImgTitle.appendChild(upBtnImgInput);
+        configCon.insertBefore(upBtnImgTitle, insertPos);
+
+        let downBtnImgTitle=document.createElement("h2");
+        downBtnImgTitle.innerHTML=i18n("downBtnImg");
+        downBtnImgTitle.style.width="50%";
+        downBtnImgTitle.style.float="left";
+        let downBtnImgInput=document.createElement("input");
+        downBtnImgInput.style.width="100%";
+        downBtnImgInput.placeholder="data:image/png;base64,DownBtn";
+        downBtnImgInput.value=rulesData.downBtnImg||'';
+        downBtnImgTitle.appendChild(downBtnImgInput);
+        configCon.insertBefore(downBtnImgTitle, insertPos);
+
         let opacityTitle=document.createElement("h2");
         opacityTitle.innerHTML=i18n("opacity");
         let opacityInput=document.createElement("input");
@@ -1867,6 +1897,8 @@
             rulesData.openInNewTab=openInNewTabInput.checked;
             rulesData.initRun=initRunInput.checked;
             rulesData.preload=preloadInput.checked;
+            rulesData.upBtnImg=upBtnImgInput.value;
+            rulesData.downBtnImg=downBtnImgInput.value;
             storage.setItem("rulesData", rulesData);
             let customUrls=customUrlsInput.value.trim();
             if(customUrls){
@@ -2060,6 +2092,12 @@
                         });
                         ruleUrls=urls;
                     }
+                }
+                if(rulesData.upBtnImg){
+                    upSvg=`<img src="${rulesData.upBtnImg}"/>`;
+                }
+                if(rulesData.downBtnImg){
+                    downSvg=`<img src="${rulesData.downBtnImg}"/>`;
                 }
                 if(typeof(rulesData.opacity)=="undefined"){
                     rulesData.opacity=0.3;
@@ -2393,7 +2431,7 @@
     var downSvgCSS=`display:initial;position:absolute;cursor: pointer;margin: 0 15px;width: 30px;height: 30px;vertical-align: middle;fill: currentColor;overflow: hidden;transform: rotate(180deg);`;
 
     var initStyle=`display: contents;right: unset;left: unset;top: unset;bottom: unset;inset: unset;clear: both;cy: initial;d: initial;dominant-baseline: initial;empty-cells: initial;fill: initial;fill-opacity: initial;fill-rule: initial;filter: initial;flex: initial;flex-flow: initial;float: initial;flood-color: initial;flood-opacity: initial;grid: initial;grid-area: initial;height: initial;hyphens: initial;image-orientation: initial;image-rendering: initial;inline-size: initial;inset-block: initial;inset-inline: initial;isolation: initial;letter-spacing: initial;lighting-color: initial;line-break: initial;list-style: initial;margin-block: initial;margin: 0px auto;margin-inline: initial;marker: initial;mask: initial;mask-type: initial;max-block-size: initial;max-height: initial;max-inline-size: initial;max-width: initial;min-block-size: initial;min-height: initial;min-inline-size: initial;min-width: initial;mix-blend-mode: initial;object-fit: initial;object-position: initial;offset: initial;opacity: initial;order: initial;orphans: initial;outline: initial;outline-offset: initial;overflow-anchor: initial;overflow-clip-margin: initial;overflow-wrap: initial;overflow: initial;overscroll-behavior-block: initial;overscroll-behavior-inline: initial;overscroll-behavior: initial;padding-block: initial;padding: initial;padding-inline: initial;page: initial;page-orientation: initial;paint-order: initial;perspective: initial;perspective-origin: initial;pointer-events: initial;position: relative;quotes: initial;r: initial;resize: initial;ruby-position: initial;rx: initial;ry: initial;scroll-behavior: initial;scroll-margin-block: initial;scroll-margin: initial;scroll-margin-inline: initial;scroll-padding-block: initial;scroll-padding: initial;scroll-padding-inline: initial;scroll-snap-align: initial;scroll-snap-stop: initial;scroll-snap-type: initial;scrollbar-gutter: initial;shape-image-threshold: initial;shape-margin: initial;shape-outside: initial;shape-rendering: initial;size: initial;speak: initial;stop-color: initial;stop-opacity: initial;stroke: initial;stroke-dasharray: initial;stroke-dashoffset: initial;stroke-linecap: initial;stroke-linejoin: initial;stroke-miterlimit: initial;stroke-opacity: initial;stroke-width: initial;tab-size: initial;table-layout: initial;text-align: initial;text-align-last: initial;text-anchor: initial;text-combine-upright: initial;text-decoration: initial;text-decoration-skip-ink: initial;text-indent: initial;text-overflow: initial;text-shadow: initial;text-size-adjust: initial;text-transform: initial;text-underline-offset: initial;text-underline-position: initial;touch-action: initial;transform: initial;transform-box: initial;transform-origin: initial;transform-style: initial;transition: initial;user-select: initial;vector-effect: initial;vertical-align: initial;visibility: initial;border-spacing: initial;-webkit-border-image: initial;-webkit-box-align: initial;-webkit-box-decoration-break: initial;-webkit-box-direction: initial;-webkit-box-flex: initial;-webkit-box-ordinal-group: initial;-webkit-box-orient: initial;-webkit-box-pack: initial;-webkit-box-reflect: initial;-webkit-highlight: initial;-webkit-hyphenate-character: initial;-webkit-line-break: initial;-webkit-line-clamp: initial;-webkit-mask-box-image: initial;-webkit-mask: initial;-webkit-mask-composite: initial;-webkit-perspective-origin-x: initial;-webkit-perspective-origin-y: initial;-webkit-print-color-adjust: initial;-webkit-rtl-ordering: initial;-webkit-ruby-position: initial;-webkit-tap-highlight-color: initial;-webkit-text-combine: initial;-webkit-text-decorations-in-effect: initial;-webkit-text-emphasis: initial;-webkit-text-emphasis-position: initial;-webkit-text-fill-color: initial;-webkit-text-security: initial;-webkit-text-stroke: initial;-webkit-transform-origin-x: initial;-webkit-transform-origin-y: initial;-webkit-transform-origin-z: initial;-webkit-user-drag: initial;-webkit-user-modify: initial;white-space: initial;widows: initial;width: initial;will-change: initial;word-break: initial;word-spacing: initial;x: initial;y: initial;`;
-    var pageTextStyle=`line-height: 30px;text-decoration: none;user-select: none;visibility: visible;position: initial;width: auto;height: auto;float: none;clear: both;margin: 0px auto;text-align: center;display: inline;font-weight: bold;font-style: normal;font-size: 16px;letter-spacing: initial;vertical-align: super;color: rgb(85, 85, 95);`;
+    var pageTextStyle=`line-height: 30px;text-decoration: none;user-select: none;visibility: visible;position: initial;width: auto;max-width: 80%; white-space: nowrap; text-overflow: ellipsis;overflow: hidden;height: auto;float: none;clear: both;margin: 0px auto;text-align: center;display: inline-block;font-weight: bold;font-style: normal;font-size: 16px;letter-spacing: initial;vertical-align: top;color: rgb(85, 85, 95);`;
 
     var tipsWords=document.createElement("div");
     tipsWords.className="pagetual_tipsWords";
@@ -2611,6 +2649,7 @@
         pageBar.appendChild(pageText);
         if(ruleParser.nextTitle){
             pageText.innerHTML=ruleParser.nextTitle+" ";
+            pageText.title=ruleParser.nextTitle;
         }
         if(ruleParser.curSiteRule.pageNum || (hasPageNum && /[&\/\?](p=|page[=\/_-]?)\d+|[_-]\d+\./.test(url))){
             pageText.innerHTML+="Page ";
@@ -2621,6 +2660,7 @@
             pageNum.style.cssText=pageTextStyle;
             pageNum.style.cursor="pointer";
             pageNum.style.color="";
+            pageNum.style.marginLeft="5px";
             pageNum.addEventListener("click", e=>{
                 let pageInput=prompt(i18n("inputPageNum"), "1");
                 if(pageInput){
