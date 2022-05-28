ca89431f38a90779e6a2d73b68c7daac16fed24a
hoothin <rixixi@gmail.com>
2022-05-28 11:22:19 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,10 +4,10 @@
 // @name:zh-TW   搜索醬
 // @name:ja      検索ちゃん
 // @namespace    hoothin
-// @version      0.9.8
+// @version      0.9.9
 // @description  Jump to any search engine quickly and easily!
-// @description:zh-CN  又一个搜索引擎跳转脚本
-// @description:zh-TW  又一個搜尋引擎跳轉脚本
+// @description:zh-CN  又一个搜索引擎跳转脚本，在搜索时便捷跳转各大搜索引擎，如谷歌、必应、百度、鸭鸭等
+// @description:zh-TW  又一個搜尋引擎跳轉脚本，在搜索時便捷跳轉各大搜尋引擎，如谷歌、必應、百度、鴨鴨等
 // @description:ja  任意の検索エンジンにすばやく簡単にジャンプします！
 // @author       hoothin
 // @match        *://*/*
@@ -687,7 +687,7 @@
      }
      .search-jumper-searchBar:hover {
          margin-top: 0;
-         opacity: 0.9;
+         opacity: 1;
      }
      .search-jumper-left,
      .search-jumper-left .search-jumper-type,
