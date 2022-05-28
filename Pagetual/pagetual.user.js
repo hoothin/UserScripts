5d73e8bbd5ae659d33a9102504ef9670e8aa3182
hoothin <rixixi@gmail.com>
2022-05-28 16:43:01 +0800

    Update pagetual.user.js

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:it      Pagetual
 // @name:ko      東方永頁機
 // @namespace    hoothin
-// @version      1.9
+// @version      1.9.1
 // @description  Perpetual pages - Most powerful Auto Pager script. Auto loading next paginated web pages and inserting into current page.
 // @description:zh-CN  自动翻页脚本 - 自动加载并拼接下一分页内容，无需规则驱动支持任意网页
 // @description:zh-TW  自動翻頁脚本 - 自動加載並拼接下一分頁內容，無需規則驅動支持任意網頁
@@ -885,12 +885,23 @@
                     }
                     self.curSiteRule.pageElement=self.geneSelector(ele);
                     if(ele.children.length>1){
-                        let middleChild=ele.children[parseInt(ele.children.length/2)];
-                        if((middleChild.style && middleChild.style.position=="absolute" && middleChild.style.left && middleChild.style.top) || ele.tagName=="UL" || curHeight==0){
-                            ele=[ele];
-                        }else{
-                            self.curSiteRule.pageElement+=">*";
-                            ele=ele.children;
+                        let hasText=false;
+                        for(let i in ele.childNodes){
+                            let child=ele.childNodes[i];
+                            if(child.nodeType==3 && child.nodeValue.trim()!=''){
+                                hasText=true;
+                                ele=[ele];
+                                break;
+                            }
+                        }
+                        if(!hasText){
+                            let middleChild=ele.children[parseInt(ele.children.length/2)];
+                            if((middleChild.style && middleChild.style.position=="absolute" && middleChild.style.left && middleChild.style.top) || ele.tagName=="UL" || curHeight==0){
+                                ele=[ele];
+                            }else{
+                                self.curSiteRule.pageElement+=">*";
+                                ele=ele.children;
+                            }
                         }
                     }else{
                         ele=[ele];
