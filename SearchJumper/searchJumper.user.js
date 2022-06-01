3e1706339864f969a42d7a953211f314728e0450
hoothin <rixixi@gmail.com>
2022-06-01 19:59:44 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜索醬
 // @name:ja      検索ちゃん
 // @namespace    hoothin
-// @version      1.2
+// @version      1.2.1
 // @description  Jump to any search engine quickly and easily!
 // @description:zh-CN  又一个搜索引擎跳转脚本，在搜索时便捷跳转各大搜索引擎，如谷歌、必应、百度、鸭鸭等
 // @description:zh-TW  又一個搜尋引擎跳轉脚本，在搜索時便捷跳轉各大搜尋引擎，如谷歌、必應、百度、鴨鴨等
@@ -845,6 +845,7 @@
 
     class SearchBar {
         constructor() {
+            if (searchData.prefConfig.cssText) cssText = searchData.prefConfig.cssText;
             let styleEle = document.createElement("style");
             styleEle.innerHTML = createHTML(cssText);
             document.documentElement.appendChild(styleEle);
@@ -1254,7 +1255,7 @@
                         ele.dataset.current = true;
                     }
                 } else if (data.url.indexOf(location.host) != -1 &&
-                           data.url.replace(/^https?:\/\//, "").replace(location.host, "").replace(/[\?#].*/, "") == location.pathname) {
+                           data.url.replace(/^https?:\/\//, "").replace(location.host, "").replace(/\/?[\?#].*/, "") == location.pathname.replace(/\/$/, "")) {
                     let urlReg = data.url.match(/[^\/\?&]+(?=%[stb])/g);
                     if (urlReg) {
                         urlReg = urlReg.join('.*');
