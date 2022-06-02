a50c5502b3c66afc07d0cbb067d76bf9353ce289
hoothin <rixixi@gmail.com>
2022-06-02 20:04:00 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜索醬
 // @name:ja      検索ちゃん
 // @namespace    hoothin
-// @version      1.3
+// @version      1.3.1
 // @description  Jump to any search engine quickly and easily!
 // @description:zh-CN  又一个搜索引擎跳转脚本，在搜索时便捷跳转各大搜索引擎，如谷歌、必应、百度、鸭鸭等
 // @description:zh-TW  又一個搜尋引擎跳轉脚本，在搜索時便捷跳轉各大搜尋引擎，如谷歌、必應、百度、鴨鴨等
@@ -1321,7 +1321,7 @@
                     }
                 });
             }
-            if (!currentSite) {
+            if (!currentSite || data.hideNotMatch) {
                 if (data.match) {
                     if (new RegExp(data.match).test(location.href)) {
                         ele.dataset.current = true;
@@ -1337,34 +1337,35 @@
                     }
                 }
                 if (ele.dataset.current) {
-                    let submitParams = location.href.match(/#p{(.*?)}/);
-                    if (submitParams) {
-                        setTimeout(() => {
-                            submitParams = submitParams[1];
-                            let params = new URLSearchParams(submitParams);
-                            let form;
-                            params.forEach((v, k) => {
-                                let input = document.querySelector(k);
-                                input.value = v;
-                                if (!form) {
-                                    form = input.parentNode;
-                                    while (form.tagName != 'FORM') {
-                                        form = form.parentNode;
-                                        if (!form || form.tagName == 'BODY') break;
+                    if (!currentSite) {
+                        let submitParams = location.href.match(/#p{(.*?)}/);
+                        if (submitParams) {
+                            setTimeout(() => {
+                                submitParams = submitParams[1];
+                                let params = new URLSearchParams(submitParams);
+                                let form;
+                                params.forEach((v, k) => {
+                                    let input = document.querySelector(k);
+                                    input.value = v;
+                                    if (!form) {
+                                        form = input.parentNode;
+                                        while (form.tagName != 'FORM') {
+                                            form = form.parentNode;
+                                            if (!form || form.tagName == 'BODY') break;
+                                        }
                                     }
+                                });
+                                if (form) {
+                                    let submitBtn = form.querySelector("[type=submit]");
+                                    if(submitBtn) submitBtn.click();
+                                    else form.submit();
                                 }
-                            });
-                            if (form) {
-                                let submitBtn = form.querySelector("[type=submit]");
-                                if(submitBtn) submitBtn.click();
-                                else form.submit();
-                            }
-                        }, 500);
+                            }, 500);
+                        }
                     }
+                } else if (data.hideNotMatch) {
+                    ele.style.display = 'none';
                 }
-            }
-            if (data.hideNotMatch && data.match && new RegExp(data.match).test(location.href) === false) {
-                ele.style.display = 'none';
             }
             if (!isBookmarklet && (openInNewTab || searchData.prefConfig.openInNewTab)) {
                 ele.setAttribute("target", "_blank");
