62410cdd193d96ed766ec1cba0b607d552b7524e
hoothin <rixixi@gmail.com>
2022-06-06 16:13:02 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜索醬
 // @name:ja      検索ちゃん
 // @namespace    hoothin
-// @version      1.3.8
+// @version      1.3.8.1
 // @description  Jump to any search engine quickly and easily!
 // @description:zh-CN  又一个搜索引擎跳转脚本，在搜索时便捷跳转各大搜索引擎，如谷歌、必应、百度等
 // @description:zh-TW  又一個搜尋引擎跳轉脚本，在搜索時便捷跳轉各大搜尋引擎，如Google、Bing、Baidu等
@@ -719,7 +719,7 @@
                      margin-top: -${this.scale * 25}px;
                      opacity: 0.3;
                      vertical-align: top;
-                     transition:margin-top 0.25s ease, margin-left 0.25s, opacity 0.25s;
+                     transition:margin-top 1s ease, margin-left 1s, opacity 1s, transform 1s;
                      user-select: none;
                      box-sizing:content-box;
                      text-align: center;
@@ -755,6 +755,7 @@
                  .search-jumper-searchBar:hover {
                      margin-top: 0;
                      opacity: 1;
+                     transition:margin-top 0.25s ease, margin-left 0.25s, opacity 0.25s, transform 0.25s;
                  }
                  .search-jumper-searchBar.initShow {
                      margin-top: 0;
@@ -805,7 +806,6 @@
                  .search-jumper-bottom>.search-jumper-searchBar {
                      position: relative;
                      margin-top: 0px;
-                     transition: transform 0.25s;
                      -webkit-transform:scale(.9);
                      -moz-transform:scale(.9);
                      transform:scale(.9);
