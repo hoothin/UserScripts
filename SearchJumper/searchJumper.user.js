cad5ba6fac158661c297a2ac053f3ec4bfcd4498
hoothin <rixixi@gmail.com>
2022-06-07 14:40:25 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜索醬
 // @name:ja      検索ちゃん
 // @namespace    hoothin
-// @version      1.3.9
+// @version      1.3.9.1
 // @description  Jump to any search engine quickly and easily!
 // @description:zh-CN  又一个搜索引擎跳转脚本，在搜索时便捷跳转各大搜索引擎
 // @description:zh-TW  又一個搜尋引擎跳轉脚本，在搜索時便捷跳轉各大搜尋引擎
@@ -1046,10 +1046,16 @@
                     for (let i in searchData.sitesConfig) {
                         if (currentSite) break;
                         typeData = searchData.sitesConfig[i];
+                        if (!typeData) {
+                            continue;
+                        }
                         let sites = typeData.sites;
                         for (let j in sites) {
                             if (currentSite) break;
                             let data = sites[j];
+                            if (!data || !data.url) {
+                                continue;
+                            }
                             if (data.match === '0') {
                             } else if (data.match) {
                                 if (new RegExp(data.match).test(location.href)) {
