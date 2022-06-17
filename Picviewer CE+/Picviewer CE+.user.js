b65dbd621899eaa3ae6fe3d44ad31f64e7472caf
hoothin <rixixi@gmail.com>
2022-06-17 09:31:53 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2022.6.2.1
+// @version              2022.6.17.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             http://hoothin.com
@@ -15981,6 +15981,9 @@ ImgOps | https://imgops.com/#b#`;
                 if(bgImgs)imgs=imgs.concat(bgImgs);
                 // 排除库里面的图片
                 imgs = imgs.filter(function(img){
+                    if (img.parentNode && (img.parentNode.id=="icons" || img.parentNode.className=="search-jumper-btn")) {
+                        return false;
+                    }
                     return !(container.contains(img) || (preloadContainer&&preloadContainer.contains(img)));
                 });
 
@@ -20842,6 +20845,11 @@ ImgOps | https://imgops.com/#b#`;
         }
 
         function checkPreview(e){
+            let selStr;
+            try {
+                selStr=document.getSelection().toString();
+            }catch(e){}
+            if (selStr) return false;
             let keyActive=(prefs.floatBar.globalkeys.type == "hold" && checkGlobalKeydown(e)) ||
                 (prefs.floatBar.globalkeys.type == "press" && globalFuncEnabled);
             return prefs.floatBar.globalkeys.invertInitShow?!keyActive:keyActive;
