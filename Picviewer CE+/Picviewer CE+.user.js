aee013cd80eeb43d34f29296f612dc249c5ddfb1
hoothin <rixixi@gmail.com>
2022-06-25 10:40:37 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2022.6.25.1
+// @version              2022.6.25.2
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             http://hoothin.com
@@ -15950,7 +15950,7 @@ ImgOps | https://imgops.com/#b#`;
             },
             getAllValidImgs:function(newer){
                 var validImgs = [];
-                var imgs = document.querySelectorAll('img:not(#search-jumper img)'),
+                var imgs = document.getElementsByTagName('img'),
                     container = document.querySelector('.pv-gallery-container'),
                     preloadContainer = document.querySelector('.pv-gallery-preloaded-img-container');
 
@@ -15982,6 +15982,13 @@ ImgOps | https://imgops.com/#b#`;
                 if(bgImgs)imgs=imgs.concat(bgImgs);
                 // 排除库里面的图片
                 imgs = imgs.filter(function(img){
+                    if (img.parentNode) {
+                        if (img.parentNode.id=="icons") {
+                            return false;
+                        } else if (img.parentNode.classList && img.parentNode.classList.contains("search-jumper-btn")) {
+                            return false;
+                        }
+                    }
                     return !(container.contains(img) || (preloadContainer&&preloadContainer.contains(img)));
                 });
 
