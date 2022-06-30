cce34574f112b4aa68f2a591a11c112301cb4cbc
hoothin <rixixi@gmail.com>
2022-06-30 20:37:03 +0800

    Update pagetual.user.js

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:it      Pagetual
 // @name:ko      東方永頁機
 // @namespace    hoothin
-// @version      1.9.26.11
+// @version      1.9.26.12
 // @description  Perpetual pages - Most powerful Auto Pager script. Auto loading next paginated web pages and inserting into current page.
 // @description:zh-CN  自动翻页脚本 - 自动加载并拼接下一分页内容，无需规则驱动支持任意网页
 // @description:zh-TW  自動翻頁脚本 - 自動加載並拼接下一分頁內容，無需規則驅動支持任意網頁
@@ -264,10 +264,10 @@
                 loadNow:"今すぐページをめくる",
                 loadConfirm:"数ページめくりたいですか？（0は途切れない）",
                 noNext:"次のページが見つかりません、新しいルールを作成してください",
-                passSec: "＃t＃秒前に更新",
-                passMin: "＃t＃分前に更新",
-                passHour: "＃t＃時間前に更新",
-                passDay: "＃t＃日前に更新",
+                passSec: "#t#秒前に更新",
+                passMin: "#t#分前に更新",
+                passHour: "#t#時間前に更新",
+                passDay: "#t#日前に更新",
                 cantDel: "組み込みルールを削除できません",
                 confirmDel: "このルールを削除してもよろしいですか？",
                 updateSucc: "更新に成功しました",
