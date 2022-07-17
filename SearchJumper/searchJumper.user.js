2ea7032825b914201a826f3290bb93441a8f082a
hoothin <rixixi@gmail.com>
2022-07-17 21:50:47 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜索醬
 // @name:ja      検索ちゃん
 // @namespace    hoothin
-// @version      1.6.5.9.8
+// @version      1.6.5.9.9
 // @description  Jump to any search engine quickly and easily, the most powerful, most complete search enhancement script!
 // @description:zh-CN  又一个多搜索引擎切换脚本，在搜索时一键跳转各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  又一個多搜尋引擎切換脚本，在搜索時一鍵跳轉各大搜尋引擎，支持任意頁面右鍵劃詞搜索與全面自定義
@@ -1440,6 +1440,12 @@
                                 self.searchInput.value = "";
                                 self.searchInput.style.paddingLeft = `${15 + self.searchLockInput.scrollWidth}px`;
                                 self.searchInput.placeholder = i18n("inputKeywords");
+                                if (e.ctrlKey) {
+                                    let siteEle = self.bar.querySelector("a.search-jumper-btn:not(.input-hide)");
+                                    if (siteEle) {
+                                        self.openSiteBtn(siteEle);
+                                    }
+                                }
                             }
                             break;
                         case 8://退格
