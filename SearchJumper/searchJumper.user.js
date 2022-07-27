364b22b6232124ef698032291f1f239a9f5744af
hoothin <rixixi@gmail.com>
2022-07-27 09:34:45 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜索醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.5.9.36.2
+// @version      1.6.5.9.36.5
 // @description  Jump to any search engine quickly and easily, the most powerful, most complete search enhancement script!
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键跳转各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜索時一鍵跳轉各大搜尋引擎，支持任意頁面右鍵劃詞搜索與全面自定義
@@ -598,6 +598,9 @@
             }, {
                 name: "编辑当前网页",
                 url: "javascript:(function(){document.body.setAttribute('contenteditable', 'true');alert('已开启网页编辑，按ESC键取消');document.onkeydown = function (e) {e = e || window.event;if(e.keyCode==27){document.body.setAttribute('contenteditable', 'false');}}})();"
+            }, {
+                name: "万能命令",
+                url: "https://wn.run/%u"
             } ]
         }
     ];
@@ -2093,7 +2096,7 @@
                     }
                 }
                 if (typeof data.description !== 'undefined') {
-                    ele.dataset.title = data.description;
+                    ele.dataset.title = type + " - " + data.description;
                 } else {
                     ele.dataset.title = type;
                 }
@@ -2448,7 +2451,7 @@
                 }
                 let isPage = /^(https?|ftp):/.test(data.url);
                 ele.className = "search-jumper-btn";
-                if (typeof data.description !== 'undefined') ele.title = data.description;
+                if (typeof data.description !== 'undefined') ele.title = name + " - " + data.description;
                 ele.dataset.name = name;
                 ele.classList.add("search-jumper-word");
                 let word = document.createElement("span");
