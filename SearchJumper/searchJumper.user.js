a065f6354975f996434594145cf1dd7abd91acd9
hoothin <rixixi@gmail.com>
2022-08-03 20:49:12 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜索醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.5.9.37.3
+// @version      1.6.5.9.37.5
 // @description  Jump to any search engine quickly and easily, the most powerful, most complete search enhancement script.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键跳转各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜索時一鍵跳轉各大搜尋引擎，支持任意頁面右鍵劃詞搜索與全面自定義
@@ -1614,7 +1614,6 @@
                     }
                 }
                 lastSign = 0;
-                storage.setItem("lastSign", 0);
                 let inputTimer;
                 this.lockSiteKeywords = false;
                 this.inInput = false;
@@ -4885,6 +4884,7 @@
                     resolve(data || 0);
                 });
             });
+            storage.setItem("lastSign", 0);
             inPagePostParams = await new Promise((resolve) => {
                 storage.getItem("inPagePostParams", data => {
                     resolve(data || false);
