ea90ad7a2bba218fcc657e034f7285bed841bf8d
hoothin <rixixi@gmail.com>
2022-08-03 16:05:38 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2022.8.3.1
+// @version              2022.8.3.2
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             http://hoothin.com
@@ -11871,15 +11871,15 @@ ImgOps | https://imgops.com/#b#`;
             debug: false,
             customLang:'auto',
             customRules:`[
-//{
-//name: "Example",
-//url: /https?:\/\/www.google(\.\w{1,3}){1,3}\/search\?.*/,
-//getImage: function(a){
-//},
-//src: /avatar/i,
-//r: /\?.*$/i,
-//s: ''
-//}
+//  {
+//    name: "Example, can be safely deleted",
+//    url: /https?:\\/\\/www.google(\\.\\w{1,3}){1,3}\\/search\\?.*/,
+//    getImage: function(a) {
+//    },
+//    src: /avatar/i,
+//    r: /\\?.*$/i,
+//    s: ''
+//  }
 ]`,
             firstEngine:"Tineye"
         };
@@ -20743,7 +20743,10 @@ ImgOps | https://imgops.com/#b#`;
                         })
                         //siteInfo=customRules.concat(siteInfo);
                     }
-                }catch(e){}
+                }catch(e){
+                    console.log("Wrong rule for Picviewer CE+");
+                    console.log(e);
+                }
 
                 var self=this,r=0;
                 self.rules=[];
