496111666bf68124ad50d5aad96933eb142d52dd
hoothin <rixixi@gmail.com>
2022-08-04 16:21:32 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜索醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.5.9.37.8
+// @version      1.6.5.9.37.9
 // @description  Jump to any search engine quickly and easily, the most powerful, most complete search enhancement script.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键跳转各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜索時一鍵跳轉各大搜尋引擎，支持任意頁面右鍵劃詞搜索與全面自定義
@@ -2486,6 +2486,9 @@
                         if (/^http/.test(siteEle.href) && !siteEle.onclick) {
                             storage.setItem("lastSign", siteNames);
                             _GM_openInTab(siteEle.href, {incognito: true});
+                            setTimeout(() => {
+                                storage.setItem("lastSign", 0);
+                            }, 1000);
                             break;
                         }
                     }
@@ -2518,6 +2521,9 @@
                         if (/^http/.test(siteEle.href) && !siteEle.onclick) {
                             storage.setItem("lastSign", siteNames);
                             window.open(siteEle.href, '_blank');
+                            setTimeout(() => {
+                                storage.setItem("lastSign", 0);
+                            }, 1000);
                             break;
                         }
                     }
