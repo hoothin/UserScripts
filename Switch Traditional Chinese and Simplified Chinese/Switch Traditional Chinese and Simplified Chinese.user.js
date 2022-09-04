a8df87413b41779d00006c22f942d03943d3c473
hoothin <rixixi@gmail.com>
2022-09-04 19:13:32 +0800

    Update Switch Traditional Chinese and Simplified Chinese.user.js

--- Switch Traditional Chinese and Simplified Chinese/Switch Traditional Chinese and Simplified Chinese.user.js
+++ Switch Traditional Chinese and Simplified Chinese/Switch Traditional Chinese and Simplified Chinese.user.js
@@ -6,7 +6,7 @@
 // @namespace    hoothin
 // @supportURL   https://github.com/hoothin/UserScripts
 // @homepageURL  https://github.com/hoothin/UserScripts
-// @version      1.2.3
+// @version      1.2.5
 // @description        任意轉換網頁中的簡體中文與繁體中文（默認簡體→繁體）
 // @description:zh-CN  任意转换网页中的简体中文与繁体中文（默认繁体→简体）
 // @description:ja     简繁中国語に変換
@@ -17,19 +17,29 @@
 // @grant        GM_getValue
 // @grant        GM_registerMenuCommand
 // @grant        GM_notification
+// @grant        GM_listValues
+// @grant        GM_deleteValue
 // @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=rixixi@sina.com&item_name=Greasy+Fork+donation
 // @contributionAmount 1
 // ==/UserScript==
 //因一簡對多繁，所以簡轉繁需要優先排除異體字，並根據詞匯轉換。其他需要語義分析的，暫時無解。整理繁簡對照表很費時，因此不打臉的話不再更新，如有需求，刪減自用。更精細的需求可自行申請相應API或自行訓練語義AI並搭建對照數據庫。在油猴脚本裏面如此這般折騰，我是覺得沒有意義啦。。。
 (function() {
     'use strict';
-    var auto = false;//是否自動切換至OS使用的默認語言
-    var shortcutKey = 119;//快捷鍵keyCode
+    var auto = false;
+    var shortcutKey = 'F8';
+    var ctrlKey = true;
+    var altKey = false;
+    var shiftKey = false;
+    var metaKey = false;
     //此處為單字轉換
     var scStr = '皑蔼碍爱翱袄奥坝罢摆败颁办绊帮绑镑谤剥饱宝报鲍辈贝钡狈备惫绷笔毕毙闭边编贬变辩辫鳖瘪濒滨宾摈饼拨钵铂驳卜补参蚕残惭惨灿苍舱仓沧厕侧册测层诧搀掺蝉馋谗缠铲产阐颤场尝长偿肠厂畅钞车彻尘陈衬撑称惩诚骋痴迟驰耻齿炽冲虫宠畴踌筹绸丑橱厨锄雏础储触处传疮闯创锤纯绰辞词赐聪葱囱从丛凑窜错达带贷担单郸掸胆惮诞弹当挡党荡档捣岛祷导盗灯邓敌涤递缔点垫电淀钓调谍叠钉顶锭订东动栋冻斗犊独读赌镀锻断缎兑队对吨顿钝夺鹅额讹恶饿儿尔饵贰发罚阀珐矾钒烦范贩饭访纺飞废费纷坟奋愤粪丰枫锋风疯冯缝讽凤肤辐抚辅赋复负讣妇缚该钙盖干赶秆赣冈刚钢纲岗皋镐搁鸽阁铬个给龚宫巩贡钩沟构购够蛊顾剐关观馆惯贯广规硅归龟闺轨诡柜贵刽辊滚锅国过骇韩汉阂鹤贺横轰鸿红后壶护沪户哗华画划话怀坏欢环还缓换唤痪焕涣黄谎挥辉毁贿秽会烩汇讳诲绘荤浑伙获货祸击机积饥讥鸡绩缉极辑级挤几蓟剂济计记际继纪夹荚颊贾钾价驾歼监坚笺间艰缄茧检碱硷拣捡简俭减荐槛鉴践贱见键舰剑饯渐溅涧浆蒋桨奖讲酱胶浇骄娇搅铰矫侥脚饺缴绞轿较秸阶节茎惊经颈静镜径痉竞净纠厩旧驹举据锯惧剧鹃绢杰洁结诫届紧锦仅谨进晋烬尽劲荆觉决诀绝钧军骏开凯颗壳课垦恳抠库裤夸块侩宽矿旷况亏岿窥馈溃扩阔蜡腊莱来赖蓝栏拦篮阑兰澜谰揽览懒缆烂滥捞劳涝乐镭垒类泪篱离里鲤礼丽厉励砾历沥隶俩联莲连镰怜涟帘敛脸链恋炼练粮凉两辆谅疗辽镣猎临邻鳞凛赁龄铃凌灵岭领馏刘龙聋咙笼垄拢陇楼娄搂篓芦卢颅庐炉掳卤虏鲁赂禄录陆驴吕铝侣屡缕虑滤绿峦挛孪滦乱抡轮伦仑沦纶论萝罗逻锣箩骡骆络妈玛码蚂马骂吗买麦卖迈脉瞒馒蛮满谩猫锚铆贸么霉没镁门闷们锰梦谜弥觅绵缅庙灭悯闽鸣铭谬谋亩钠纳难挠脑恼闹馁腻撵捻酿鸟聂啮镊镍柠狞宁拧泞钮纽脓浓农疟诺欧鸥殴呕沤盘庞赔喷鹏骗飘频贫苹凭评泼颇扑铺朴谱脐齐骑岂启气弃讫牵扦钎铅迁签谦钱钳潜浅谴堑枪呛墙蔷强抢锹桥乔侨翘窍窃钦亲轻氢倾顷请庆琼穷趋区躯驱龋颧权劝却鹊让饶扰绕热韧认纫荣绒软锐闰润洒萨鳃赛伞丧骚扫涩杀纱筛晒闪陕赡缮伤赏烧绍赊摄慑设绅审婶肾渗声绳胜圣师狮湿诗尸时蚀实识驶势释饰视试寿兽枢输书赎属术树竖数帅双谁税顺说硕烁丝饲耸怂颂讼诵擞苏诉肃虽绥岁孙损笋缩琐锁獭挞抬摊贪瘫滩坛谭谈叹汤烫涛绦腾誊锑题体屉条贴铁厅听烃铜统头图涂团颓蜕脱鸵驮驼椭洼袜弯湾顽万网韦违围为潍维苇伟伪纬谓卫温闻纹稳问瓮挝蜗涡窝呜钨乌诬无芜吴坞雾务误锡牺袭习铣戏细虾辖峡侠狭厦锨鲜纤咸贤衔闲显险现献县馅羡宪线厢镶乡详响项萧销晓啸蝎协挟携胁谐写泻谢锌衅兴汹锈绣虚嘘须许绪续轩悬选癣绚学勋询寻驯训讯逊压鸦鸭哑亚讶阉烟盐严颜阎艳厌砚彦谚验鸯杨扬疡阳痒养样瑶摇尧遥窑谣药爷页业叶医铱颐遗仪彝蚁艺亿忆义诣议谊译异绎荫阴银饮樱婴鹰应缨莹萤营荧蝇颖哟拥佣痈踊咏涌优忧邮铀犹游诱舆鱼渔娱与屿语吁御狱誉预驭鸳渊辕园员圆缘远愿约跃钥岳粤悦阅云郧匀陨运蕴酝晕韵杂灾载攒暂赞赃脏凿枣灶责择则泽贼赠扎札轧铡闸诈斋债毡盏斩辗崭栈战绽张涨帐账胀赵蛰辙锗这贞针侦诊镇阵挣睁狰帧郑证织职执纸挚掷帜质钟终种肿众诌轴皱昼骤猪诸诛烛瞩嘱贮铸筑驻专砖转赚桩庄装妆壮状锥赘坠缀谆浊兹资渍踪综总纵邹诅组钻致钟么为只凶准启板里雳余链泄厘裆着周赞松于态内众撸剥躏号唉冲制纮恺镳标确将志并删鱿荞麸谷签咨宽系发面托恶跡喂啰喽锈诶岩呐布卷随历闲复跶呆唠产裢隐卧刹姜郁虱鉴宁巨注浏';
     var tcStr = '皚藹礙愛翺襖奧壩罷擺敗頒辦絆幫綁鎊謗剝飽寶報鮑輩貝鋇狽備憊繃筆畢斃閉邊編貶變辯辮鼈癟瀕濱賓擯餅撥缽鉑駁蔔補參蠶殘慚慘燦蒼艙倉滄廁側冊測層詫攙摻蟬饞讒纏鏟産闡顫場嘗長償腸廠暢鈔車徹塵陳襯撐稱懲誠騁癡遲馳恥齒熾沖蟲寵疇躊籌綢醜櫥廚鋤雛礎儲觸處傳瘡闖創錘純綽辭詞賜聰蔥囪從叢湊竄錯達帶貸擔單鄲撣膽憚誕彈當擋黨蕩檔搗島禱導盜燈鄧敵滌遞締點墊電澱釣調諜疊釘頂錠訂東動棟凍鬥犢獨讀賭鍍鍛斷緞兌隊對噸頓鈍奪鵝額訛惡餓兒爾餌貳發罰閥琺礬釩煩範販飯訪紡飛廢費紛墳奮憤糞豐楓鋒風瘋馮縫諷鳳膚輻撫輔賦複負訃婦縛該鈣蓋幹趕稈贛岡剛鋼綱崗臯鎬擱鴿閣鉻個給龔宮鞏貢鈎溝構購夠蠱顧剮關觀館慣貫廣規矽歸龜閨軌詭櫃貴劊輥滾鍋國過駭韓漢閡鶴賀橫轟鴻紅後壺護滬戶嘩華畫劃話懷壞歡環還緩換喚瘓煥渙黃謊揮輝毀賄穢會燴彙諱誨繪葷渾夥獲貨禍擊機積饑譏雞績緝極輯級擠幾薊劑濟計記際繼紀夾莢頰賈鉀價駕殲監堅箋間艱緘繭檢堿鹼揀撿簡儉減薦檻鑒踐賤見鍵艦劍餞漸濺澗漿蔣槳獎講醬膠澆驕嬌攪鉸矯僥腳餃繳絞轎較稭階節莖驚經頸靜鏡徑痙競淨糾廄舊駒舉據鋸懼劇鵑絹傑潔結誡屆緊錦僅謹進晉燼盡勁荊覺決訣絕鈞軍駿開凱顆殼課墾懇摳庫褲誇塊儈寬礦曠況虧巋窺饋潰擴闊蠟臘萊來賴藍欄攔籃闌蘭瀾讕攬覽懶纜爛濫撈勞澇樂鐳壘類淚籬離裏鯉禮麗厲勵礫曆瀝隸倆聯蓮連鐮憐漣簾斂臉鏈戀煉練糧涼兩輛諒療遼鐐獵臨鄰鱗凜賃齡鈴淩靈嶺領餾劉龍聾嚨籠壟攏隴樓婁摟簍蘆盧顱廬爐擄鹵虜魯賂祿錄陸驢呂鋁侶屢縷慮濾綠巒攣孿灤亂掄輪倫侖淪綸論蘿羅邏鑼籮騾駱絡媽瑪碼螞馬罵嗎買麥賣邁脈瞞饅蠻滿謾貓錨鉚貿麽黴沒鎂門悶們錳夢謎彌覓綿緬廟滅憫閩鳴銘謬謀畝鈉納難撓腦惱鬧餒膩攆撚釀鳥聶齧鑷鎳檸獰甯擰濘鈕紐膿濃農瘧諾歐鷗毆嘔漚盤龐賠噴鵬騙飄頻貧蘋憑評潑頗撲鋪樸譜臍齊騎豈啓氣棄訖牽扡釺鉛遷簽謙錢鉗潛淺譴塹槍嗆牆薔強搶鍬橋喬僑翹竅竊欽親輕氫傾頃請慶瓊窮趨區軀驅齲顴權勸卻鵲讓饒擾繞熱韌認紉榮絨軟銳閏潤灑薩鰓賽傘喪騷掃澀殺紗篩曬閃陝贍繕傷賞燒紹賒攝懾設紳審嬸腎滲聲繩勝聖師獅濕詩屍時蝕實識駛勢釋飾視試壽獸樞輸書贖屬術樹豎數帥雙誰稅順說碩爍絲飼聳慫頌訟誦擻蘇訴肅雖綏歲孫損筍縮瑣鎖獺撻擡攤貪癱灘壇譚談歎湯燙濤縧騰謄銻題體屜條貼鐵廳聽烴銅統頭圖塗團頹蛻脫鴕馱駝橢窪襪彎灣頑萬網韋違圍爲濰維葦偉僞緯謂衛溫聞紋穩問甕撾蝸渦窩嗚鎢烏誣無蕪吳塢霧務誤錫犧襲習銑戲細蝦轄峽俠狹廈鍁鮮纖鹹賢銜閑顯險現獻縣餡羨憲線廂鑲鄉詳響項蕭銷曉嘯蠍協挾攜脅諧寫瀉謝鋅釁興洶鏽繡虛噓須許緒續軒懸選癬絢學勳詢尋馴訓訊遜壓鴉鴨啞亞訝閹煙鹽嚴顔閻豔厭硯彥諺驗鴦楊揚瘍陽癢養樣瑤搖堯遙窯謠藥爺頁業葉醫銥頤遺儀彜蟻藝億憶義詣議誼譯異繹蔭陰銀飲櫻嬰鷹應纓瑩螢營熒蠅穎喲擁傭癰踴詠湧優憂郵鈾猶遊誘輿魚漁娛與嶼語籲禦獄譽預馭鴛淵轅園員圓緣遠願約躍鑰嶽粵悅閱雲鄖勻隕運蘊醞暈韻雜災載攢暫贊贓髒鑿棗竈責擇則澤賊贈紮劄軋鍘閘詐齋債氈盞斬輾嶄棧戰綻張漲帳賬脹趙蟄轍鍺這貞針偵診鎮陣掙睜猙幀鄭證織職執紙摯擲幟質鍾終種腫衆謅軸皺晝驟豬諸誅燭矚囑貯鑄築駐專磚轉賺樁莊裝妝壯狀錐贅墜綴諄濁茲資漬蹤綜總縱鄒詛組鑽緻鐘麼為隻兇準啟闆裡靂餘鍊洩釐襠著週讚鬆於態內眾擼褫躪號欸衝製紘愷钀標確將誌並刪魷蕎麩穀籤諮寛係髮麵託噁蹟餵囉嘍銹誒巖吶佈捲隨歷閒復躂獃嘮產褳隱臥剎薑鬱蝨鑑寧鉅註瀏';
     //此處為匹配上具體詞語就優先轉換的單字
     var sc2tc = {'为':'爲','剥':'剝','产':'產','么':'麼','宁':'寧','启':'啟','锈':'鏽',
+                 '丑':[
+                     '醜',
+                     ['丑','小丑','丑角']
+                 ],
                  '周':[
                      '周',
                      ['週','周报','周期','周会','周日','周刊','周波','周岁','周末','周考','一周','抓周','双周刊','名剧周','黄金周','周休二日']
@@ -221,6 +231,8 @@
         default:
             break;
     }
+
+    const inConfigPage = location.host == 'greasyfork.org' && /scripts\/24300(\-[^\/]*)?$/.test(location.pathname);
 
     var isSimple = (lang === "zh-cn" || lang === "zh-hans" || lang === "zh-sg" || lang === "zh-my");
     var action = 0;//1:noChange, 2:showSimplified, 3:showTraditional
@@ -491,7 +503,32 @@
         setLanguage();
     }
 
-    var saveAction=GM_getValue("action_" + location.hostname.toString().replace(/\./g,"_"));
+    let _auto = GM_getValue("auto");
+    if (typeof _auto != 'undefined') {
+        auto = _auto;
+    }
+    let _shortcutKey = GM_getValue("shortcutKey");
+    if (typeof _shortcutKey != 'undefined') {
+        shortcutKey = _shortcutKey;
+    }
+    let _ctrlKey = GM_getValue("ctrlKey");
+    if (typeof _ctrlKey != 'undefined') {
+        ctrlKey = _ctrlKey;
+    }
+    let _altKey = GM_getValue("altKey");
+    if (typeof _altKey != 'undefined') {
+        altKey = _altKey;
+    }
+    let _shiftKey = GM_getValue("shiftKey");
+    if (typeof _shiftKey != 'undefined') {
+        shiftKey = _shiftKey;
+    }
+    let _metaKey = GM_getValue("metaKey");
+    if (typeof _metaKey != 'undefined') {
+        metaKey = _metaKey;
+    }
+
+    var saveAction = GM_getValue("action_" + location.hostname.toString().replace(/\./g,"_"));
     action=saveAction?saveAction:(isSimple?(auto?2:3):(auto?3:2));
     if((auto||saveAction) && action > 1){
         setTimeout(function(){
@@ -516,9 +553,10 @@
 
     var curLang=isSimple;
     document.addEventListener("keydown", function(e) {
-        if(e.keyCode == shortcutKey && e.ctrlKey) {
+        if(e.key == shortcutKey && e.ctrlKey == ctrlKey && e.altKey == altKey && e.shiftKey == shiftKey && e.metaKey == metaKey) {
             if("TEXTAREA"==document.activeElement.tagName){
                 document.activeElement.innerHTML=curLang?traditionalized(document.activeElement.innerHTML):simplized(document.activeElement.innerHTML);
+                document.activeElement.value=curLang?traditionalized(document.activeElement.value):simplized(document.activeElement.value);
                 curLang=!curLang;
             }else if("INPUT"==document.activeElement.tagName){
                 document.activeElement.value=curLang?traditionalized(document.activeElement.value):simplized(document.activeElement.value);
@@ -529,6 +567,145 @@
             }
         }
     });
+
+    if (inConfigPage) {
+        let parent = document.querySelector('#additional-info');
+        let baseCon = document.createElement('div');
+        baseCon.style.margin = '20px';
+        parent.insertBefore(baseCon, parent.children[0]);
+        let checkIndex = 0;
+        let createCheckbox = (name, defaultValue) => {
+            let box = document.createElement('div');
+            let checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.checked = defaultValue;
+            let id = 'stcnsc-checkbox' + checkIndex++;
+            checkbox.id = id;
+            let label = document.createElement('label');
+            label.setAttribute('for', id);
+            label.innerText = name;
+            box.appendChild(checkbox);
+            box.appendChild(label);
+            baseCon.appendChild(box);
+            return checkbox;
+        };
+        let autoInput = createCheckbox('總是自動切換', auto);
+        let shortcutCon = document.createElement('div');
+        shortcutCon.style.display = 'flex';
+        shortcutCon.style.alignItems = 'center';
+        let shortcutTitle = document.createElement('h3');
+        shortcutTitle.style.margin = '5px 0';
+        shortcutTitle.innerText = '快捷鍵：';
+        shortcutCon.appendChild(shortcutTitle);
+        let shortcutInput = document.createElement('input');
+        shortcutInput.style.height = '20px';
+        shortcutInput.style.width = '50px';
+        shortcutInput.setAttribute('readonly', "readonly");
+        shortcutInput.value = shortcutKey;
+        shortcutInput.addEventListener("keydown", function(e) {
+            if (e.key) {
+                shortcutInput.value = e.key;
+                e.stopPropagation();
+                e.preventDefault();
+            }
+        }, true);
+        shortcutCon.appendChild(shortcutInput);
+        baseCon.appendChild(shortcutCon);
+        let ctrlKeyInput = createCheckbox('Ctrl 鍵', ctrlKey);
+        let altKeyInput = createCheckbox('Alt 鍵', altKey);
+        let shiftKeyInput = createCheckbox('Shift 鍵', shiftKey);
+        let metaKeyInput = createCheckbox('Meta 鍵', metaKey);
+
+        let siteChanged = false;
+        let sitesTcTitle = document.createElement('h3');
+        sitesTcTitle.style.margin = '5px 0';
+        sitesTcTitle.innerText = '正體中文站點：';
+        baseCon.appendChild(sitesTcTitle);
+        let sitesTcInput = document.createElement('textarea');
+        sitesTcInput.style.width = '100%';
+        baseCon.appendChild(sitesTcInput);
+
+        let sitesScTitle = document.createElement('h3');
+        sitesScTitle.style.margin = '5px 0';
+        sitesScTitle.innerText = '簡體中文站點：';
+        baseCon.appendChild(sitesScTitle);
+        let sitesScInput = document.createElement('textarea');
+        sitesScInput.style.width = '100%';
+        baseCon.appendChild(sitesScInput);
+
+        let sitesDisableTitle = document.createElement('h3');
+        sitesDisableTitle.style.margin = '5px 0';
+        sitesDisableTitle.innerText = '禁用站點：';
+        baseCon.appendChild(sitesDisableTitle);
+        let sitesDisableInput = document.createElement('textarea');
+        sitesDisableInput.style.width = '100%';
+        baseCon.appendChild(sitesDisableInput);
+
+        sitesTcInput.addEventListener("change", function(e) {
+            siteChanged = true;
+        });
+        sitesScInput.addEventListener("change", function(e) {
+            siteChanged = true;
+        });
+        sitesDisableInput.addEventListener("change", function(e) {
+            siteChanged = true;
+        });
+
+        let sitesList = GM_listValues();
+        sitesList.forEach(site => {
+            if (site.indexOf('action_') === 0) {
+                let _action = GM_getValue(site);
+                site = site.replace(/^action_/, '').replace(/_/g, '.') + '\n';
+                switch (_action) {
+                    case 1:
+                        sitesDisableInput.value += site;
+                        break;
+                    case 2:
+                        sitesScInput.value += site;
+                        break;
+                    case 3:
+                        sitesTcInput.value += site;
+                        break;
+                }
+            }
+        });
+        let saveBtn = document.createElement('button');
+        saveBtn.innerText = '保存設置';
+        saveBtn.style.display = 'block';
+        saveBtn.addEventListener("click", function(e) {
+            auto = autoInput.checked;
+            shortcutKey = shortcutInput.value;
+            ctrlKey = ctrlKeyInput.checked;
+            altKey = altKeyInput.checked;
+            shiftKey = shiftKeyInput.checked;
+            metaKey = metaKeyInput.checked;
+
+            if (siteChanged) {
+                sitesList.forEach(site => {
+                    if (site.indexOf('action_') === 0) {
+                        GM_deleteValue(site);
+                    }
+                });
+                sitesDisableInput.value.trim().split('\n').forEach(site => {
+                    GM_setValue("action_" + site.replace(/\./g,"_"), 1);
+                });
+                sitesScInput.value.trim().split('\n').forEach(site => {
+                    GM_setValue("action_" + site.replace(/\./g,"_"), 2);
+                });
+                sitesTcInput.value.trim().split('\n').forEach(site => {
+                    GM_setValue("action_" + site.replace(/\./g,"_"), 3);
+                });
+            }
+            GM_setValue('auto', auto);
+            GM_setValue('shortcutKey', shortcutKey);
+            GM_setValue('ctrlKey', ctrlKey);
+            GM_setValue('altKey', altKey);
+            GM_setValue('shiftKey', shiftKey);
+            GM_setValue('metaKey', metaKey);
+            alert('保存設置成功！')
+        });
+        baseCon.appendChild(saveBtn);
+    }
 
     GM_registerMenuCommand("繁簡切換【Ctrl+F8】", switchLanguage);
 })();
