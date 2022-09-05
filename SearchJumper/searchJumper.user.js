e280418c1eca532790fe0e66e2c327e3e23fb723
hoothin <rixixi@gmail.com>
2022-09-05 20:26:25 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜索醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.16
+// @version      1.6.6.16.1
 // @description  Jump to any search engine quickly and easily, the most powerful, most complete search enhancement script.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键跳转各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜索時一鍵跳轉各大搜尋引擎，支持任意頁面右鍵劃詞搜索與全面自定義
@@ -2381,11 +2381,11 @@
                         color: black;
                     }
                     #rangePickerBtn {
-                        width: 32px;
+                        width: 28px;
                         float: right;
-                        top: -37px;
-                        right: 3px;
-                        position: relative;
+                        margin-top: -33px;
+                        margin-right: 4px;
+                        position: sticky;
                         display: block;
                         cursor: pointer;
                         background: rgb(255 255 255 / 80%);
