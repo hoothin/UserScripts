b3a6d9e5b355c0cf32c054c01f3946b40089d952
hoothin <rixixi@gmail.com>
2022-09-06 16:23:16 +0800

    Update pagetual.user.js

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:it      Pagetual
 // @name:ko      東方永頁機
 // @namespace    hoothin
-// @version      1.9.31
+// @version      1.9.31.1
 // @description  Perpetual pages - most powerful auto-pager script, auto loading next paginated web pages and inserting into current page.
 // @description:zh-CN  自动翻页脚本 - 自动加载并拼接下一分页内容，支持任意网页
 // @description:zh-TW  自動翻頁脚本 - 自動加載並拼接下一分頁內容，支持任意網頁
@@ -4214,6 +4214,7 @@
                 td.appendChild(inTd);
                 pageBar.appendChild(td);
             } else {
+                inTd.style.width='100%';
                 pageBar.appendChild(inTd);
             }
         }
