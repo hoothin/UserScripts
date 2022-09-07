d57bbe8dde291c9f1c53c774833f02766ea38a57
hoothin <rixixi@gmail.com>
2022-09-07 19:58:47 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2022.9.7
+// @version              2022.9.7.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             http://hoothin.com
@@ -41,7 +41,7 @@
 // @grant                unsafeWindow
 // @require              https://greasyfork.org/scripts/6158-gm-config-cn/code/GM_config%20CN.js?version=23710
 // @require              https://greasyfork.org/scripts/438080-pvcep-rules/code/pvcep_rules.js?version=1089924
-// @require              https://greasyfork.org/scripts/440698-pvcep-lang/code/pvcep_lang.js?version=1089401
+// @require              https://greasyfork.org/scripts/440698-pvcep-lang/code/pvcep_lang.js?version=1090794
 // @downloadURL          https://greasyfork.org/scripts/24204-picviewer-ce/code/Picviewer%20CE+.user.js
 // @updateURL            https://greasyfork.org/scripts/24204-picviewer-ce/code/Picviewer%20CE+.user.js
 // @match                *://*/*
@@ -17638,9 +17638,9 @@ ImgOps | https://imgops.com/#b#`;
                     ';
                 document.head.appendChild(style);
             },
-            clickOut:function(){
+            clickOut:function(e){
                 if(!this.magnifier.classList.contains("pv-magnifier-container_focus")){
-                    this.remove();
+                    this.remove(e);
                 }
             },
             focus:function(){
@@ -17754,11 +17754,16 @@ ImgOps | https://imgops.com/#b#`;
                 };
             },
             remove:function(e){
-                e.preventDefault();
-                e.stopPropagation();
-                this.magnifier.parentNode.removeChild(this.magnifier);
+                if (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+                if (this.magnifier.parentNode) {
+                    this.magnifier.parentNode.removeChild(this.magnifier);
+                    MagnifierC.all.splice(MagnifierC.all.indexOf(this),1);
+                }
                 document.removeEventListener('mousemove',this._move,true);
-                MagnifierC.all.splice(MagnifierC.all.indexOf(this),1);
+                document.removeEventListener('mouseup',this._clickOut,true);
             },
         };
 
