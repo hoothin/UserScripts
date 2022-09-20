f80b4a96ec323a9f6b66c7fae2b37f01273ec5d9
hoothin <rixixi@gmail.com>
2022-09-20 21:30:13 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.41
+// @version      1.6.6.42
 // @description  Jump to any search engine quickly and easily, the most powerful, most complete search enhancement script.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键跳转各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵跳轉各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -640,12 +640,12 @@
         openInNewTab: false,
         enableInPage: true,
         altKey: false,
-        ctrlKey: false,
+        ctrlKey: true,
         shiftKey: false,
         metaKey: false,
         autoClose: false,
         autoDelay: 1000,
-        shortcut: false,
+        shortcut: true,
         initShow: false,
         customSize: 100,
         typeOpenTime: 250,
@@ -6872,7 +6872,7 @@
                     let startX = e.clientX;
                     let startY = e.clientY;
                     let mouseMoveHandler = e => {
-                        if (Math.abs(startX - e.clientX) + Math.abs(startY - e.clientY) > 5) {
+                        if (Math.abs(startX - e.clientX) + Math.abs(startY - e.clientY) > 2) {
                             clearTimeout(showToolbarTimer);
                             document.removeEventListener('mousemove', mouseMoveHandler, true);
                             e.target.removeEventListener('scroll', scrollHandler);
@@ -6921,6 +6921,7 @@
                         if (draging) return;
                         if (targetElement != e.target) return;
                         if ((e.which === 1 || e.which === 2) && !searchData.prefConfig.leftMouse) return;
+                        if (e.which === 1 && getSelectStr() !== '') return;
                         searchBar.showInPage();
                         shown = true;
                     }, parseInt(searchData.prefConfig.longPressTime));
@@ -7242,7 +7243,7 @@
                             }
                             document.removeEventListener('mouseup', mouseUpHandler, false);
                             document.removeEventListener('mousemove', mouseMoveHandler, false);
-                            if (getSelectStr() ==='' && window.confirm(i18n("importOrNot"))) {
+                            if (getSelectStr() === '' && window.confirm(i18n("importOrNot"))) {
                                 let configTxt = e.target.innerText.trim(), configData;
                                 try {
                                     configData = JSON.parse(configTxt);
