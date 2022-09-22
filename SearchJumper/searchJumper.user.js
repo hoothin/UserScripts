627c9893e086b5378333124f313f92ede58cb2d1
hoothin <rixixi@gmail.com>
2022-09-22 18:13:12 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.45.9
+// @version      1.6.6.45.10
 // @description  Jump to any search engine quickly and easily, the most powerful, most complete search enhancement script.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键跳转各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵跳轉各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -5891,8 +5891,8 @@
                     if (clientX < 0) clientX = 5;
                     else if (clientX + self.bar.scrollWidth > viewWidth) clientX = viewWidth - self.bar.scrollWidth - 20;
                     let clientY = e.clientY;
-                    if (clientY > viewHeight / 2) clientY -= (self.bar.scrollHeight + 15);
-                    else clientY += 15;
+                    if (clientY > viewHeight / 2) clientY -= (self.bar.scrollHeight + 20);
+                    else clientY += 30;
                     self.bar.style.left = clientX + "px";
                     self.bar.style.top = clientY + "px";
                     self.removeBar();
