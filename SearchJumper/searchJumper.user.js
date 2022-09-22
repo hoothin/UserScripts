3fc9e0e1aa3f13cda2dc72e5302b5474259edb4e
hoothin <rixixi@gmail.com>
2022-09-22 12:53:37 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.45.4
+// @version      1.6.6.45.5
 // @description  Jump to any search engine quickly and easily, the most powerful, most complete search enhancement script.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键跳转各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵跳轉各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -4637,6 +4637,17 @@
                                  document.activeElement.tagName == 'TEXTAREA' ||
                                  document.activeElement.contentEditable == 'true')) {
                                 return;
+                            } else {
+                                let contentEditable = false;
+                                let parent = document.activeElement;
+                                while (parent) {
+                                    if (contentEditable || parent.tagName == 'BODY') {
+                                        break;
+                                    }
+                                    parent = parent.parentNode;
+                                    contentEditable = parent.contentEditable == 'true';
+                                }
+                                if (contentEditable) return;
                             }
                         }
                         var key = (e.key || String.fromCharCode(e.keyCode)).toLowerCase();
@@ -5154,6 +5165,17 @@
                                  document.activeElement.tagName == 'TEXTAREA' ||
                                  document.activeElement.contentEditable == 'true')) {
                                 return;
+                            } else {
+                                let contentEditable = false;
+                                let parent = document.activeElement;
+                                while (parent) {
+                                    if (contentEditable || parent.tagName == 'BODY') {
+                                        break;
+                                    }
+                                    parent = parent.parentNode;
+                                    contentEditable = parent.contentEditable == 'true';
+                                }
+                                if (contentEditable) return;
                             }
                         }
                         var key = (e.key || String.fromCharCode(e.keyCode)).toLowerCase();
@@ -6838,6 +6860,17 @@
                                  document.activeElement.tagName == 'TEXTAREA' ||
                                  document.activeElement.contentEditable == 'true')) {
                                 return;
+                            } else {
+                                let contentEditable = false;
+                                let parent = document.activeElement;
+                                while (parent) {
+                                    if (contentEditable || parent.tagName == 'BODY') {
+                                        break;
+                                    }
+                                    parent = parent.parentNode;
+                                    contentEditable = parent.contentEditable == 'true';
+                                }
+                                if (contentEditable) return;
                             }
                         }
                         var key = (e.key || String.fromCharCode(e.keyCode)).toLowerCase();
@@ -6883,6 +6916,17 @@
                             e.target.tagName == 'TEXTAREA' ||
                             e.target.contentEditable == 'true') {
                             return;
+                        } else {
+                            let contentEditable = false;
+                            let parent = e.target;
+                            while (parent) {
+                                if (contentEditable || parent.tagName == 'BODY') {
+                                    break;
+                                }
+                                parent = parent.parentNode;
+                                contentEditable = parent.contentEditable == 'true';
+                            }
+                            if (contentEditable) return;
                         }
                     }
                     waitForMouse = true;
