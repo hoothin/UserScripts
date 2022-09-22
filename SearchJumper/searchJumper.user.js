2dc7e260ab79fe5de9125538f9eb725801c889b4
hoothin <rixixi@gmail.com>
2022-09-22 19:06:33 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.45.11
+// @version      1.6.6.45.12
 // @description  Jump to any search engine quickly and easily, the most powerful, most complete search enhancement script.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键跳转各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵跳轉各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -4772,7 +4772,7 @@
 
                 let showTimer;
                 typeBtn.addEventListener('mouseenter', e => {
-                    if (searchData.prefConfig.showSiteLists && (searchData.prefConfig.alwaysShowSiteLists || ele.classList.contains("search-jumper-hide"))) {
+                    if (!self.bar.classList.contains("funcKeyCall") && searchData.prefConfig.showSiteLists && (searchData.prefConfig.alwaysShowSiteLists || ele.classList.contains("search-jumper-hide"))) {
                         self.listPos(ele.children[0], siteList);
                     } else {
                         self.tipsPos(typeBtn, ele.dataset.title);
