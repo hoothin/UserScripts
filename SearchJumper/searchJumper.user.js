73c26f2a3427f97139724af58bc565f1469a7839
hoothin <rixixi@gmail.com>
2022-10-09 09:07:28 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.46.35
+// @version      1.6.6.46.36
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -5937,7 +5937,7 @@
                                 if (url.indexOf('%input{') !== -1) {
                                     self.showCustomInputWindow(url, _url => {
                                         storage.setItem("postUrl", [_url, data.charset]);
-                                        ele.href = _url.replace(/[:%]p{.*/, '');
+                                        ele.href = _url.replace(/\?.*/, "").replace(/[:%]p{.*/, '');
                                         if (ele.target === '_blank') {
                                             _GM_openInTab(ele.href, {active: true});
                                         } else {
@@ -5946,7 +5946,7 @@
                                     });
                                 } else {
                                     storage.setItem("postUrl", [url, data.charset]);
-                                    ele.href = url.replace(/[:%]p{.*/, '');
+                                    ele.href = url.replace(/\?.*/, "").replace(/[:%]p{.*/, '');
                                     if (ele.target === '_blank') {
                                         _GM_openInTab(ele.href, {active: true});
                                     } else {
