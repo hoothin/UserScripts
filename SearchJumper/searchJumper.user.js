49182f89ef06896f68776673f17d0c7d363ebb05
hoothin <rixixi@gmail.com>
2022-10-17 19:59:41 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.46.51
+// @version      1.6.6.46.52
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -7423,11 +7423,13 @@
                     }
                     let startX = e.clientX;
                     let startY = e.clientY;
+                    let moved = false;
                     let mouseMoveHandler = e => {
                         if (Math.abs(startX - e.clientX) + Math.abs(startY - e.clientY) > 2) {
                             clearTimeout(showToolbarTimer);
                             document.removeEventListener('mousemove', mouseMoveHandler, true);
                             e.target.removeEventListener('scroll', scrollHandler);
+                            moved = true;
                         }
                     };
                     let scrollHandler = e => {
@@ -7442,7 +7444,7 @@
                             e.preventDefault();
                         } else if (!inputSign) {
                             setTimeout(() => {
-                                if ((matchKey && e.which !== 1) || (e.which === 1 && searchData.prefConfig.selectToShow && getSelectStr())) {
+                                if ((matchKey && e.which !== 1) || (moved && e.which === 1 && searchData.prefConfig.selectToShow && getSelectStr())) {
                                     searchBar.showInPage(true, e);
                                 } else {
                                     searchBar.waitForHide();
