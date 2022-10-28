7713a6ac36c9178dd593a95a0bea652d0da32ddb
hoothin <rixixi@gmail.com>
2022-10-28 19:41:46 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.46.59
+// @version      1.6.6.46.60
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -7470,9 +7470,11 @@
                             e.preventDefault();
                         } else if (!inputSign) {
                             setTimeout(() => {
+                                if (shown) return;
                                 if ((matchKey && e.which !== 1) || (moved && e.which === 1 && searchData.prefConfig.selectToShow && getSelectStr())) {
                                     searchBar.showInPage(true, e);
                                 } else {
+                                    waitForMouse = false;
                                     searchBar.waitForHide();
                                 }
                             }, 0);
@@ -7485,6 +7487,8 @@
                     if (e.type === 'dblclick') {
                         if (getSelectStr() !== '') {
                             shown = true;
+                            draging = false;
+                            document.removeEventListener('mouseup', mouseUpHandler, true);
                             document.removeEventListener('mousemove', mouseMoveHandler, true);
                             e.target.removeEventListener('scroll', scrollHandler);
                             clearTimeout(showToolbarTimer);
@@ -7515,6 +7519,7 @@
                         if (targetElement != e.target) return;
                         if ((e.which === 1 || e.which === 2) && !searchData.prefConfig.leftMouse) return;
                         if (e.which === 1 && getSelectStr() !== '') return;
+                        searchBar.bar.classList.remove("funcKeyCall");
                         searchBar.showInPage();
                         shown = true;
                     }, parseInt(searchData.prefConfig.longPressTime));
