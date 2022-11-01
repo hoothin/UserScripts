1f8206db258fed820f8b72587ff9167bae75dae2
hoothin <rixixi@gmail.com>
2022-11-01 13:34:06 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.46.63
+// @version      1.6.6.46.64
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -8048,6 +8048,21 @@
                     }
                     .searchJumperFrame-body>.sitesCon div.exist {
                         text-decoration:line-through;
+                    }
+                    @media (prefers-color-scheme: dark) {
+                      .searchJumperFrame-body,
+                      .searchJumperFrame-input-title,
+                      .searchJumperFrame-inputs>input,
+                      .searchJumperFrame-inputs>textarea,
+                      .searchJumperFrame-inputs>select,
+                      .searchJumperFrame-body select {
+                        background-color: black;
+                        color: #d5d5d5;
+                      }
+                      .searchJumperFrame-title,
+                      .searchJumperFrame-buttons>button {
+                        background: #245d8f;
+                      }
                     }
                 `;
                 this.filterCssEle = _GM_addStyle(this.filterCss);
