00724e5c4f8bc39d94c3ad8119ea7a2884f816c9
hoothin <rixixi@gmail.com>
2022-11-04 08:37:39 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.46.66
+// @version      1.6.6.46.67
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -711,7 +711,7 @@
         showInSearchEngine: false,
         showInSearchJumpPage: true,
         limitInPageLen: 1,
-        ignoreWords: ["a", "in", "into", "the", "to", "on", "among", "between", "and", "an", "of", "by", "with", "about", "under"],
+        ignoreWords: ["a", "in", "into", "the", "to", "on", "among", "between", "and", "an", "of", "by", "with", "about", "under", "or", "at", "as"],
         inPageRule: {},
         firstFiveWordsColor: [],
         inPageWordsStyles: [],
@@ -2645,7 +2645,7 @@
                         if (!word) return;
                         if (init) {
                             if (word.length < (searchData.prefConfig.limitInPageLen || 1)) return;
-                            if ((searchData.prefConfig.ignoreWords || []).includes(word)) return;
+                            if ((searchData.prefConfig.ignoreWords || []).includes(word.toLowerCase())) return;
                         }
                         let title = "";
                         let style = "";
@@ -3559,6 +3559,14 @@
                         } else {
                             len = word.content.length;
                             pos = node.data.toUpperCase().indexOf(word.content.toUpperCase());
+                            if (pos >= 0) {
+                                if (pos !== 0 && /\w/.test(node.data[pos - 1])) {
+                                    pos = -1;
+                                }
+                                if (pos + word.content.length !== node.data.length && /\w/.test(node.data[pos + word.content.length])) {
+                                    pos = -1;
+                                }
+                            }
                         }
                         if (pos >= 0) {
                             if (typeof word.hideParent !== 'undefined') {
