d8b13811948bb31a29218030f53e24b63351576f
hoothin <rixixi@gmail.com>
2022-12-04 20:39:51 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -1366,7 +1366,7 @@
                  #search-jumper-alllist:hover+.search-jumper-showallBg {
                      backdrop-filter: none;
                      -webkit-backdrop-filter: none;
-                     background-color: rgb(28 33 39 / 70%);
+                     background-color: rgb(28 33 39 / 80%);
                  }
                  .search-jumper-historylist {
                      display: flex;
