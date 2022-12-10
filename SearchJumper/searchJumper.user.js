18ded68f803c8a08dd2a59bb7d7c380c44a62a61
hoothin <rixixi@gmail.com>
2022-12-10 12:52:46 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.48
+// @version      1.6.6.49
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -3337,6 +3337,9 @@
                             if (hide.dataset.content === word.content) {
                                 hide.classList.remove("searchJumper-hide");
                                 hide.removeAttribute('data-content');
+                                [].forEach.call(hide.querySelectorAll(".searchJumper-hide"), hideChild => {
+                                    hideChild.classList.remove("searchJumper-hide");
+                                });
                             }
                         });
                     }
@@ -3349,12 +3352,13 @@
                             if (hideChange && hideParent != -1) {
                                 let parentDepth = hideParent;
                                 let parent = mark.parentElement;
-                                while(parentDepth-- > 0) {
+                                while(parentDepth-- > 0 && parent) {
                                     parent = parent.parentElement;
                                 }
                                 if (parent) {
                                     parent.dataset.content = word.content;
                                     parent.classList.add("searchJumper-hide");
+                                    mark.classList.add("searchJumper-hide");
                                 }
                             }
                         }
@@ -3589,6 +3593,9 @@
                     [].forEach.call(ele.querySelectorAll(".searchJumper-hide"), hide => {
                         hide.classList.remove("searchJumper-hide");
                         hide.removeAttribute('data-content');
+                        [].forEach.call(hide.querySelectorAll(".searchJumper-hide"), hideChild => {
+                            hideChild.classList.remove("searchJumper-hide");
+                        });
                     });
                     this.navMarks.innerHTML = createHTML();
                     this.marks = {};
@@ -3628,12 +3635,13 @@
                             if (typeof word.hideParent !== 'undefined') {
                                 let parentDepth = word.hideParent;
                                 let parent = node.parentElement;
-                                while(parentDepth-- > 0) {
+                                while(parentDepth-- > 0 && parent) {
                                     parent = parent.parentElement;
                                 }
                                 if (parent) {
                                     parent.dataset.content = word.content;
                                     parent.classList.add("searchJumper-hide");
+                                    node.classList.add("searchJumper-hide");
                                 }
                             }
                             let curList = self.marks[word.content];
