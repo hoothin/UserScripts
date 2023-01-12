75a68ebf13454f2dfe1c9fcd052db873bcf39b46
hoothin <rixixi@gmail.com>
2023-01-12 10:00:37 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.54.6
+// @version      1.6.6.54.7
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -6232,10 +6232,20 @@
                         let url = getUrl();
                         if ((data.charset && data.charset != 'utf-8') || /[:%]p{/.test(data.url)) {
                             if (url === false) return false;
+                            let jumpFrom = data.url.match(/#jumpFrom{(.*?)}/);
+                            let processPostUrl = _url => {
+                                storage.setItem("postUrl", [_url, data.charset]);
+                                if (jumpFrom) {
+                                    _url = jumpFrom[1];
+                                } else {
+                                    _url = _url.replace(/\?.*/, "").replace(/[:%]p{.*/, '').replace(/[^\/]*$/, '');
+                                }
+                                return _url;
+                            };
                             if (url.indexOf('%input{') !== -1) {
                                 self.showCustomInputWindow(url, _url => {
-                                    storage.setItem("postUrl", [_url, data.charset]);
-                                    ele.href = _url.replace(/\?.*/, "").replace(/[:%]p{.*/, '');
+                                    _url = processPostUrl(_url);
+                                    ele.href = _url;
                                     if (ele.target === '_blank') {
                                         _GM_openInTab(ele.href, {active: true});
                                     } else {
@@ -6244,8 +6254,7 @@
                                 });
                                 return;
                             } else {
-                                storage.setItem("postUrl", [url, data.charset]);
-                                url = url.replace(/\?.*/, '').replace(/[:%]p{.*/, '').replace(/[^\/]*$/, '');
+                                url = processPostUrl(url);
                             }
                         }
                         let alt = e && e.altKey;
@@ -9539,7 +9548,7 @@
 
 
         storage.getItem("postUrl", postUrl => {
-            if (postUrl && postUrl[0].indexOf(location.host) != -1) {
+            if (postUrl && postUrl[0].indexOf(location.host.replace(/.*\.(\w+\.\w+)/, "$1")) != -1) {
                 storage.setItem("postUrl", '');
                 submitByForm(postUrl[1], postUrl[0], '_self');
             } else {
