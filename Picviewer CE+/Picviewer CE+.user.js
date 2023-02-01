aa6b7fe7baa5a7fe705383761dd2df8ae65e8f43
hoothin <rixixi@gmail.com>
2023-02-01 18:47:33 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.2.1.1
+// @version              2023.2.1.2
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -20550,6 +20550,16 @@ ImgOps | https://imgops.com/#b#`;
                     case 'magnifier':
                         new MagnifierC(this.img,this.data);
                         break;
+                    case 'download':
+                        {
+                            let copy = prefs.floatBar.download2copy;
+                            if (copy) {
+                                _GM_setClipboard(this.data.src || this.data.imgSrc);
+                            } else {
+                                _GM_download(this.data.src||this.data.imgSrc, (this.data.img.alt || this.data.img.title), prefs.saveName);
+                            }
+                        }
+                        break;
                     case 'actual':
                     case 'search':
                     case 'current':
@@ -20940,7 +20950,7 @@ ImgOps | https://imgops.com/#b#`;
                 },100);
             },
             open:function(e,buttonType){
-                if (buttonType === 'download') {
+                if (buttonType === 'download' && !this.data.xhr) {
                     let copy = prefs.floatBar.download2copy;
                     if (e && (e.ctrlKey || e.altKey || e.shiftKey || e.metaKey)) copy = !copy;
                     if (copy) {
