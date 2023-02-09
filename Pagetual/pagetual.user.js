b5231d06fe1c2f872e6a2f34183fb6c4f399e6d4
hoothin <rixixi@gmail.com>
2023-02-09 11:17:35 +0800

    Update pagetual.user.js

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:it      Pagetual
 // @name:ko      東方永頁機
 // @namespace    hoothin
-// @version      1.9.33.51
+// @version      1.9.33.52
 // @description  Perpetual pages - Most powerful auto-pager script. Auto loading next paginated web pages and inserting into current page. Support thousands of web sites without any rule.
 // @description:zh-CN  自动翻页 - 加载并拼接下一分页内容至当前页尾，无需规则自动适配任意网页
 // @description:zh-TW  自動翻頁 - 加載並拼接下一分頁內容至當前頁尾，無需規則自動適配任意網頁
@@ -2489,7 +2489,7 @@
             if (this.inited) return;
             this.inited = true;
             let self = this;
-            let cssText = `
+            this.cssText = `
              #pagetual-sideController {
                  position: fixed;
                  top: calc(50% - 75px);
@@ -2530,7 +2530,7 @@
                  color: red;
              }
             `;
-            _GM_addStyle(cssText);
+            this.styleEle = _GM_addStyle(this.cssText);
             let frame = document.createElement("div");
             frame.id = "pagetual-sideController";
             frame.innerHTML = createHTML(`
@@ -2645,6 +2645,12 @@
 
         addToStage() {
             this.init();
+            if (!this.styleEle || !this.styleEle.parentNode) {
+                this.styleEle = _GM_addStyle(this.cssText);
+            }
+            if (!isPause) {
+                this.frame.classList.remove("stop");
+            }
             document.body.appendChild(this.frame);
         }
 
@@ -2668,7 +2674,7 @@
 
         init() {
             let self = this;
-            let cssText = `
+            this.cssText = `
              #pagetual-nextSwitch {
               position: fixed;
               top: 10px;
@@ -2730,7 +2736,7 @@
               transform: scale(1.2);
              }
             `;
-            _GM_addStyle(cssText);
+            this.styleEle = _GM_addStyle(this.cssText);
             let frame = document.createElement("div");
             frame.id = "pagetual-nextSwitch";
             frame.innerHTML = createHTML(`
@@ -2785,6 +2791,9 @@
         }
 
         start() {
+            if (!this.styleEle || !this.styleEle.parentNode) {
+                this.styleEle = _GM_addStyle(this.cssText);
+            }
             document.documentElement.appendChild(this.frame);
         }
 
@@ -2809,7 +2818,7 @@
         init() {
             let self = this;
             this.signList = [];
-            let cssText = `
+            this.cssText = `
              body.pagetual-picker,
              body.pagetual-picker *:hover,
              body.pagetual-picker a:hover {
@@ -2961,7 +2970,7 @@
               color: orangered;
              }
             `;
-            _GM_addStyle(cssText);
+            this.styleEle = _GM_addStyle(this.cssText);
             this.mainSignDiv = this.createSignDiv();
             this.allSignDiv = [];
             let frame = document.createElement("div");
@@ -3263,6 +3272,9 @@
         start() {
             if (this.inPicker) return;
             this.inPicker = true;
+            if (!this.styleEle || !this.styleEle.parentNode) {
+                this.styleEle = _GM_addStyle(this.cssText);
+            }
             document.documentElement.appendChild(this.frame);
             document.body.appendChild(this.mainSignDiv);
             document.body.classList.add("pagetual-picker");
