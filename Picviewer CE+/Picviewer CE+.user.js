083ea49da624f96be871bb2da54979c9a50ebf60
hoothin <rixixi@gmail.com>
2023-02-24 11:50:29 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.2.19.1
+// @version              2023.2.24.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -16868,6 +16868,7 @@ ImgOps | https://imgops.com/#b#`;
                     background-clip: padding-box;\
                     background-color: rgba(20,20,20,0.75);\
                     position:relative;\
+                    transition: background-color .3s ease;\
                     }\
                     .pv-gallery-img-container-top {\
                     border-top: '+ prefs.gallery.sidebarSize +'px solid transparent;\
@@ -17034,7 +17035,7 @@ ImgOps | https://imgops.com/#b#`;
                     display:none;\
                     }\
                     .pv-gallery-container.pv-gallery-sidebar-toggle-hide>.pv-gallery-body>.pv-gallery-img-container>span.pv-gallery-sidebar-toggle{\
-                    opacity: 0.1;\
+                    opacity: 0.6;\
                     transition: opacity .3s ease;\
                     }\
                     .pv-gallery-container.pv-gallery-sidebar-toggle-hide>.pv-gallery-body>.pv-gallery-img-container>span.pv-gallery-sidebar-toggle:hover{\
@@ -17046,6 +17047,9 @@ ImgOps | https://imgops.com/#b#`;
                     .pv-gallery-container.pv-gallery-sidebar-toggle-hide>.pv-gallery-head{\
                     opacity: 0.1;\
                     transition: opacity .3s ease;\
+                    }\
+                    .pv-gallery-container.pv-gallery-sidebar-toggle-hide>.pv-gallery-body>.pv-gallery-img-container{\
+                    background-color: black;\
                     }\
                     .pv-gallery-container.pv-gallery-sidebar-toggle-hide>.pv-gallery-head:hover{\
                     opacity: 1;\
