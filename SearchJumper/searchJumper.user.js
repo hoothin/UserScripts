74b74643afc1ac677cc15a7ec37a188ff308c735
hoothin <rixixi@gmail.com>
2023-03-09 08:44:17 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.55.18
+// @version      1.6.6.55.19
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -4087,13 +4087,18 @@
                     if (siteBtn.dataset.name == siteName) {
                         let mouseDownEvent = new PointerEvent("mousedown", {altKey: e.altKey, ctrlKey: e.ctrlKey, shiftKey: e.shiftKey, metaKey: e.metaKey})
                         siteBtn.dispatchEvent(mouseDownEvent);
+                        let isPage = /^(https?|ftp):/.test(siteBtn.href);
+                        if (isPage) {
+                            siteBtn.setAttribute("target", "_blank");
+                        }
                         if (!this.customInput) {
-                            if (siteBtn.onclick) {
-                                siteBtn.onclick(e);
-                            } else {
+                            if (siteBtn.onclick || !isPage) {
                                 siteBtn.click();
+                            } else {
+                                _GM_openInTab(siteBtn.href, {active: true});
                             }
                         }
+                        siteBtn.setAttribute("target", siteBtn.dataset.target==1?"_blank":"");
                         return;
                     }
                 }
