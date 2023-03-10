6679cae7c5e6159283625b8bd7426514cb746063
hoothin <rixixi@gmail.com>
2023-03-10 17:50:54 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.55.22
+// @version      1.6.6.55.23
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -1836,7 +1836,7 @@
                      bottom: unset;
                      top: 5%;
                  }
-                 .search-jumper-type.not-expand>a:nth-of-type(10)~a:not(.historySite) {
+                 .search-jumper-type.not-expand>a:nth-of-type(10)~a {
                      display: none;
                  }
                  #search-jumper .sitelist {
@@ -5199,9 +5199,15 @@
                         }
                         if (findSame) continue;
                         btn.classList.add("historySite");
-                        if (self.searchJumperExpand.parentNode == typeEle) {
-                            typeEle.insertBefore(btn, self.searchJumperExpand);
-                        } else typeEle.appendChild(btn);
+                        if (searchData.prefConfig.historyInsertFirst) {
+                            if (typeEle.children.length > 1) {
+                                typeEle.insertBefore(btn, typeEle.children[1]);
+                            } else typeEle.appendChild(btn);
+                        } else {
+                            if (self.searchJumperExpand.parentNode == typeEle) {
+                                typeEle.insertBefore(btn, self.searchJumperExpand);
+                            } else typeEle.appendChild(btn);
+                        }
                         if (++num >= searchData.prefConfig.historyLength) break;
                     }
                 }
