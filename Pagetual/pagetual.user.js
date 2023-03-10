63b9b13450bda3a4a7d6c674617a0a49b80f4ecd
hoothin <rixixi@gmail.com>
2023-03-10 14:48:00 +0800

    Update pagetual.user.js

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:it      Pagetual
 // @name:ko      東方永頁機
 // @namespace    hoothin
-// @version      1.9.34.18
+// @version      1.9.34.19
 // @description  Perpetual pages - Most powerful auto-pager script. Auto loading next paginated web pages and inserting into current page. Support thousands of web sites without any rule.
 // @description:zh-CN  终极自动翻页 - 加载并拼接下一分页内容至当前页尾，智能适配任意网页
 // @description:zh-TW  終極自動翻頁 - 加載並拼接下一分頁內容至當前頁尾，智能適配任意網頁
@@ -2752,6 +2752,7 @@
             if (!isPause) {
                 this.frame.classList.remove("stop");
             }
+            if (this.frame.parentNode) return;
             document.body.appendChild(this.frame);
             clearTimeout(this.hideTimer);
             this.hideTimer = setTimeout(() => {
@@ -5437,12 +5438,8 @@
             if (nextPageBar) {
                 scrollToPageBar(nextPageBar);
             } else {
-                let nextEle = pageBar.parentNode.children[pageBar.parentNode.children.length - 1];
-                if (nextEle) scrollToPageBar(nextEle);
-                else {
-                    scrollH = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
-                    window.scrollTo({ top: scrollH || 9999999, behavior: 'smooth'});
-                }
+                scrollH = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+                window.scrollTo({ top: scrollH || 9999999, behavior: 'smooth'});
             }
         });
         if (!rulesData.hideBarArrow) {
