1e5227264e69630ffec8c9d05d44c068933368d1
hoothin <rixixi@gmail.com>
2023-03-21 15:53:00 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.55.33
+// @version      1.6.6.55.34
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -5275,6 +5275,10 @@
                             }
                         }
                         if (findSame) continue;
+                        if (btn.children.length && btn.children[0].dataset.src) {
+                            btn.children[0].src = btn.children[0].dataset.src;
+                            btn.children[0].dataset.src = '';
+                        }
                         btn.classList.add("historySite");
                         if (!init && searchData.prefConfig.historyInsertFirst) {
                             if (typeEle.children.length > 1) {
