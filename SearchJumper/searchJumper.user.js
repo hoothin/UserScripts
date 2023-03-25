a9745627f03fca42ebe04c1c796c8d6be7f1ea67
hoothin <rixixi@gmail.com>
2023-03-25 10:29:27 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.55.36
+// @version      1.6.6.55.37
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -2922,7 +2922,14 @@
                     if (!this.lockWords) {
                         this.highlight("");
                         this.highlightSpans = {};
-                    } else this.highlight("insert");
+                    } else {
+                        this.highlight("insert");
+                        for (let i in this.highlightSpans) {
+                            let span = this.highlightSpans[i];
+                            let curList = this.marks[i];
+                            this.setHighlightSpan(span, 0, curList.length);
+                        }
+                    }
                     return wordSpans;
                 }
                 let targetWords = this.anylizeInPageWords(words, this.lockWords, !!init);
@@ -7993,7 +8000,8 @@
                     if (keywordsMatch) {
                         keywords = new URLSearchParams(location.search).get(keywordsMatch[1]);
                     }
-                } else {
+                }
+                if (!keywords) {
                     keywordsMatch = currentSite.url.match(/https?:\/\/[^\/]*\/(.*)%s\b/);
                     if (keywordsMatch) {
                         keywordsMatch = location.href.match(new RegExp((keywordsMatch[1] || (location.host.replace(/\./g, "\\.") + "/")) + "(.*?)(\/|$)"));
