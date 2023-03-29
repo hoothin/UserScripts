5f8ce5cd7b31ab86a30a0c7f9dea0c4f3ab9a4d0
hoothin <rixixi@gmail.com>
2023-03-29 14:56:12 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.55.40
+// @version      1.6.6.55.41
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -4418,8 +4418,9 @@
                 });
                 //Search in page
 
-                this.searchJumperExpand.addEventListener("click", e => {
+                let expandTypeHandler = e => {
                     let typeEle = this.searchJumperExpand.parentNode;
+                    if (!typeEle || !typeEle.classList.contains('not-expand')) return;
                     typeEle.classList.remove('not-expand');
                     let leftRight = this.con.classList.contains("search-jumper-left") ||
                         this.con.classList.contains("search-jumper-right");
@@ -4435,7 +4436,15 @@
                     setTimeout(() => {
                         self.checkScroll();
                     }, 251);
-                });
+                };
+                this.searchJumperExpand.addEventListener("click", expandTypeHandler);
+                if (searchData.prefConfig.overOpen) {
+                    this.searchJumperExpand.addEventListener('mouseenter', e => {
+                        setTimeout(() => {
+                            expandTypeHandler(e);
+                        }, 500);
+                    }, false);
+                }
                 this.pickerBtn.addEventListener("click", e => {
                     this.searchJumperInputKeyWords.value = "";
                     this.togglePicker();
