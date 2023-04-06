eaebb1a673bc5247ca9552ecb953ab87c659feb4
hoothin <rixixi@gmail.com>
2023-04-06 18:51:51 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.55.50
+// @version      1.6.6.55.51
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -724,7 +724,8 @@
         selectToShow: true,
         expandType: false,
         rightMouse: true,
-        shiftLastUsedType: true
+        shiftLastUsedType: true,
+        mouseLeaveToHide: true
     };
     function run() {
         let lang = navigator.appName == "Netscape" ? navigator.language : navigator.userLanguage;
@@ -2699,10 +2700,23 @@
                 alllist.appendChild(historylist);
                 this.historylist = historylist;
 
-                let enterHandler = e => {
-                    bar.classList.remove("initShow");
-                };
-                bar.addEventListener('mouseenter', enterHandler, false);
+                bar.addEventListener('mouseenter', e => {
+                    if (bar.classList.contains("grabbing")) return;
+                    if (this.hideTimeout) {
+                        clearTimeout(this.hideTimeout);
+                    }
+                    this.checkScroll(true);
+                    if (searchData.prefConfig.mouseLeaveToHide) {
+                        bar.classList.remove("initShow");
+                    }
+                }, false);
+                if (searchData.prefConfig.mouseLeaveToHide) {
+                    bar.addEventListener('mouseleave', e => {
+                        if (bar.classList.contains("grabbing")) return;
+                        this.waitForHide();
+                    }, false);
+                }
+
                 if (searchData.prefConfig.initShow) {
                     bar.classList.add("initShow");
                 } else {
@@ -4535,18 +4549,6 @@
                     });
                 }
                 cacheImgManager();
-
-                this.bar.addEventListener('mouseenter', e => {
-                    if (this.bar.classList.contains("grabbing")) return;
-                    if (this.hideTimeout) {
-                        clearTimeout(this.hideTimeout);
-                    }
-                    this.checkScroll(true);
-                }, false);
-                this.bar.addEventListener('mouseleave', e => {
-                    if (this.bar.classList.contains("grabbing")) return;
-                    this.waitForHide();
-                }, false);
 
                 if (lastSign) {
                     targetElement = lastSign.target;
@@ -10618,6 +10620,9 @@
             }
             if (typeof searchData.prefConfig.rightMouse === "undefined") {
                 searchData.prefConfig.rightMouse = true;
+            }
+            if (typeof searchData.prefConfig.mouseLeaveToHide === "undefined") {
+                searchData.prefConfig.mouseLeaveToHide = true;
             }
             if (typeof searchData.prefConfig.suggestType === "undefined") {
                 if (lang === "zh-CN") {
