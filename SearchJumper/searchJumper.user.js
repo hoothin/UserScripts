24a12d9e40fc0671343174ab8f227339d6ea5364
hoothin <rixixi@gmail.com>
2023-04-07 20:04:22 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -2950,7 +2950,7 @@
                         for (let i in this.highlightSpans) {
                             let span = this.highlightSpans[i];
                             let curList = this.marks[i];
-                            this.setHighlightSpan(span, 0, curList.length);
+                            this.setHighlightSpan(span, 0, curList ? curList.length : 1);
                         }
                     }
                     return wordSpans;
@@ -3009,7 +3009,8 @@
                     removeBtn.innerHTML = createHTML(`<svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><title>${i18n("removeBtn")}</title><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64z m165.4 618.2l-66-0.3L512 563.4l-99.3 118.4-66.1 0.3c-4.4 0-8-3.5-8-8 0-1.9 0.7-3.7 1.9-5.2l130.1-155L340.5 359c-1.2-1.5-1.9-3.3-1.9-5.2 0-4.4 3.6-8 8-8l66.1 0.3L512 464.6l99.3-118.4 66-0.3c4.4 0 8 3.5 8 8 0 1.9-0.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"></path></svg>`);
                     wordSpan.appendChild(removeBtn);
 
-                    this.setHighlightSpan(wordSpan, -1, this.marks[word.content].length);
+                    let curList = this.marks[word.content];
+                    this.setHighlightSpan(wordSpan, -1, curList ? curList.length : 1);
                     this.highlightSpans[word.content] = wordSpan;
 
                     this.searchInPageLockWords.appendChild(wordSpan);
