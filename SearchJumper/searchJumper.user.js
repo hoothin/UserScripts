e97d60736d4d129564ac3fa66b2728f0287916fd
hoothin <rixixi@gmail.com>
2023-04-16 20:34:25 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.55.61
+// @version      1.6.6.55.62
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -2273,7 +2273,7 @@
                      height: 33px;
                      position: absolute;
                      user-select: none;
-                     background: rgb(0 0 0 / 50%);
+                     background: #212022;
                      white-space: nowrap;
                      overflow: hidden;
                      display: flex;
@@ -4079,7 +4079,10 @@
                         };
                     }
                     document.addEventListener("keydown", self.showAllKeydownHandler);
-                    if (this.searchJumperInputKeyWords.value) this.searchJumperInputKeyWords.focus();
+                    if (this.searchJumperInputKeyWords.value) {
+                        this.searchJumperInputKeyWords.focus();
+                        this.searchJumperInputKeyWords.select();
+                    }
                 }, 0);
             }
 
@@ -4143,6 +4146,7 @@
                     } else if (!this.searchJumperInputKeyWords.value && currentSite) {
                         this.searchJumperInputKeyWords.value = getKeywords() || cacheKeywords;
                         this.searchJumperInputKeyWords.focus();
+                        this.searchJumperInputKeyWords.select();
                     }
                     let firstType = this.bar.querySelector('.search-jumper-needInPage:not(.notmatch)>span');
                     if (firstType && !firstType.parentNode.classList.contains('search-jumper-open')) {
