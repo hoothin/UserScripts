0c0f22ba184436924e72996bb67427e62ccdb615
hoothin <rixixi@gmail.com>
2023-05-06 19:47:39 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.6.82.64
+// @version      1.6.6.83.64
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -8379,7 +8379,7 @@
                     if (cache == 'data:,' || !cache) cache = 'fail';
                     cacheIcon[src] = cache;
                     storage.setItem("cacheIcon", cacheIcon);
-                    debug(src + " cached");
+                    debug(src + " cached, left " + cachePool.length + " icons");
                 }
             } else {
                 cacheFontIcon(target);
@@ -9271,7 +9271,9 @@
                 document.addEventListener('received', e => {
                     received = true;
                     clearTimeout(sendMessageTimer);
-                    debug("Start cache!");
+                    if (cachePool.length > 0) {
+                        debug(`Start cache ${cachePool.length} icons!`);
+                    }
                     cacheImgManager();
                 });
 
@@ -9369,6 +9371,8 @@
                         storage.setItem("cacheIcon", newCache);
                         if (searchData.prefConfig.cacheSwitch) {
                             _GM_notification(i18n('startCache'));
+                            searchBar.con.classList.add("in-input");
+                            searchBar.con.style.visibility = "hidden";
                             searchBar.appendBar();
                             setTimeout(() => {
                                 cacheFontManager(true);
