8f979934d8303765ed3395584cba802bcc59059d
hoothin <rixixi@gmail.com>
2023-05-08 07:57:32 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.8.88.64
+// @version      1.6.9.88.64
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -9192,7 +9192,7 @@
                 while (parentForm) {
                     if (parentForm.nodeName.toUpperCase() === "FORM") {
                         let target = parentForm.target;
-                        if (target && target != '_blank' && target != '_self' && target != '_parent' && target != '_top') {
+                        if (target && typeof target == "string" && target != '_blank' && target != '_self' && target != '_parent' && target != '_top') {
                             let targetIframe = getBody(document).querySelector(target);
                             if (!targetIframe) {
                                 parentForm = null;
