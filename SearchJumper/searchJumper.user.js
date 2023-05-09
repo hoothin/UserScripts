0eede755c8f307642bc3a0454a935438f4c16736
hoothin <rixixi@gmail.com>
2023-05-09 19:55:52 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.11.88.64
+// @version      1.6.12.88.64
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -1984,7 +1984,7 @@
                      background-color: #212022;
                      color: white;
                      border: none;
-                     font-size: 20px;
+                     font-size: 16px;
                      height: 35px;
                      margin-bottom: 0;
                      padding: 5px;
@@ -3898,13 +3898,11 @@
                     } else if ((!root || node === ele) &&
                                node.nodeType == 1 &&
                                node.childNodes &&
-                               node.nodeName.toUpperCase() != "SCRIPT" &&
-                               node.nodeName.toUpperCase() != "STYLE" &&
-                               node.nodeName.toUpperCase() != "TEXTAREA" &&
+                               !/^(SCRIPT|STYLE|TEXTAREA|MARK)$/i.test(node.nodeName) &&
                                node.contentEditable != 'true' &&
                                node.ariaHidden != 'true' &&
-                               (!node.hasAttribute || node.hasAttribute('jsname') == false) &&
-                               node.nodeName.toUpperCase() != "MARK") {
+                               node.role != "search" &&
+                               (!node.hasAttribute || node.hasAttribute('jsname') == false)) {
                         if (!searchingPre && (node.nodeName.toUpperCase() === "PRE" || node.nodeName.toUpperCase() === "CODE")) {
                             preEles.push(node);
                         } else {
