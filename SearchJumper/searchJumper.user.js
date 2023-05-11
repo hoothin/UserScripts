4dafc963110f0235e86459e8f26e1dc46404b4f8
hoothin <rixixi@gmail.com>
2023-05-11 10:58:01 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.13.88.64
+// @version      1.6.14.88.64
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -3804,7 +3804,7 @@
                     let len, pos = -1, skip, spannode, middlebit, middleclone;
                     skip = 0;
                     let pa = node.parentNode;
-                    if (node.nodeType == 3 && (typeof word.hideParent !== 'undefined' || /^BODY$/i.test(pa.nodeName) || pa.offsetParent || (pa.scrollHeight && pa.scrollWidth))) {
+                    if (node.nodeType == 3 && node.data && (typeof word.hideParent !== 'undefined' || /^BODY$/i.test(pa.nodeName) || pa.offsetParent || (pa.scrollHeight && pa.scrollWidth))) {
                         if (word.isRe) {
                             let wordMatch = node.data.match(new RegExp(word.content, word.reCase));
                             if (wordMatch) {
@@ -3813,8 +3813,16 @@
                                 pos = node.data.indexOf(content);
                             }
                         } else {
-                            len = word.content.length;
-                            pos = node.data.toUpperCase().indexOf(word.content.toUpperCase());
+                            if (_unsafeWindow.searchJumperPinyin) {
+                                let pinyin = _unsafeWindow.searchJumperPinyin(node.data, word.content);
+                                if (pinyin.matched) {
+                                    len = pinyin.len;
+                                    pos = pinyin.pos;
+                                } else pos = -1;
+                            } else {
+                                len = word.content.length;
+                                pos = node.data.toUpperCase().indexOf(word.content.toUpperCase());
+                            }
                             if (pos >= 0 && /^[a-z]+$/i.test(word.content)) {
                                 if (pos !== 0 && /[a-z]/i.test(node.data[pos - 1])) {
                                     pos = -1;
