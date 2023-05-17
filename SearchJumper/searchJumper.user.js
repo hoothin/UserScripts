d775b678617b72faf061f8f190f781194446c18e
hoothin <rixixi@gmail.com>
2023-05-17 17:28:59 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.24.88.64
+// @version      1.6.25.88.64
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -6874,11 +6874,6 @@
                     let keywords = _keyWords || self.searchJumperInputKeyWords.value || getSelectStr();
                     if (!keywords) {
                         keywords = getKeywords();
-                        if (!keywords && hasWordParam) {
-                            if (typeof navigator.clipboard.readText !== "undefined") {
-                                keywords = await navigator.clipboard.readText();
-                            }
-                        }
                     }
                     if (keywords && !_keyWords) {
                         if (keywords != cacheKeywords) {
@@ -7078,6 +7073,12 @@
                     }
                     let targetBaseUrl = targetUrl.replace(/^https?:\/\//i, "");
                     if (!keywords) keywords = (currentSite && cacheKeywords);
+                    if (!keywords && hasWordParam && typeof navigator.clipboard.readText !== "undefined") {
+                        keywords = await navigator.clipboard.readText();
+                        if (keywords && !_keyWords) {
+                            inputString = keywords;
+                        }
+                    }
                     if (!keywords && hasWordParam) {
                         self.customInput = true;
                         if (self.con.classList.contains("search-jumper-showall")) return false;
