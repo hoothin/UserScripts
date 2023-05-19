d1c4aadec08dc1d0f204d130b26d1e8575e47d21
hoothin <rixixi@gmail.com>
2023-05-19 09:05:04 +0800

    Update pagetual.user.js

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:it      Pagetual
 // @name:ko      東方永頁機
 // @namespace    hoothin
-// @version      1.9.36.26
+// @version      1.9.36.27
 // @description  Perpetual pages - Most powerful auto-pager script. Auto loading next paginated web pages and inserting into current page. Support thousands of web sites without any rule.
 // @description:zh-CN  终极自动翻页 - 加载并拼接下一分页内容至当前页尾，智能适配任意网页
 // @description:zh-TW  終極自動翻頁 - 加載並拼接下一分頁內容至當前頁尾，智能適配任意網頁
@@ -2838,6 +2838,7 @@
                 }
                 if (realSrc) {
                     img.src = realSrc;
+                    img.removeAttribute("data-src");
                     if (img.style.display == "none") {
                         img.style.display = "";
                     }
@@ -3577,6 +3578,7 @@
               float: right;
               cursor: pointer;
               overflow: hidden;
+              background-color: unset!important;
              }
              #pagetual-picker button:hover {
               transform: scale(1.2);
@@ -3697,6 +3699,8 @@
               text-align: center;
               font-size: large;
               margin-top: 10px;
+              box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.12);
+              border-radius: 5px;
              }
              #pagetual-picker .command:hover {
               color: orangered;
@@ -7021,6 +7025,8 @@
                 }
                 if (!fitWidth && iframe.style.marginLeft == '0px') {
                     iframe.style.width = "100vw";
+                    iframe.style.maxWidth = "100vw";
+                    iframe.style.minWidth = "100vw";
                     var cWidth = document.body.clientWidth || document.documentElement.clientWidth;
                     var iWidth = window.innerWidth;
                     iframe.style.marginLeft = -iframe.getBoundingClientRect().left - (iWidth - cWidth) / 2 + "px";
