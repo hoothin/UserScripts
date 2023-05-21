7fe5f97bb48702c0898ebe405dae1c5a352077da
hoothin <rixixi@gmail.com>
2023-05-21 14:41:05 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.28.88.64
+// @version      1.6.29.88.64
 // @description  Assistant for switching search engines. Jump to any search engine quickly, can also search anything (selected text / image / link) on any engine with a simple right click or a variety of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持任意页面右键划词搜索与全面自定义
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支持任意頁面右鍵劃詞搜尋與全面自定義
@@ -9713,7 +9713,7 @@
                             }
                             type.sites.forEach(site => {
                                 let icon = site.icon;
-                                if (!icon) icon = site.url.replace(/^(https?:\/\/[^\/]*\/).*$/, "$1favicon.ico");
+                                if (!icon) icon = site.url.replace(/^showTips:(https?:\/\/[^\/]*\/).*$/, "$1favicon.ico");
                                 if (/^http/.test(icon)) {
                                     let siteCache = cacheIcon[icon];
                                     if (siteCache) {
@@ -9736,7 +9736,7 @@
                             }
                             type.sites.forEach(site => {
                                 let icon = site.icon;
-                                if (!icon) icon = site.url.replace(/^(https?:\/\/[^\/]*\/).*$/, "$1favicon.ico");
+                                if (!icon) icon = site.url.replace(/^showTips:(https?:\/\/[^\/]*\/).*$/, "$1favicon.ico");
                                 if (/^http/.test(icon)) {
                                     let siteCache = cacheIcon[icon];
                                     if (siteCache && siteCache !== 'fail') {
