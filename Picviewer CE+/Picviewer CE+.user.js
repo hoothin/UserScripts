61083a88b8953d377012e5250c367841fa0826b5
hoothin <rixixi@gmail.com>
2023-05-21 18:08:28 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.5.13.1
+// @version              2023.5.21.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -22656,7 +22656,8 @@ ImgOps | https://imgops.com/#b#`;
                     _GM_openInTab('https://pixlr.com/x/', {active:true});
                 }else{
                     storage.setItem("editUrl", "");
-                    if(/^https:\/\/pixlr\.com\/x/.test(location.href)){
+                    if(/^https:\/\/pixlr\.com\//.test(location.href)){
+                        await sleep(1000);
                         await clickEle('#home-open-url');
                         await input('#image-url', src);
                         await clickEle('.dialog>.buttons>a.button.positive');
@@ -22669,7 +22670,8 @@ ImgOps | https://imgops.com/#b#`;
                     _GM_openInTab('https://pixlr.com/e/', {active:true});
                 }else{
                     storage.setItem("editUrl", "");
-                    if(/^https:\/\/pixlr\.com\/e/.test(location.href)){
+                    if(/^https:\/\/pixlr\.com\//.test(location.href)){
+                        await sleep(1000);
                         await clickEle('#home-open-url');
                         await input('#image-url', src);
                         await clickEle('.dialog>.buttons>a.button.positive');
