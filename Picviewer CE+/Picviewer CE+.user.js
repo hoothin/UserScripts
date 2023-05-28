37d66ec3e7c7d91115b7e8229f910305f4ce30b5
hoothin <rixixi@gmail.com>
2023-05-28 18:20:31 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.5.21.1
+// @version              2023.5.28.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -41,7 +41,7 @@
 // @grant                GM.notification
 // @grant                unsafeWindow
 // @require              https://greasyfork.org/scripts/6158-gm-config-cn/code/GM_config%20CN.js?version=23710
-// @require              https://greasyfork.org/scripts/438080-pvcep-rules/code/pvcep_rules.js?version=1188180
+// @require              https://greasyfork.org/scripts/438080-pvcep-rules/code/pvcep_rules.js?version=1196514
 // @require              https://greasyfork.org/scripts/440698-pvcep-lang/code/pvcep_lang.js?version=1185366
 // @downloadURL          https://greasyfork.org/scripts/24204-picviewer-ce/code/Picviewer%20CE+.user.js
 // @updateURL            https://greasyfork.org/scripts/24204-picviewer-ce/code/Picviewer%20CE+.user.js
@@ -18171,9 +18171,10 @@ ImgOps | https://imgops.com/#b#`;
         };
 
         //图片窗口
-        function ImgWindowC(img, data){
+        function ImgWindowC(img, data, actual){
             this.loaded=false;
             this.img=img;
+            this.actual=!!actual;
             this.src=data?data.src:img.src;
             this.data = data;
 
@@ -18710,7 +18711,7 @@ ImgOps | https://imgops.com/#b#`;
                                 }
                                 if (imgData && imgData.img) {
                                     this.remove();
-                                    new LoadingAnimC(imgData, "current", false, true);
+                                    new LoadingAnimC(imgData, (this.actual ? "actual" : "current"), false, true);
                                 }
                             }
                         } else {
@@ -18724,7 +18725,7 @@ ImgOps | https://imgops.com/#b#`;
                                 }
                                 if (imgData) {
                                     this.remove();
-                                    new LoadingAnimC(imgData, "current", false, true);
+                                    new LoadingAnimC(imgData, (this.actual ? "actual" : "current"), false, true);
                                 }
                             }
                         }
@@ -20895,7 +20896,7 @@ ImgOps | https://imgops.com/#b#`;
                     case 'current':
                     case 'original'://original 是为了兼容以前的规则
                         if(this.data.src!=this.img.src)this.data.src=this.img.src;
-                        new ImgWindowC(this.img, this.data);
+                        new ImgWindowC(this.img, this.data, this.buttonType == 'actual');
                         break;
                 };
             },
