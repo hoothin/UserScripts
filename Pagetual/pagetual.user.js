0d123dcaccd89dcc906f1687e70aec3403927938
hoothin <rixixi@gmail.com>
2023-06-16 12:04:13 +0800

    1.9.36.38

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:it      Pagetual
 // @name:ko      東方永頁機
 // @namespace    hoothin
-// @version      1.9.36.37
+// @version      1.9.36.38
 // @description  Perpetual pages - powerful auto-pager script. Auto loading next paginated web pages and inserting into current page. Support thousands of web sites without any rule.
 // @description:zh-CN  终极自动翻页 - 加载并拼接下一分页内容至当前页尾，智能适配任意网页
 // @description:zh-TW  終極自動翻頁 - 加載並拼接下一分頁內容至當前頁尾，智能適配任意網頁
@@ -1446,9 +1446,12 @@
             }
         }
 
-        getValidHeight(ele) {
+        getValidHeight(ele, win) {
             if (!ele.offsetParent) return 0;
             let h = ele.scrollHeight;
+            if (win && win.getComputedStyle(ele).overflow == "hidden") {
+                h = ele.offsetHeight;
+            }
             if (h === 0 && ele.parentNode && ele.parentNode.children.length === 1) {
                 h = ele.parentNode.scrollHeight;
             }
@@ -1567,7 +1570,7 @@
                         }
                         if (!hasText) {
                             ele = ele.children[0];
-                            curHeight = self.getValidHeight(ele);
+                            curHeight = self.getValidHeight(ele, curWin);
                             curWidth = parseInt(ele.offsetWidth || ele.scrollWidth);
                         }
                     }
@@ -1611,7 +1614,7 @@
                             continue;
                         }
                         if (/^ARTICLE$/i.test(curNode.nodeName)) articleNum++;
-                        let h = self.getValidHeight(curNode);
+                        let h = self.getValidHeight(curNode, curWin);
                         let w = curNode.scrollWidth;
                         if (isNaN(h) || isNaN(w)) continue;
                         isHori = Math.abs(preOffsetTop - curNode.offsetTop) <= 20 ? true : (preOffsetTop == -1 && curNode.nextElementSibling && curNode.nextElementSibling.offsetTop == curNode.offsetTop);
@@ -1624,7 +1627,7 @@
                         }
                         let a = h * w, moreChild = curNode.children[0];
                         while (moreChild) {
-                            let ch = self.getValidHeight(moreChild);
+                            let ch = self.getValidHeight(moreChild, curWin);
                             let cw = moreChild.scrollWidth;
                             if (h < ch) {
                                 h = ch;
