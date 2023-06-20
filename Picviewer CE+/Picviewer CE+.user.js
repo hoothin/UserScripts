fe73e2f51e8609d5b176e170922a3b9e689b8bf5
hoothin <rixixi@gmail.com>
2023-06-20 10:01:27 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.6.13.1
+// @version              2023.6.20.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -20545,14 +20545,17 @@ ImgOps | https://imgops.com/#b#`;
         };
 
         // 载入动画
-        function LoadingAnimC(data,buttonType,waitImgLoad,openInTopWindow){
-            this.args=arrayFn.slice.call(arguments,0);
-            this.data=data;//data
-            this.buttonType=buttonType;//点击的按钮类型
-            this.openInTopWindow=openInTopWindow;//是否在顶层窗口打开，如果在frame里面的话
-            this.waitImgLoad=waitImgLoad;//是否等待完全读取后打开
+        function LoadingAnimC(data, buttonType, waitImgLoad, openInTopWindow) {
+            this.args = arrayFn.slice.call(arguments, 0);
+            if (data.src != data.imgSrc && !data.srcs) {
+                data.srcs = [data.imgSrc];
+            }
+            this.data = data;//data
+            this.buttonType = buttonType;//点击的按钮类型
+            this.openInTopWindow = openInTopWindow;//是否在顶层窗口打开，如果在frame里面的话
+            this.waitImgLoad = waitImgLoad;//是否等待完全读取后打开
             this.init();
-        };
+        }
 
         LoadingAnimC.all=[];
 
