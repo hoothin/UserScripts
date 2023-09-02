a40e9b35984b7909eca1f8c4b5dda0c52871127f
hoothin <rixixi@gmail.com>
2023-09-02 16:24:38 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.29.98
+// @version      1.6.29.99
 // @description  Assistant that assists with the seamless transition between search engines, providing the ability to swiftly navigate to any platform and conduct searches effortlessly. Additionally, it allows for the selection of text, images, or links to be searched on any search engine with a simple right-click or by utilizing a range of menus and shortcuts.
 // @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持划词右键搜索、页内关键词查找与高亮、可视化自定义页内操作等
 // @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支援劃詞右鍵搜尋、頁內關鍵詞查找與高亮、可視化自定義頁內操作等
@@ -7622,6 +7622,11 @@
                     targetUrlData = "";
                     targetUrlData = await getUrl();
                     if (/^c(opy)?:/.test(data.url) || /^\[/.test(data.url) || /[:%]P{/.test(data.url) || (data.charset && data.charset != 'utf-8') || /[:%]p{/.test(data.url) || self.customInput) {
+                        if (e.button == 1 || e.button == 2) {
+                            ele.href = targetUrlData;
+                            clickHandler();
+                            clicked = false;
+                        }
                     } else {
                         if (!targetUrlData) {
                             //wait for all input stoped
@@ -7649,6 +7654,7 @@
                         }
                         return false;
                     }
+                    if (!e) e = {};
                     let isPage = /^(https?|ftp):/.test(targetUrlData);
                     if (!self.batchOpening && !isBookmark) {
                         let historyLength = Math.max(searchData.prefConfig.historyLength, 20);
