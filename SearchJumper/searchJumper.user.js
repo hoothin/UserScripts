a4a7a7c76a50bbc58ce53f8d466ddfe835d1d6da
hoothin <rixixi@gmail.com>
2023-09-02 17:56:54 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -6,8 +6,8 @@
 // @namespace    hoothin
 // @version      1.6.29.99
 // @description  Assistant that assists with the seamless transition between search engines, providing the ability to swiftly navigate to any platform and conduct searches effortlessly. Additionally, it allows for the selection of text, images, or links to be searched on any search engine with a simple right-click or by utilizing a range of menus and shortcuts.
-// @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换各大搜索引擎，支持划词右键搜索、页内关键词查找与高亮、可视化自定义页内操作等
-// @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換各大搜尋引擎，支援劃詞右鍵搜尋、頁內關鍵詞查找與高亮、可視化自定義頁內操作等
+// @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换搜索引擎，支持划词右键搜索、页内关键词查找与高亮、可视化操作模拟、高级自定义等
+// @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換搜尋引擎，支援劃詞右鍵搜尋、頁內關鍵詞查找與高亮、可視化操作模擬、高級自定義等
 // @description:ja  任意の検索エンジンにすばやく簡単にジャンプします！
 // @author       hoothin
 // @license      MPL-2.0
