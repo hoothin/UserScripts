33dbac943b87db6ce932a56a21bf1b502bbf264d
hoothin <rixixi@gmail.com>
2023-09-05 17:21:40 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,10 +4,10 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.29.99
+// @version      1.6.30.1
 // @description  Assistant that assists with the seamless transition between search engines, providing the ability to swiftly navigate to any platform and conduct searches effortlessly. Additionally, it allows for the selection of text, images, or links to be searched on any search engine with a simple right-click or by utilizing a range of menus and shortcuts.
-// @description:zh-CN  高效搜索引擎辅助增强，在搜索时一键切换搜索引擎，支持划词右键搜索、页内关键词查找与高亮、可视化操作模拟、高级自定义等
-// @description:zh-TW  高效搜尋引擎輔助增强，在搜尋時一鍵切換搜尋引擎，支援劃詞右鍵搜尋、頁內關鍵詞查找與高亮、可視化操作模擬、高級自定義等
+// @description:zh-CN  高效搜索辅助，在搜索时一键切换搜索引擎，支持划词右键搜索、页内关键词查找与高亮、可视化操作模拟、高级自定义等
+// @description:zh-TW  高效搜尋輔助，在搜尋時一鍵切換搜尋引擎，支援劃詞右鍵搜尋、頁內關鍵詞查找與高亮、可視化操作模擬、高級自定義等
 // @description:ja  任意の検索エンジンにすばやく簡単にジャンプします！
 // @author       hoothin
 // @license      MPL-2.0
@@ -5465,7 +5465,7 @@
                     //self.recoveHistory();
                     if (self.funcKeyCall) {
                         self.setFuncKeyCall(false);
-                        if (currentSite && !currentSite.hideNotMatch) {
+                        if (currentSite && !currentSite.hideNotMatch && !searchData.prefConfig.hideOnSearchEngine) {
                             self.initPos();
                             let firstType = self.bar.querySelector('.search-jumper-type:nth-child(1)>span');
                             if (firstType && !firstType.classList.contains("search-jumper-open")) {
