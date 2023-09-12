b7716c392feaa702a0ef5c7fb4b3703c1b6109e7
hoothin <rixixi@gmail.com>
2023-09-12 14:49:43 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.6.30.3
+// @version      1.6.30.4
 // @description  Assistant that assists with the seamless transition between search engines, providing the ability to swiftly navigate to any platform and conduct searches effortlessly. Additionally, it allows for the selection of text, images, or links to be searched on any search engine with a simple right-click or by utilizing a range of menus and shortcuts.
 // @description:zh-CN  高效搜索辅助，在搜索时一键切换搜索引擎，支持划词右键搜索、页内关键词查找与高亮、可视化操作模拟、高级自定义等
 // @description:zh-TW  高效搜尋輔助，在搜尋時一鍵切換搜尋引擎，支援劃詞右鍵搜尋、頁內關鍵詞查找與高亮、可視化操作模擬、高級自定義等
@@ -2174,6 +2174,9 @@
                  .minSizeMode.search-jumper-searchBar>.search-jumper-type:not(.search-jumper-open),
                  .minSizeMode.search-jumper-searchBar:hover>.search-jumper-type:not(.search-jumper-open) {
                      display: none;
+                 }
+                 .minSizeModeClose.minSizeMode.search-jumper-searchBar:hover>.search-jumper-type {
+                     display: inline-flex;
                  }
                  .funcKeyCall>.search-jumper-searchBar>.search-jumper-type:not(.search-jumper-open) {
                      display: none;
@@ -6545,7 +6548,8 @@
                         self.recoveHistory();
                         ele.classList.add("search-jumper-open");
                         if (searchData.prefConfig.minSizeMode) {
-                            self.bar.classList.add("minSizeMode");
+                            //self.bar.classList.add("minSizeMode");
+                            self.bar.classList.remove("minSizeModeClose");
                         }
                         if (sites.length > (searchData.prefConfig.expandTypeLength || 12) && !searchData.prefConfig.expandType) {
                             ele.classList.add("not-expand");
@@ -6606,7 +6610,8 @@
                         });
                     } else {
                         if (searchData.prefConfig.minSizeMode) {
-                            self.bar.classList.remove("minSizeMode");
+                            //self.bar.classList.remove("minSizeMode");
+                            self.bar.classList.add("minSizeModeClose");
                         }
                         ele.classList.remove("search-jumper-open");
                         if (leftRight) {
