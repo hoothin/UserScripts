f3a37ac61fddf4aec93d89b631219d933bd8bae2
hoothin <rixixi@gmail.com>
2023-10-04 15:36:39 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.10.4.1
+// @version              2023.10.4.2
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -23044,7 +23044,7 @@ ImgOps | https://imgops.com/#b#`;
             isTabs: true,
             skin: 'tab',
             frameStyle: {
-                minWidth: "480px",
+                minWidth: "350px",
                 width: ((visualLength((i18n("floatBar") + i18n("magnifier") + i18n("gallery") + i18n("imgWindow") + i18n("others")),"14px","arial,tahoma,myriad pro,sans-serif") + 250) || 480) + 'px',
                 zIndex:'2147483648',
             },
@@ -23059,7 +23059,8 @@ ImgOps | https://imgops.com/#b#`;
                 "#pv-prefs input.color { width: 120px; }",
                 "#pv-prefs input.order { width: 250px; }",
                 "#pv-prefs .config_header>a { border-bottom: solid 2px; }",
-                "#pv-prefs .config_header>a:hover { color: #9f9f9f; }",,
+                "#pv-prefs .config_header>a:hover { color: #9f9f9f; }",
+                "#pv-prefs .nav-tabs { text-wrap: nowrap; width: 95vw; display: flex; overflow-x: auto; overflow-y: visible; }",
             ].join('\n'),
             fields: {
                 // 浮动工具栏
