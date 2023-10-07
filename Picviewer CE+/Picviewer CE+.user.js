21136b4c3c008d294a64428d5c3e36e664781a2d
hoothin <rixixi@gmail.com>
2023-10-07 21:32:50 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.10.7.1
+// @version              2023.10.7.2
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -15782,7 +15782,7 @@ ImgOps | https://imgops.com/#b#`;
                             spanMark.dataset.src=item.src;
                             spanMark.dataset.srcs=item.srcs?item.srcs.join(","):"";
                             if(item.xhr)spanMark.dataset.xhr=encodeURIComponent(JSON.stringify(item.xhr));
-                            spanMark.dataset.description=encodeURIComponent(item.description || (item.img ? (item.img.title || item.img.alt) : ""));
+                            spanMark.dataset.description=encodeURIComponent(item.description || (item.img ? (item.img.title || item.img.alt || "") : ""));
                             spanMark.dataset.thumbSrc=item.imgSrc;
                             let title = item.img ? (item.img.title || item.img.alt || "").slice(-50) : "";
                             if (title) {
@@ -16654,13 +16654,14 @@ ImgOps | https://imgops.com/#b#`;
                 var canvasImgs=Array.from(getBody(document).querySelectorAll('canvas')).reduceRight((total, canvas) => {
                     if (canvas.clientHeight != 0) {
                         try {
-                            canvas.src = canvas.toDataURL("image/png");
+                            if (!canvas.src) {
+                                canvas.src = canvas.toDataURL("image/png");
+                            }
                             total.push(canvas);
                         } catch(e) {}
                     }
                     if (!canvas.src && canvas.dataset.src) {
                         canvas.src = canvas.dataset.src;
-                        delete canvas.dataset.src;
                         total.push(canvas);
                     }
                     return total;
