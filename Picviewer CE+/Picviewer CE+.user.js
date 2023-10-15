40c981fb74b33fcdf7b55e203cfacc942b366912
hoothin <rixixi@gmail.com>
2023-10-15 19:31:59 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.10.14.1
+// @version              2023.10.15.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -16051,7 +16051,7 @@ ImgOps | https://imgops.com/#b#`;
 
                 for(var i=0,ii=data.length;i<ii;i++){
                     data_i=data[i];
-                    data_i_src=data_i.src;
+                    data_i_src=data_i.imgSrc;
                     if(dataSrcs.indexOf(data_i_src)!=-1){//已经存在
                         data.splice(i,1);//移除
                         i--;
@@ -16060,7 +16060,7 @@ ImgOps | https://imgops.com/#b#`;
                     }
                     dataSrcs.push(data_i_src);
 
-                    if(imgSrc==data_i_src || imgSrc==data_i.imgSrc){
+                    if(imgSrc==data_i_src || imgSrc==data_i.src){
                         index=i;
                     }
                 };
