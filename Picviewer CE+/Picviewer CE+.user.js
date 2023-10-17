a1f0d0ffdccf83b245ece40b49711352cef6fcbf
hoothin <rixixi@gmail.com>
2023-10-17 12:01:50 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.10.17.1
+// @version              2023.10.17.2
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -13855,7 +13855,6 @@ ImgOps | https://imgops.com/#b#`;
                         if (self.urlFilter) {
                             urlFilterHeadItem.title = self.urlFilter;
                             urlFilterHeadItem.style.display = "inline-block";
-                            self.changeMinView();
                         } else {
                             urlFilterHeadItem.style.display = "";
                         }
@@ -13863,6 +13862,8 @@ ImgOps | https://imgops.com/#b#`;
                     }
                 }
 
+                this.urlFilter = "";
+                this.lockMaxSize = false;
                 var urlFilter = storage.getItem("urlFilter" + location.hostname) || false;
                 var lockMaxSize = storage.getItem("lockMaxSize" + location.hostname) || false;
                 if (urlFilter) {
@@ -14546,7 +14547,6 @@ ImgOps | https://imgops.com/#b#`;
                         }, 100);
                     })
                 }
-                self.urlFilter="";
 
 
                 self.batchDl=document.createElement('p');
