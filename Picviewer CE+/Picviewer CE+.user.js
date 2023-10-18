e7c1ddbd6c7df95a81b2e0fdba3087f44f9af579
hoothin <rixixi@gmail.com>
2023-10-18 08:55:52 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.10.17.2
+// @version              2023.10.18.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -15868,6 +15868,7 @@ ImgOps | https://imgops.com/#b#`;
                     this._dataCache = {};
                     this.eleMaps['maximize-container'].innerHTML = createHTML("");
                 }
+                var urlReg=new RegExp(this.urlFilter);
                 var createSpanMark = item => {
                     var spanMark=self._spanMarkPool[item.imgSrc];
                     if(!spanMark){
@@ -15898,9 +15899,9 @@ ImgOps | https://imgops.com/#b#`;
                     }
                     if (item.sizeW == 0 && item.sizeH == 0 && item.img.complete) {
                         spanMark.style.display="none";
-                    } else if(item.sizeW && item.sizeH && (item.sizeW<sizeInputW.value || item.sizeH<sizeInputH.value)){
+                    } else if (item.sizeW && item.sizeH && (item.sizeW<sizeInputW.value || item.sizeH<sizeInputH.value || !urlReg.test(item.src) || (self.lockMaxSize && (item.sizeW>self.lockMaxSize.w || item.sizeH>self.lockMaxSize.h)))){
                         spanMark.style.display="none";
-                    }else{
+                    } else {
                         spanMark.style.display="";
                     }
                     thumbnails.appendChild(spanMark);
