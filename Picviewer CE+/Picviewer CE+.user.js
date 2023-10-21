add3790895b04a10a60326e74b389e6adeafd25b
hoothin <rixixi@gmail.com>
2023-10-21 15:52:23 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.10.20.1
+// @version              2023.10.21.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -13386,6 +13386,12 @@ ImgOps | https://imgops.com/#b#`;
 
                 getBody(document).appendChild(maximizeTrigger);
 
+                container.querySelector("#galleryRotate").addEventListener('focus',function(e) {
+                    eleMaps['head-command-drop-list-others'].classList.add("focus");
+                });
+                container.querySelector("#galleryRotate").addEventListener('blur',function(e){
+                    eleMaps['head-command-drop-list-others'].classList.remove("focus");
+                });
                 container.querySelector("#galleryRotate").addEventListener('change',function(e) {
                     self.galleryRotate = e.target.value;
                     self.fitToScreen();
