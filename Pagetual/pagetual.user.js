cdcb6cbdfae46c93fb9716a8f37dfd0cbb7d4e22
hoothin <rixixi@gmail.com>
2023-11-01 22:34:00 +0800

    Update pagetual.user.js

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:fr      Pagetual
 // @name:it      Pagetual
 // @namespace    hoothin
-// @version      1.9.36.81
+// @version      1.9.36.82
 // @description  Perpetual pages - powerful auto-pager script. Auto loading next paginated web pages and inserting into current page. Support thousands of web sites without any rule.
 // @description:zh-CN  终极自动翻页 - 加载并拼接下一分页内容至当前页尾，智能适配任意网页
 // @description:zh-TW  終極自動翻頁 - 加載並拼接下一分頁內容至當前頁尾，智能適配任意網頁
@@ -3173,8 +3173,8 @@
                     [].forEach.call(ele.querySelectorAll("img,picture>source"), img => {
                         setLazyImg(img);
                     });
-                    [].forEach.call(ele.querySelectorAll("div[data-src][data-thumb],div.img[data-src],div.lazy[data-src]"), div => {
-                        div.style.setProperty("background-image", "url(" + div.dataset.src + ")", "important");
+                    [].forEach.call(ele.querySelectorAll("div[data-src][data-thumb],div.img[data-src],div.lazy[data-src],a.lazy[data-bg]"), div => {
+                        div.style.setProperty("background-image", "url(" + (div.dataset.src || div.dataset.bg) + ")", "important");
                     });
                 }
                 if (/^A$/i.test(ele.nodeName) && ele.classList.contains("lazyload")) {
