2272b474ffc4f21131b4dd008605cd473abcb822
hoothin <rixixi@gmail.com>
2023-11-12 16:58:32 +0800

    Update searchJumperPinyinAddon.lib.js

--- SearchJumper/searchJumperPinyinAddon.lib.js
+++ SearchJumper/searchJumperPinyinAddon.lib.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   搜索酱拼音扩展
 // @name:zh-TW   搜尋醬拼音擴展
 // @namespace    hoothin
-// @version      0.1
+// @version      0.2
 // @description  Add pinyin support for SearchJumper
 // @description:zh-CN  为搜索酱的页内高亮查找添加拼音支持
 // @description:zh-TW  為搜尋醬的頁内高亮查找添加拼音支援
@@ -16,12 +16,18 @@
 (function() {
     'use strict';
     var _unsafeWindow = (typeof unsafeWindow == 'undefined') ? window : unsafeWindow;
+    if (!_unsafeWindow.searchJumperAddons) _unsafeWindow.searchJumperAddons = [];
     var { match } = PinyinMatch;
-    _unsafeWindow.searchJumperPinyin = (text, pinyin) => {
+    var pinyinSearch = (text, pinyin) => {
         let matchResult = match(text, pinyin);
         if (matchResult) {
             return {matched: true, pos: matchResult[0], len: matchResult[matchResult.length - 1] - matchResult[0] + 1};
         }
         return {matched: false};
     }
+    _unsafeWindow.searchJumperPinyin = pinyinSearch;
+    _unsafeWindow.searchJumperAddons.push({
+        type: "findInPage",
+        data: pinyinSearch
+    });
 })();
