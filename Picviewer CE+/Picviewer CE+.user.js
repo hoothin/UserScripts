75ca87f46a9d44715c55c6a4da38154817c0f44f
hoothin <rixixi@gmail.com>
2023-11-23 21:26:46 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.11.22.3
+// @version              2023.11.23.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -16178,6 +16178,7 @@ ImgOps | https://imgops.com/#b#`;
                 };
             },
             keyDownListener:function(e){
+                e.stopPropagation();
                 switch(e.keyCode){
                     case 27:
                         if(prefs.imgWindow.close.escKey){
@@ -16201,6 +16202,14 @@ ImgOps | https://imgops.com/#b#`;
                             origin=0;
                         }
                         img.style[support.cssTransform] = ' rotate('+ origin +'rad) ' + iTransform;
+                        break;
+                    case prefs.floatBar.keys.actual:
+                    case prefs.floatBar.keys.current:
+                        imgReady(this.src,{
+                            ready:function(){
+                                new ImgWindowC(this);
+                            },
+                        });
                         break;
                     case prefs.floatBar.keys.download:
                         downloadImg(this.img.src, this.selected.title, prefs.saveName);
@@ -23264,6 +23273,7 @@ ImgOps | https://imgops.com/#b#`;
 
         function keydown(event) {
             if (ImgWindowC.showing) return;
+            if (gallery && gallery.shown) return;
             if (document.activeElement &&
                 (document.activeElement.nodeName == 'INPUT' ||
                  document.activeElement.nodeName == 'INPUT' ||
