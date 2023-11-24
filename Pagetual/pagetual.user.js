8dd447fcaa8753cf31f0cabceee4a9cb019973e5
hoothin <rixixi@gmail.com>
2023-11-24 18:27:50 +0800

    1.9.36.99

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:fr      Pagetual
 // @name:it      Pagetual
 // @namespace    hoothin
-// @version      1.9.36.98
+// @version      1.9.36.99
 // @description  Perpetual pages - powerful auto-pager script. Auto loading next paginated web pages and inserting into current page. Support thousands of web sites without any rule.
 // @description:zh-CN  终极自动翻页 - 加载并拼接下一分页内容至当前页尾，智能适配任意网页
 // @description:zh-TW  終極自動翻頁 - 加載並拼接下一分頁內容至當前頁尾，智能適配任意網頁
@@ -854,7 +854,7 @@
     async function getListData(list, key) {
         return new Promise((resolve) => {
             storage.getItem(list, listData => {
-                let value = null;
+                let value;
                 if (listData) {
                     for(var i = 0; i < listData.length; i++) {
                         let data = listData[i];
@@ -2296,6 +2296,11 @@
                         next = nexts[i];
                         break;
                     }
+                }
+            }
+            if (next && doc == document) {
+                if (_unsafeWindow.getComputedStyle(next).display == "none") {
+                    next = null;
                 }
             }
             if (next && this.linkHasNoHref(next)) {
@@ -6041,6 +6046,9 @@
                 }
 
                 let _forceState = await getListData("forceState", location.host);
+                if (typeof(_forceState) == "undefined") {
+                    _forceState = await getData("forceState_" + location.host);
+                }
                 if (typeof(_forceState) == "undefined") {
                     _forceState = (rulesData.enableWhiteList ? 1 : 0);
                 }
