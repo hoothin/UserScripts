434c440c33567399c1cf7927a1f8b4160edce865
hoothin <rixixi@gmail.com>
2023-12-01 17:50:02 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2023.11.30.1
+// @version              2023.12.1.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -16822,7 +16822,7 @@ ImgOps | https://imgops.com/#b#`;
                                     node.src = src;
                                     total.push(node);
                                     hasSrc = true;
-                                    prop = prop.replace(bgReg, "$1");
+                                    prop = prop.replace(bgReg, "$2");
                                     match = bgReg.exec(prop);
                                 }
                                 while (match) {
@@ -16830,7 +16830,7 @@ ImgOps | https://imgops.com/#b#`;
                                     node = document.createElement("img");
                                     node.src = src;
                                     total.push(node);
-                                    prop = prop.replace(bgReg, "$1");
+                                    prop = prop.replace(bgReg, "$2");
                                     match = bgReg.exec(prop);
                                 }
                             }
@@ -16844,7 +16844,7 @@ ImgOps | https://imgops.com/#b#`;
                                     node.src = src;
                                     total.push(node);
                                     hasSrc = true;
-                                    prop = prop.replace(bgReg, "$1");
+                                    prop = prop.replace(bgReg, "$2");
                                     match = bgReg.exec(prop);
                                 }
                                 while (match) {
@@ -16852,7 +16852,7 @@ ImgOps | https://imgops.com/#b#`;
                                     node = document.createElement("img");
                                     node.src = src;
                                     total.push(node);
-                                    prop = prop.replace(bgReg, "$1");
+                                    prop = prop.replace(bgReg, "$2");
                                     match = bgReg.exec(prop);
                                 }
                             }
@@ -16866,7 +16866,7 @@ ImgOps | https://imgops.com/#b#`;
                                     node.src = src;
                                     total.push(node);
                                     hasSrc = true;
-                                    prop = prop.replace(bgReg, "$1");
+                                    prop = prop.replace(bgReg, "$2");
                                     match = bgReg.exec(prop);
                                 }
                                 while (match) {
@@ -16874,7 +16874,7 @@ ImgOps | https://imgops.com/#b#`;
                                     node = document.createElement("img");
                                     node.src = src;
                                     total.push(node);
-                                    prop = prop.replace(bgReg, "$1");
+                                    prop = prop.replace(bgReg, "$2");
                                     match = bgReg.exec(prop);
                                 }
                             }
