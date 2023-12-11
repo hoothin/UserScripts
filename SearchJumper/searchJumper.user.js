f809bfc22f778f9a01596f003440d67eb26b0bbf
hoothin <rixixi@gmail.com>
2023-12-11 21:25:15 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -1710,7 +1710,7 @@
                      justify-content: flex-start;
                  }
                  #search-jumper.search-jumper-showall #search-jumper-alllist.new-mode .sitelist {
-                     width: 72%;
+                     width: 73%;
                      max-height: unset;
                  }
                  #search-jumper.search-jumper-showall #search-jumper-alllist.new-mode .sitelist>.sitelistCon {
