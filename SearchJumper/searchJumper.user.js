ec775364b39a46d58c35cbe2c18a722a9b0183c8
hoothin <rixixi@gmail.com>
2023-12-21 21:08:08 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.7.56
+// @version      1.7.57
 // @description  Assist with the seamless transition between any search engine(Google/Bing/Custom), providing the ability to swiftly navigate to any platform and conduct searches effortlessly. Additionally, it allows for the selection of text, images, or links to be searched on any search engine with a simple right-click or by utilizing a range of menus and shortcuts.
 // @description:zh-CN  高效搜索辅助，在搜索时一键切换任何搜索引擎(百度/必应/谷歌等)，支持划词右键搜索、页内关键词查找与高亮、可视化操作模拟、高级自定义等
 // @description:zh-TW  高效搜尋輔助，在搜尋時一鍵切換任意搜尋引擎，支援劃詞右鍵搜尋、頁內關鍵詞查找與高亮、可視化操作模擬、高級自定義等
@@ -882,7 +882,19 @@
         } else {
             _GM_openInTab = (s, t) => {window.open(s)};
         }
-        if (typeof GM_notification != 'undefined') {
+        if (ext) {
+            _GM_notification = s => {
+                chrome.notifications.create(
+                  "searchjumper-notification",
+                  {
+                    type: "basic",
+                    iconUrl: "/icon/icon128.png",
+                    title: "SearchJumper",
+                    message: s,
+                  }
+                )
+            }
+        } else if (typeof GM_notification != 'undefined') {
             _GM_notification = s => GM_notification({text: s, onclick: e => _GM_openInTab(configPage, {active: true})});
         } else if (typeof GM != 'undefined' && typeof GM.notification != 'undefined') {
             _GM_notification = s => GM.notification({text: s, onclick: e => _GM_openInTab(configPage, {active: true})});
@@ -6668,6 +6680,7 @@
                         self.createAddonSpan(name, addon);
                     });
                 }
+                if (isAllPage) return;
                 if (disableHighlight && disableHighlight != location.hostname && window.top == window.self) {
                     storage.setItem("disableHighlight", "");
                 }
