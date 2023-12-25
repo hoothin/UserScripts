4f59571ff7ed3e1714cf1e45b15d83ccd33a0239
hoothin <rixixi@gmail.com>
2023-12-25 08:11:25 +0800

    Update searchJumper.user.js

--- SearchJumper/searchJumper.user.js
+++ SearchJumper/searchJumper.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   搜尋醬
 // @name:ja      検索ちゃん - SearchJumper
 // @namespace    hoothin
-// @version      1.7.59
+// @version      1.7.60
 // @description  Assist with the seamless transition between any search engine(Google/Bing/Custom), providing the ability to swiftly navigate to any platform and conduct searches effortlessly. Additionally, it allows for the selection of text, images, or links to be searched on any search engine with a simple right-click or by utilizing a range of menus and shortcuts.
 // @description:zh-CN  高效搜索辅助，在搜索时一键切换任何搜索引擎(百度/必应/谷歌等)，支持划词右键搜索、页内关键词查找与高亮、可视化操作模拟、高级自定义等
 // @description:zh-TW  高效搜尋輔助，在搜尋時一鍵切換任意搜尋引擎，支援劃詞右鍵搜尋、頁內關鍵詞查找與高亮、可視化操作模擬、高級自定義等
@@ -9120,7 +9120,8 @@
                     if (searchData.prefConfig.multiline == 1 || searchData.prefConfig.multiline == 2) {
                         if (inputString &&
                             wordParamReg.test(ele.dataset.url) &&
-                            inputString.indexOf("\n") !== -1) {
+                            inputString.indexOf("\n") !== -1 &&
+                            !/^(c|show)/.test(ele.dataset.url)) {
                             if (searchData.prefConfig.multiline == 1 ||
                                 confirm(i18n("multiline"))) {
                                 let selStrArr = inputString.split("\n");
