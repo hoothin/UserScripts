81916b59226106954913c44ec68f6944e3a2986d
hoothin <rixixi@gmail.com>
2024-02-08 12:06:58 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -10,7 +10,7 @@
 // @description:zh-TW    線上看圖工具，支援圖片翻轉、旋轉、縮放、彈出大圖、批量儲存
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2024.2.4.3
+// @version              2024.2.8.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -12081,6 +12081,7 @@ ImgOps | https://imgops.com/#b#`;
                 exportType:'grid',
                 sidebarPosition: 'bottom',//'top' 'right' 'bottom' 'left'  四个可能值
                 sidebarSize: 120,//侧栏的高（如果是水平放置）或者宽（如果是垂直放置）
+                backgroundColor: 'rgba(20,20,20,0.75)',
                 sidebarToggle: true, // 是否显示隐藏按钮
                 transition:false,//大图片区的动画。
                 preload:true,//对附近的图片进行预读。
@@ -17616,7 +17617,7 @@ ImgOps | https://imgops.com/#b#`;
                     height: 100%;\
                     width: 100%;\
                     background-clip: padding-box;\
-                    background-color: rgba(20,20,20,0.75);\
+                    background-color: ' + (prefs.gallery.backgroundColor || 'rgba(20,20,20,0.75)') + ';\
                     position:relative;\
                     transition: background-color .3s ease;\
                     }\
@@ -24024,6 +24025,13 @@ ImgOps | https://imgops.com/#b#`;
                     type: 'checkbox',
                     "default": prefs.gallery.scrollEndToChange,
                     title: i18n("galleryScrollEndToChangeTip")
+                },
+                'gallery.backgroundColor': {
+                    label: i18n("backgroundColor"),
+                    type: 'text',
+                    className: 'color',
+                    "default": prefs.gallery.backgroundColor || 'rgba(20,20,20,0.75)',
+                    line: 'end'
                 },
                 'gallery.exportType': {
                     label: i18n("galleryExportType"),
