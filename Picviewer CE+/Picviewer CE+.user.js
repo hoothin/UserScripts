decc1eba94c5436deab34ba5f0bd3f076c0f673c
hoothin <rixixi@gmail.com>
2024-05-20 08:49:43 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -12,7 +12,7 @@
 // @description:ja       オンラインで画像を強力に閲覧できるツール。ポップアップ表示、拡大・縮小、回転、一括保存などの機能を自動で実行できます
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2024.5.20.1
+// @version              2024.5.20.2
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -24156,12 +24156,6 @@ ImgOps | https://imgops.com/#b#`;
                     });
                     return true;
                 } else {
-                    if (uniqueImgWin && uniqueImgWin.imgWindow && !uniqueImgWin.removed) {
-                        uniqueImgWin.imgWindow.classList.remove("pv-pic-window-transition-all");
-                        uniqueImgWin.imgWindow.classList.remove("preview");
-                        uniqueImgWin.previewed = true;
-                        uniqueImgWin = null;
-                    }
                     return false;
                 }
             };
@@ -24534,6 +24528,7 @@ ImgOps | https://imgops.com/#b#`;
         function keyup(event) {
             let isFuncKey = event.key == 'Alt' || event.key == 'Control' || event.key == 'Shift' || event.key == 'Meta';
             if(isFuncKey && (prefs.floatBar.globalkeys.type == "hold" || !checkPreview(event)) && (uniqueImgWin && !uniqueImgWin.removed)){
+                clearTimeout(checkFloatBarTimer);
                 if(prefs.floatBar.globalkeys.closeAfterPreview){
                     if (uniqueImgWin) {
                         uniqueImgWin.remove();
