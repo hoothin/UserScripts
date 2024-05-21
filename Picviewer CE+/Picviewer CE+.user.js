6e8a0469eeb39c7ccfc76ea414a5fa45e0f86992
hoothin <rixixi@gmail.com>
2024-05-21 09:41:54 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -12,7 +12,7 @@
 // @description:ja       オンラインで画像を強力に閲覧できるツール。ポップアップ表示、拡大・縮小、回転、一括保存などの機能を自動で実行できます
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2024.5.20.2
+// @version              2024.5.21.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -46,7 +46,7 @@
 // @grant                GM.notification
 // @grant                unsafeWindow
 // @require              https://update.greasyfork.org/scripts/6158/23710/GM_config%20CN.js
-// @require              https://update.greasyfork.org/scripts/438080/1379686/pvcep_rules.js
+// @require              https://update.greasyfork.org/scripts/438080/1380261/pvcep_rules.js
 // @require              https://update.greasyfork.org/scripts/440698/1372505/pvcep_lang.js
 // @downloadURL          https://greasyfork.org/scripts/24204-picviewer-ce/code/Picviewer%20CE+.user.js
 // @updateURL            https://greasyfork.org/scripts/24204-picviewer-ce/code/Picviewer%20CE+.meta.js
@@ -23174,9 +23174,14 @@ ImgOps | https://imgops.com/#b#`;
                     }
 
                     if (matchedRule.description) {
-                        var node = getElementMix(matchedRule.description, img);
+                        let desc = matchedRule.description, attr;
+                        if (Array.isArray(desc) && desc.length === 2) {
+                            attr = desc[1];
+                            desc = desc[0];
+                        }
+                        var node = getElementMix(desc, img);
                         if (node) {
-                            description = node.getAttribute('title') || node.textContent;
+                            description = attr ? node.getAttribute(attr) : (node.getAttribute('title') || node.textContent);
                         }
                     }
                 }
@@ -24109,9 +24114,14 @@ ImgOps | https://imgops.com/#b#`;
                                     type = 'rule';
 
                                     if (matchedRule.description) {
-                                        var node = getElementMix(matchedRule.description, img);
+                                        let desc = matchedRule.description, attr;
+                                        if (Array.isArray(desc) && desc.length === 2) {
+                                            attr = desc[1];
+                                            desc = desc[0];
+                                        }
+                                        var node = getElementMix(desc, img);
                                         if (node) {
-                                            description = node.getAttribute('title') || node.textContent;
+                                            description = attr ? node.getAttribute(attr) : (node.getAttribute('title') || node.textContent);
                                         }
                                     }
                                     result.src = src;
