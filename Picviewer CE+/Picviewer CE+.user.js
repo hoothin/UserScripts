1b3389328a89a6df0c11f9e8af7f33bd9b3a4106
hoothin <rixixi@gmail.com>
2024-06-01 12:46:34 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -12,7 +12,7 @@
 // @description:ja       オンラインで画像を強力に閲覧できるツール。ポップアップ表示、拡大・縮小、回転、一括保存などの機能を自動で実行できます
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2024.5.31.4
+// @version              2024.6.1.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -16874,6 +16874,7 @@ ImgOps | https://imgops.com/#b#`;
                 }
             },
             keyUpListener:function(e){
+                if (e.ctrlKey || e.metaKey) return;
                 const key = e.key || String.fromCharCode(e.keyCode);
                 if (e.target != this.gallery) return;
                 switch(key.toLowerCase()){
@@ -24787,10 +24788,10 @@ ImgOps | https://imgops.com/#b#`;
                 return false;
             }
 
-            if (event && (event.ctrlKey || event.altKey || event.shiftKey || event.metaKey) && window.getSelection().toString()) {
-                return false;
+            if (event) {
+                if (event.ctrlKey || event.metaKey) return false;
+                if ((event.altKey || event.shiftKey) && window.getSelection().toString()) return false;
             }
-            if (key == 'c' && event && (event.ctrlKey || event.metaKey)) return false;
             if (floatBar && isKeyDownEffectiveTarget(event.target)) {
                 Object.keys(prefs.floatBar.keys).some(function(action) {
                     if (action == 'enable' || action == 'search') return;
