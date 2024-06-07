c49d9fb92c6e142fea3a3945d843a09ad450e6eb
hoothin <rixixi@gmail.com>
2024-06-07 18:09:25 +0800

    1.9.37.52

--- Pagetual/pagetual.user.js
+++ Pagetual/pagetual.user.js
@@ -10,7 +10,7 @@
 // @name:fr      Pagetual
 // @name:it      Pagetual
 // @namespace    hoothin
-// @version      1.9.37.51
+// @version      1.9.37.52
 // @description  Perpetual pages - powerful auto-pager script. Auto fetching next paginated web pages and inserting into current page for infinite scroll. Support thousands of web sites without any rule.
 // @description:zh-CN  终极自动翻页 - 加载并拼接下一分页内容至当前页尾，智能适配任意网页
 // @description:zh-TW  終極自動翻頁 - 加載並拼接下一分頁內容至當前頁尾，智能適配任意網頁
@@ -5313,12 +5313,18 @@
     function startAutoScroll() {
         clearInterval(autoScrollInterval);
         if (autoScroll <= 0) return;
-        let scrollRange = 1;
+        let scrollRange_o = 1;
         if (autoScroll > 1000) {
-            scrollRange = parseInt(autoScroll / 1000)
+            scrollRange_o = parseInt(autoScroll / 1000);
         }
+        let devicePixelRatio = window.devicePixelRatio;
+        let scrollRange = Math.ceil(scrollRange_o / devicePixelRatio);
         autoScrollInterval = setInterval(() => {
             if (isPause) return;
+            if (devicePixelRatio !== window.devicePixelRatio) {
+                devicePixelRatio = window.devicePixelRatio;
+                scrollRange = Math.ceil(scrollRange_o / devicePixelRatio);
+            }
             window.scroll(window.scrollX, window.scrollY + scrollRange);
         }, parseInt(1000 / autoScroll));
     }
