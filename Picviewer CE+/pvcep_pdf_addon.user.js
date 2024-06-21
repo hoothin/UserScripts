be1198abd9c2f81f8bd82a39bcc047d33a51d43f
hoothin <rixixi@gmail.com>
2024-06-21 18:37:40 +0800

    Update pvcep_pdf_addon.user.js

--- Picviewer CE+/pvcep_pdf_addon.user.js
+++ Picviewer CE+/pvcep_pdf_addon.user.js
@@ -3,14 +3,18 @@
 // @name:zh-CN   Picviewer CE+ PDF 扩展
 // @name:zh-TW   Picviewer CE+ PDF 擴充
 // @namespace    https://github.com/hoothin/UserScripts
-// @version      2024-06-20
+// @version      2024-06-21
 // @description  Batch Download as PDF instead of ZIP
 // @description:zh-CN   取代 ZIP， 打包下载时下载为 PDF
 // @description:zh-TW   取代 ZIP， 打包下載時下載為 PDF
 // @author       hoothin
 // @match        *://*/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @grant        unsafeWindow
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @require      https://unpkg.com/jspdf@latest/dist/jspdf.umd.min.js
 // ==/UserScript==
 
@@ -43,8 +47,12 @@
             const pdf = new window.jspdf.jsPDF();
             const fileLength = this.fileList.length;
             for (const [key, param] of this.fileList.entries()) {
-                const fileName = param[0];
+                let fileName = param[0];
                 let blob = param[1];
+                if (!blob && this.fileList.length === 1) {
+                    fileName = this.fileList[0][0];
+                    blob = this.fileList[0][1];
+                }
                 let dataUrl = await blobToDataURL(blob);
                 const imgProps = pdf.getImageProperties(dataUrl);
                 const pdfWidth = pdf.internal.pageSize.getWidth();
@@ -58,6 +66,19 @@
             pdf.save(pdfName);
         };
     }
+
     const _unsafeWindow = typeof unsafeWindow === 'undefined' ? window : unsafeWindow;
-    _unsafeWindow.pvcepimg2pdf = img2pdf;
+    let disabled = !!GM_getValue("pvcep_pdf_disabled"), registerId;
+    function registerMenuCommand() {
+        if (disabled) {
+            _unsafeWindow.pvcepimg2pdf = null;
+        } else _unsafeWindow.pvcepimg2pdf = img2pdf;
+        registerId = GM_registerMenuCommand(disabled ? "Enable addon" : "Disable addon", () => {
+            GM_unregisterMenuCommand(registerId);
+            disabled = !disabled;
+            GM_setValue("pvcep_pdf_disabled", disabled);
+            registerMenuCommand();
+        });
+    }
+    registerMenuCommand();
 })();
