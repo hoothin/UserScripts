ed26394a0e6d6ac8455afce9982c4537e822f3b3
hoothin <rixixi@gmail.com>
2024-06-23 11:14:50 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -12,7 +12,7 @@
 // @description:ja       オンラインで画像を強力に閲覧できるツール。ポップアップ表示、拡大・縮小、回転、一括保存などの機能を自動で実行できます
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2024.6.23.1
+// @version              2024.6.23.2
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -12242,7 +12242,7 @@ ImgOps | https://imgops.com/#b#`;
             },
             onload: function(d) {
                 let blob = d.response;
-                let ext = blob.type.replace(/.*image\/(\w+).*/, "$1");
+                let ext = blob.type.replace(/.*image\/([\w\-]+).*/, "$1");
                 let conversion = formatDict.get(ext);
                 if (canvas && (conversion || forcePng)) {
                     var self = this;
@@ -25527,6 +25527,7 @@ ImgOps | https://imgops.com/#b#`;
                 'gallery.formatConversion': {
                     label: i18n("formatConversion"),
                     type: 'textarea',
+                    title: 'webp>png\nx-icon>png',
                     "default": prefs.gallery.formatConversion || ''
                 },
                 'gallery.aria2Host': {
