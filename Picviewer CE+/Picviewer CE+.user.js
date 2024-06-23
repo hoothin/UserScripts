3858bbb8380e3017ebaa7496f113dd0b13eb6a8d
hoothin <rixixi@gmail.com>
2024-06-23 09:34:31 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -12,7 +12,7 @@
 // @description:ja       オンラインで画像を強力に閲覧できるツール。ポップアップ表示、拡大・縮小、回転、一括保存などの機能を自動で実行できます
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2024.6.22.6
+// @version              2024.6.23.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://www.hoothin.com
@@ -13443,11 +13443,11 @@ ImgOps | https://imgops.com/#b#`;
 
             function getSupportEventName(){
                 var ret='DOMMouseScroll';
-                if(eventSupported('wheel')){//w3c FF>=17 ie>=9
-                    ret='wheel';
-                }else if(eventSupported('mousewheel')){//opera,chrome
+                if(eventSupported('mousewheel')){//opera,chrome
                     ret='mousewheel';
-                };
+                }else if(eventSupported('wheel')){//w3c FF>=17 ie>=9
+                    ret='wheel';
+                }
                 return ret;
             };
 
@@ -20731,6 +20731,9 @@ ImgOps | https://imgops.com/#b#`;
                     .compare>.pv-pic-search-state{\
                     display: none;\
                     }\
+                    .pv-pic-window-container_focus:not(.preview)>.pv-pic-search-state {\
+                    opacity:0.8;\
+                    }\
                     .pv-pic-window-container_focus:not(.preview) .pv-pic-window-imgbox:hover~.pv-pic-window-pre,\
                     .pv-pic-window-container_focus:not(.preview) .pv-pic-window-imgbox:hover~.pv-pic-window-next{\
                     opacity:0.3;\
@@ -20809,6 +20812,7 @@ ImgOps | https://imgops.com/#b#`;
                     color: white;\
                     cursor: pointer;\
                     display: none;\
+                    box-sizing: content-box;\
                     transition: all 0.3s ease;\
                     }\
                     .pv-pic-search-state>.pv-icon:hover {\
@@ -20816,9 +20820,6 @@ ImgOps | https://imgops.com/#b#`;
                     }\
                     .pv-pic-search-state>.pv-icon * {\
                     pointer-events: none;\
-                    }\
-                    .pv-pic-window-container_focus:not(.preview)>.pv-pic-search-state {\
-                    opacity:0.8;\
                     }\
                     .pv-pic-window-scrollSign {\
                     display: none;\
