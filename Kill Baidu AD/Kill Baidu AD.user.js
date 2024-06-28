8775b7b96888ca999b32bc371a0381d2bc66809f
hoothin <rixixi@gmail.com>
2024-06-28 09:32:15 +0800

    Update Kill Baidu AD.user.js

--- Kill Baidu AD/Kill Baidu AD.user.js
+++ Kill Baidu AD/Kill Baidu AD.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN         百度广告(首尾推广及右侧广告)清理
 // @name:zh-TW         百度廣告(首尾推廣及右側廣告)清理
 // @namespace          hoothin
-// @version            1.23.2
+// @version            1.23.3
 // @description        彻底清理百度搜索(www.baidu.com)结果首尾的推广广告、二次顽固广告、右侧广告，去除重定向，删除百家号
 // @description:zh-CN  彻底清理百度搜索(www.baidu.com)结果首尾的推广广告、二次顽固广告、右侧广告，去除重定向，移除百家号
 // @description:zh-TW  徹底清理百度搜索(www.baidu.com)結果首尾的推廣廣告、二次頑固廣告、右側廣告，去除重定向，刪除百家號
@@ -11,6 +11,11 @@
 // @match              *://www.baidu.com/*
 // @match              *://m.baidu.com/*
 // @grant              GM_xmlhttpRequest
+// @grant              GM_addStyle
+// @grant              GM_getValue
+// @grant              GM_setValue
+// @grant              GM_registerMenuCommand
+// @grant              GM_unregisterMenuCommand
 // @run-at             document-start
 // @license            MIT License
 // @compatible         chrome 测试通过
@@ -21,7 +26,7 @@
 
 (function() {
     'use strict';
-    var MO = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver, killBaijiaType = 1;
+    var MO = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver, killBaijiaType = 1, killRight, killRightStyle, killRightRegister, hidePicture, hidePictureStyle, hidePictureRegister;
     if (MO) {
         var observer = new MO(function(records) {
             records.map(function(record) {
@@ -136,6 +141,9 @@
 
     function clearAD() {
         if (!document.querySelectorAll) return;
+        if (killRight) {
+            document.body.classList.add("killRight");
+        } else document.body.classList.remove("killRight");
         removeEcAd(document);
         var list = document.querySelectorAll("#content_left>div,#content_left>table");
         for (let i = 0; i < list.length; i++) {
@@ -177,6 +185,70 @@
             [].forEach.call(eles, e => {clearOneAD(e)});
         }
     }
+    function initCss() {
+        killRightStyle && killRightStyle.parentNode && killRightStyle.parentNode.removeChild(killRightStyle);
+        if (killRight) {
+            killRightStyle = GM_addStyle(`
+            #content_right,[tpl="recommend_list"],#rs_new {
+                display: none;
+            }
+            #container.sam_newgrid {
+                width: calc(100% - 150px);
+            }
+            #container.sam_newgrid #content_left {
+                column-width: 560px;
+                width: 100%;
+            }
+            #content_left .result-op, #content_left .result {
+                page-break-inside: avoid;
+            }
+            `);
+        }
+        hidePictureStyle && hidePictureStyle.parentNode && hidePictureStyle.parentNode.removeChild(hidePictureStyle);
+        if (hidePicture) {
+            hidePictureStyle = GM_addStyle(`
+            #wrapper_wrapper {
+                background: rgba(228, 228, 228, 0.29);
+            }
+            .c-span3,.c-span6>.c-img,[tpl="short_video"],[data-module="gp-peopleintro-img-click"],div[class^="img-container"] {
+                display: none;
+            }
+            a,a:active,#content_right a {
+                color: rgb(51,51,51);
+            }
+            em {
+                color: #4E6EF2!important;
+            }
+            .c-container.result[tpl], .result-op.c-container[tpl], .result-op.xpath-log.new-pmd[tpl] {
+                padding-top: 0px;
+                padding-bottom: 25px;
+                border-bottom: 1px solid rgba(211, 211, 211, 0.63);
+            }
+            `);
+        }
+    }
+    function registerMenuCommand() {
+        initCss();
+        hidePictureRegister = GM_registerMenuCommand(hidePicture ? "❌ 恢复图片" : "✅ 隐藏图片", () => {
+            GM_unregisterMenuCommand(killRightRegister);
+            GM_unregisterMenuCommand(hidePictureRegister);
+            hidePicture = !hidePicture;
+            GM_setValue("hidePicture", hidePicture);
+            registerMenuCommand();
+        });
+        killRightRegister = GM_registerMenuCommand(killRight ? "❌ 恢复右边栏与布局" : "✅ 隐藏右边栏并多列显示", () => {
+            GM_unregisterMenuCommand(killRightRegister);
+            GM_unregisterMenuCommand(hidePictureRegister);
+            killRight = !killRight;
+            GM_setValue("killRight", killRight);
+            registerMenuCommand();
+        });
+    }
+    try {
+        killRight = !!GM_getValue("killRight");
+        hidePicture = !!GM_getValue("hidePicture");
+        registerMenuCommand();
+    } catch(e) {}
     if (document.readyState == "complete" || document.readyState == "interactive") {
         clearAD();
     } else {
