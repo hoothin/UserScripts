89dd1035defbabad05292f4ff96dabe75e9167cc
hoothin <rixixi@gmail.com>
2024-08-13 19:45:42 +0800

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -12,7 +12,7 @@
 // @description:ja       オンラインで画像を強力に閲覧できるツール。ポップアップ表示、拡大・縮小、回転、一括保存などの機能を自動で実行できます
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2024.8.11.1
+// @version              2024.8.13.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://github.com/hoothin/UserScripts/tree/master/Picviewer%20CE%2B
@@ -47,7 +47,7 @@
 // @grant                unsafeWindow
 // @require              https://update.greasyfork.org/scripts/6158/23710/GM_config%20CN.js
 // @require              https://update.greasyfork.org/scripts/438080/1416490/pvcep_rules.js
-// @require              https://update.greasyfork.org/scripts/440698/1426309/pvcep_lang.js
+// @require              https://update.greasyfork.org/scripts/440698/1427239/pvcep_lang.js
 // @downloadURL          https://greasyfork.org/scripts/24204-picviewer-ce/code/Picviewer%20CE+.user.js
 // @updateURL            https://greasyfork.org/scripts/24204-picviewer-ce/code/Picviewer%20CE+.meta.js
 // @match                *://*/*
@@ -57,7 +57,7 @@
 // @exclude              *://mega.*/*
 // @exclude              *://*.mega.*/*
 // @exclude              *://onedrive.live.com/*
-// @run-at               document-body
+// @run-at               document-end
 // @created              2011-6-15
 // @contributionURL      https://ko-fi.com/hoothin
 // @contributionAmount   1
@@ -25351,6 +25351,7 @@ ImgOps | https://imgops.com/#b#`;
         }
 
         function keydown(event) {
+
             //if (ImgWindowC.showing) return;
             if (gallery && gallery.shown) return;
             if (inputActive(document)) {
@@ -25780,6 +25781,11 @@ ImgOps | https://imgops.com/#b#`;
                     type: 'text',
                     className: 'floatBar-key',
                     "default": prefs.floatBar.keys.download
+                },
+                'floatBar.disableKeySites': {
+                    label: i18n("disableKeySites"),
+                    type: 'textarea',
+                    "default": prefs.floatBar.disableKeySites
                 },
 
                 // 放大镜
@@ -26439,7 +26445,7 @@ ImgOps | https://imgops.com/#b#`;
                 }
             }, true);
         } else if (prefs.gallery.autoOpenSites) {
-            var sitesArr=prefs.gallery.autoOpenSites.split("\n");
+            let sitesArr=prefs.gallery.autoOpenSites.split("\n");
             for(let s=0;s<sitesArr.length;s++){
                 let siteReg=sitesArr[s].trim();
                 let autoViewMore=siteReg[0]=="@";
@@ -26455,7 +26461,20 @@ ImgOps | https://imgops.com/#b#`;
         }
 
         // 注册按键
-        document.addEventListener('keydown', keydown, true);
+        let disableKey = false;
+        if (prefs.floatBar.disableKeySites) {
+            let sitesArr = prefs.floatBar.disableKeySites.split("\n");
+            for(let s = 0; s < sitesArr.length; s++) {
+                let siteReg = sitesArr[s].trim();
+                if (new RegExp(siteReg).test(_URL)) {
+                    disableKey = true;
+                    break;
+                }
+            }
+        }
+        if (!disableKey) {
+            document.addEventListener('keydown', keydown, true);
+        }
 
         let canImport = false;
         for (let i = 0; i < ruleImportHost.length; i++) {
