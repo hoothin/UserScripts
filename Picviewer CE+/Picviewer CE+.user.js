898a2b2689444d5b10e9b4576c2a5f3bd463f0a3
hoothin <rixixi@gmail.com>
2025-07-05 13:51:31 +0900

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -12,7 +12,7 @@
 // @description:ja       画像を強力に閲覧できるツール。ポップアップ表示、拡大・縮小、回転、一括保存などの機能を自動で実行できます
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2025.6.29.2
+// @version              2025.7.5.1
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://github.com/hoothin/UserScripts/tree/master/Picviewer%20CE%2B
@@ -17870,6 +17870,11 @@ ImgOps | https://imgops.com/#b#`;
                                 try {
                                     src = decodeURIComponent(src);
                                 } catch (e) {}
+                            } else if (/[&\?]https?:/.test(src)) {
+                                src = src.replace(/.*[&\?](https?:)/, "$1");
+                                try {
+                                    src = decodeURIComponent(src);
+                                } catch (e) {}
                             }
                             node.src = src;
                             linkMedias.push(node);
@@ -24158,6 +24163,11 @@ ImgOps | https://imgops.com/#b#`;
                         try {
                             src = decodeURIComponent(src);
                         } catch (e) {}
+                    } else if (/[&\?]https?:/.test(src)) {
+                        src = src.replace(/.*[&\?](https?:)/, "$1");
+                        try {
+                            src = decodeURIComponent(src);
+                        } catch (e) {}
                     }
                 }
                 if(src)type='tpRule';
@@ -25284,6 +25294,11 @@ ImgOps | https://imgops.com/#b#`;
                     let src = target.href;
                     if (/[&\?]url\=/.test(src)) {
                         src = src.replace(/.*[&\?]url\=(.*?)(&.*|$)/, "$1");
+                        try {
+                            src = decodeURIComponent(src);
+                        } catch (e) {}
+                    } else if (/[&\?]https?:/.test(src)) {
+                        src = src.replace(/.*[&\?](https?:)/, "$1");
                         try {
                             src = decodeURIComponent(src);
                         } catch (e) {}
