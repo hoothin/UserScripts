09a9431948a820327336d9ffb4cfd2bd95c0bf06
hoothin <rixixi@gmail.com>
2025-08-02 20:38:22 +0900

    Update Picviewer CE+.user.js

--- Picviewer CE+/Picviewer CE+.user.js
+++ Picviewer CE+/Picviewer CE+.user.js
@@ -12,7 +12,7 @@
 // @description:ja       画像を強力に閲覧できるツール。ポップアップ表示、拡大・縮小、回転、一括保存などの機能を自動で実行できます
 // @description:pt-BR    Poderosa ferramenta de visualização de imagens on-line, que pode pop-up/dimensionar/girar/salvar em lote imagens automaticamente
 // @description:ru       Мощный онлайн-инструмент для просмотра изображений, который может автоматически отображать/масштабировать/вращать/пакетно сохранять изображения
-// @version              2025.8.1.2
+// @version              2025.8.1.3
 // @icon                 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAD////29vbKysoqKioiIiKysrKhoaGTk5N9fX3z8/Pv7+/r6+vk5OTb29vOzs6Ojo5UVFQzMzMZGRkREREMDAy4uLisrKylpaV4eHhkZGRPT08/Pz/IfxjQAAAAgklEQVQoz53RRw7DIBBAUb5pxr2m3/+ckfDImwyJlL9DDzQgDIUMRu1vWOxTBdeM+onApENF0qHjpkOk2VTwLVEF40Kbfj1wK8AVu2pQA1aBBYDHJ1wy9Cf4cXD5chzNAvsAnc8TjoLAhIzsBao9w1rlVTIvkOYMd9nm6xPi168t9AYkbANdajpjcwAAAABJRU5ErkJggg==
 // @namespace            https://github.com/hoothin/UserScripts
 // @homepage             https://github.com/hoothin/UserScripts/tree/master/Picviewer%20CE%2B
@@ -46,7 +46,7 @@
 // @grant                GM.notification
 // @grant                unsafeWindow
 // @require              https://update.greasyfork.org/scripts/6158/23710/GM_config%20CN.js
-// @require              https://update.greasyfork.org/scripts/438080/1634207/pvcep_rules.js
+// @require              https://update.greasyfork.org/scripts/438080/1634247/pvcep_rules.js
 // @require              https://update.greasyfork.org/scripts/440698/1427239/pvcep_lang.js
 // @downloadURL          https://greasyfork.org/scripts/24204-picviewer-ce/code/Picviewer%20CE+.user.js
 // @updateURL            https://greasyfork.org/scripts/24204-picviewer-ce/code/Picviewer%20CE+.meta.js
@@ -12540,7 +12540,7 @@ ImgOps | https://imgops.com/#b#`;
                 sidebarToggle: true, // 是否显示隐藏按钮
                 transition:true,//大图片区的动画。
                 preload:true,//对附近的图片进行预读。
-                max:5,//最多预读多少张（前后各多少张）
+                max:3,//最多预读多少张（前后各多少张）
 
                 zoomresized: 25, // 图片尺寸最少相差比例，单位：%
                 scaleSmallSize: 250, // 图库的新类别，缩放的图片，尺寸的高或宽都小于该值
@@ -17005,12 +17005,28 @@ ImgOps | https://imgops.com/#b#`;
                         if(imgNaturalSize.h/imgNaturalSize.w >= containerSize.h/containerSize.w){
                             let height=contentSSize.h-10;
                             height=height<0?contentSSize.h:height;
-                            imgSty.height=height + 'px';
+                            if (rotate90) {
+                                imgSty.width=height + 'px';
+                                imgSty.setProperty('width', height+'px', 'important');
+                            } else {
+                                imgSty.height=height + 'px';
+                                imgSty.setProperty('height', height+'px', 'important');
+                            }
+                            imgPaSty.height=height + 'px';
+                            imgPaSty.setProperty('height', height+'px', 'important');
                             scaled=height/imgNaturalSize.h;
                         }else{
                             let width=contentSSize.w-10;
                             width=width<0?contentSSize.w:width;
+                            if (rotate90) {
+                                imgSty.height=width + 'px';
+                                imgSty.setProperty('height', width+'px', 'important');
+                            } else {
+                                imgSty.width=width + 'px';
+                                imgSty.setProperty('width', width+'px', 'important');
+                            }
                             imgSty.width=width + 'px';
+                            imgSty.setProperty('width', width+'px', 'important');
                             scaled=width/imgNaturalSize.w;
                         };
                         scaled=(scaled*100).toFixed(2) + '%';
