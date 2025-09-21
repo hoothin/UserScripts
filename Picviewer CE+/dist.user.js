e5914d4019de15cf0d31ed73ecff8398bf0645aa
hoothin-update <rixixi@gmail.com>
2025-09-21 12:17:05 +0000

    chore(Picviewer CE+): Auto-generate dist.user.js with timestamp

--- Picviewer CE+/dist.user.js
+++ Picviewer CE+/dist.user.js
@@ -45,9 +45,9 @@
 // @grant                GM.registerMenuCommand
 // @grant                GM.notification
 // @grant                unsafeWindow
-// @require              https://hoothin.github.io/UserScripts/Picviewer%20CE%2B/GM_config%20CN.js?v=1758456432
-// @require              https://update.greasyfork.org/scripts/438080/1664500/pvcep_rules.js
-// @require              https://hoothin.github.io/UserScripts/Picviewer%20CE%2B/pvcep_lang.js?v=1758456432
+// @require              https://hoothin.github.io/UserScripts/Picviewer%20CE%2B/GM_config%20CN.js?v=23710
+// @require              https://hoothin.github.io/UserScripts/Picviewer%20CE%2B/pvcep_rules.js?v=1664500
+// @require              https://hoothin.github.io/UserScripts/Picviewer%20CE%2B/pvcep_lang.js?v=1653424
 // @match                *://*/*
 // @exclude              http://www.toodledo.com/tasks/*
 // @exclude              http*://maps.google.com*/*
