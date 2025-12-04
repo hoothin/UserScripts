486f78c9b36a954fa01593bd5c2f6d5a238fd586
hoothin <rixixi@gmail.com>
2025-12-04 18:44:52 +0900

    Update Switch Traditional Chinese and Simplified Chinese.user.js

--- Switch Traditional Chinese and Simplified Chinese/Switch Traditional Chinese and Simplified Chinese.user.js
+++ Switch Traditional Chinese and Simplified Chinese/Switch Traditional Chinese and Simplified Chinese.user.js
@@ -6,7 +6,7 @@
 // @namespace    hoothin
 // @supportURL   https://github.com/hoothin/UserScripts
 // @homepageURL  https://github.com/hoothin/UserScripts
-// @version      1.2.7.11
+// @version      1.2.7.12
 // @description        任意轉換網頁中的簡體中文與正體中文（默認簡體→正體），顯示漢字對應漢語拼音，自訂任意替換文本
 // @description:zh-CN  任意转换网页中的简体中文与繁体中文（默认繁体→简体），显示汉字对应汉语拼音，自定义任意替换文本
 // @description:ja     ウェブページ上の簡体字中国語と繁体字中国語を自由に変換し、中国語の文字にひらがなを表示し、任意の文字を置き換えることができます。
@@ -1121,6 +1121,17 @@
     var _unsafeWindow = (typeof unsafeWindow == 'undefined') ? window : unsafeWindow;
     _unsafeWindow.tc2sc = simplized;
     _unsafeWindow.sc2tc = traditionalized;
+
+    window.addEventListener("message", function(event) {
+        if (event.data && event.data.type === "switchChineseRequest") {
+            const receivedData = event.data.payload;
+            const result = receivedData.target === 'sc' ? simplized(receivedData.str) : traditionalized(receivedData.str);
+            window.postMessage({
+                type: "switchChineseResult",
+                payload: result
+            }, "*");
+        }
+    });
 
     var storage = {
         supportGM: typeof GM_getValue == 'function' && typeof GM_getValue('a', 'b') != 'undefined',
